b4e8fb5c1f100d9934b695198c41ffbf414322f3
cristian <devnull@localhost>
2009-01-31 05:35:40 +0000

    added result verification

--- examples/working/run-long-tests.pl
+++ examples/working/run-long-tests.pl
@@ -6,6 +6,7 @@ use Getopt::Long;
 $module = "sleekex";
 $cvs_module_rep = "/home/nguyenh2/cvs-repo";
 $log_file = "long_test_result";
+$proof_dump = "last_proof_dump";
 $output_file = $log_file;
 $temp_output_dir = ".";
 $crt = system "pwd";
@@ -15,14 +16,159 @@ $examples_path_working = "sleekex/examples/working";
 $examples_path_working_bags = "sleekex/examples/working_bags";
 $examples_path_working_mult_specs = "sleekex/examples/working_mult_specs";
 # list of file, nr of functions, function name, output, function name, output......
-@hip_files=(["append.ss",1,"append","ERROR"]);
-# list of file, string with result of each entailment....
-@sleek_files=(
-			["sleek1.slk","Valid.Fail."],
-			["sleek2.slk","Valid.Fail."]);
-
-
+%hip_files=(
+		"append.ss"=>["",1,"append","SUCCESS"],
+		"append-tail.ss"=>["--combined-lemma-heuristic",1,"append","SUCCESS"],#
+		"bll.ss"=>["",2,"insert","SUCCESS","delete","SUCCESS"],
+		"bubble.ss"=>["",4,		"id2","SUCCESS",
+								"id3","SUCCESS",
+								"bubble","SUCCESS",
+								"bsort","SUCCESS",
+								#"skip","SUCCESS"
+								],
+		"cll.ss"=>["",5, "test","SUCCESS",
+						 "insert","SUCCESS",
+						 "count_rest","SUCCESS",
+						 "count","SUCCESS",
+						 "delete","SUCCESS"],
+		"complete.ss"=>["",5,"maxim","SUCCESS",
+								 "minim","SUCCESS",
+								 "height","SUCCESS",
+								 "min_height","SUCCESS",
+								 "insert","SUCCESS"],
+		"dll.ss"=>["",10,"insert","SUCCESS",
+							 "delete","SUCCESS",
+							 "delete1","SUCCESS",
+							 "test_del","SUCCESS",
+							 "test_del2","SUCCESS",
+							 "test_fold","SUCCESS",
+							 "append","SUCCESS",
+							 "append1","SUCCESS",
+							 "f1","SUCCESS",
+							 "f2","SUCCESS",
+							 #"append3","SUCCESS",
+							 #"find_last","SUCCESS",
+							 #"id1","SUCCESS"	
+							 ],
+		"heaps.ss"=>["",5,"insert","SUCCESS",
+								#"insert1","SUCCESS",
+								"deleteoneel","SUCCESS",
+								#"deleteoneel1","SUCCESS",
+								"deleteone","SUCCESS",
+								#"deleteone1","SUCCESS",
+								"ripple","SUCCESS",
+								#"ripple1","SUCCESS",
+								"deletemax","SUCCESS",
+								#"deletemax1","SUCCESS"
+								],
+		"insertion.ss"=>["",2,"insert","SUCCESS",
+								  "insertion_sort","SUCCESS"],
+		"ll.ss"=>["",10,"append","SUCCESS",
+						  "ret_first","SUCCESS",
+						  "get_next","SUCCESS",
+						  "set_next","SUCCESS",
+						  "set_null","SUCCESS",
+						  "get_next_next","SUCCESS",
+						  "insert","SUCCESS",
+						  "delete","SUCCESS",
+						  #"delete1","SUCCESS",
+						  "create_list","SUCCESS",
+						  "rev","SUCCESS",
+						  #"reverse1","SUCCESS",
+						  #"test","SUCCESS"
+						  ],
+		"merge.ss"=>["",5,"count","SUCCESS",
+							  "split_func","SUCCESS",
+							  #"div2","SUCCESS",
+							  "merge_sort","SUCCESS",
+							  "merge","SUCCESS",
+							  "insert","SUCCESS",
+							  #"merge_sort_1","SUCCESS"
+							  ],
+		"perfect.ss"=>["",5,"simple_insert","SUCCESS",
+								"create","SUCCESS",
+								"maxim","SUCCESS",
+								"height","SUCCESS",
+								"insert","SUCCESS"],
+		
+		"qsort.ss"=>["",3,	"partition","SUCCESS",
+								"append_bll","SUCCESS",
+								"qsort","SUCCESS"],
+		"qsort-tail.ss"=>["--combined-lemma-heuristic",2,"qsort","SUCCESS",
+									"partition1","SUCCESS"],
+		"rb.ss"=>["",18,"rotate_case_3","SUCCESS",
+							"case_2","SUCCESS",
+							"rotate_case_3r","SUCCESS",
+							"case_2r","SUCCESS",
+							"is_red","SUCCESS",
+							"is_black","SUCCESS",
+							"del_6","SUCCESS",
+							"del_6r","SUCCESS",
+							"del_5","SUCCESS",
+							"del_5r","SUCCESS",
+							"del_4","SUCCESS",
+							"del_4r","SUCCESS",
+							"del_3","SUCCESS",
+							"del_3r","SUCCESS",
+							"del_2","SUCCESS",
+							#"del_2r","SUCCESS",
+							#"bh","SUCCESS",
+							"remove_min","SUCCESS",
+							"del","SUCCESS",
+							#"test_insert","SUCCESS",
+							#"node_error","SUCCESS",
+							"insert","SUCCESS"],
+		"selection.ss"=>["",3,"find_min","SUCCESS",
+								"delete_min","SUCCESS",
+								"selection_sort","SUCCESS"],
+		"sll.ss"=>["",6,"insert","SUCCESS",
+							"insert2","SUCCESS",
+							"delete","SUCCESS",
+							"get_tail","SUCCESS",
+							"insertion_sort","SUCCESS",
+							"id","SUCCESS"],
+		"trees.ss"=>["",6,"append","SUCCESS",
+								#"append1","SUCCESS",
+								"count","SUCCESS",
+								"flatten","SUCCESS",
+								#"flatten1","SUCCESS",
+								"insert","SUCCESS",
+								#"insert1","SUCCESS",
+								"remove_min","SUCCESS",
+								#"remove_min1","SUCCESS",
+								"delete","SUCCESS",
+								#"delete1","SUCCESS"
+								]);
+			#	["2-3trees.ss",4,"make_node","SUCCESS","insert_left","SUCCESS","insert_middle","SUCCESS","insert_right","SUCCESS","insert","SUCCESS"],
+	
+				
+			#	["avl-bind.ss",13,"height","SUCCESS", "rotate_left","SUCCESS", "rotate_right","SUCCESS", "get-max","SUCCESS", "rotate_double_left","SUCCESS",
+			#		"rotate_double_right","SUCCESS","build_avl1","SUCCESS","build_avl2","SUCCESS","insert","SUCCESS",
+			#		"node_error","SUCCESS","insert_inline","SUCCESS","remove_min","SUCCESS","delete","SUCCESS"],
+			#	["avl.ss",13,	 "height","SUCCESS","rotate_left","SUCCESS","rotate_right","SUCCESS",
+			#					 "get_max","SUCCESS","rotate_double_left","SUCCESS","rotate_double_right","SUCCESS",
+			#					 "build_avl1","SUCCESS","build_avl2","SUCCESS","node_error","SUCCESS",
+			#					 "insert","SUCCESS","insert_inline","SUCCESS","remove_min","SUCCESS","delete","SUCCESS"],
+			#	["avl-orig-2.ss",8,"height","SUCCESS","get_max","SUCCESS","insert","SUCCESS",
+			#					 "double_left_child","SUCCESS","double_right_child","SUCCESS",
+			#					 "rotate_left_child","SUCCESS", "rotate_right_child","SUCCESS",
+			#					 "f","SUCCESS","g","SUCCESS","h","SUCCESS","k","SUCCESS","test",
+			#					 "SUCCESS","rotate_left_child_2","SUCCESS"],
+			 
+%sleek_files=(
+		"sleek.slk"=>"Valid.Valid.Valid.Fail.Valid.",
+		"sleek1.slk"=>"Valid.",
+		"sleek10.slk"=>"Valid.Fail.",
+		"sleek2.slk"=>"Fail.Valid.Fail.Fail.Valid.Valid.Valid.Fail.",
+		"sleek3.slk"=>"Fail.Fail.Fail.",
+		"sleek4.slk"=>"Valid.Valid.",
+		"sleek6.slk"=>"Valid.Valid.",
+		"sleek7.slk"=>"Valid.Valid.Valid.Fail.Valid.Valid.Valid.Valid.Fail.Valid.",
+		"sleek8.slk"=>"Valid.Fail.Valid.Valid.Valid.Valid.Valid.Valid.Valid.Fail.Valid.Valid.Valid.Valid.Fail.Valid.Fail.",
+		"sleek9.slk"=>"Valid.");
+	
 open(LOGFILE, ">> $log_file") || die ("Could not open $log_file.\n");
+open(PROOFLOGFILE, "> $proof_dump") || die ("Could not open $proof_dump.\n");
 
 $aux = `pwd`;
 print LOGFILE "$aux";
@@ -147,27 +293,44 @@ sub hip_process_file {
 
   if ($ext eq ".ss") {
 	print LOGFILE "Checking $file\n";
-	print "$hip -tp $tp $file";
+	$parameters = "";
+	if($hip_files{$file}) {$parameters = $hip_files{$file}->[0];}
+	print PROOFLOGFILE "\n$hip -tp $tp $file $parameters\n\n";
     my $output = "";
 	eval {
         local $SIG{ALRM} = sub { `pkill hip;pkill mona;pkill poly;pkill hip`;die "alarm clock restart" };
         alarm 1800;
-		$output = `$hip -tp $tp $file 2>&1`;
+		$output = `$hip -tp $tp $file $parameters 2>&1`;
         alarm 0;
     };
     if ($@ and $@ =~ /alarm clock restart/) {  $error_count++;$error_files = $error_files . "\n " . $file . " timed out ";print LOGFILE "!!!!!!!timed out\n"; print "!!!!!!!timed out"; }
 	else {
-		print "$output";
+		print PROOFLOGFILE "$output";
 		#print LOGFILE "\n======================================\n";
 		#print LOGFILE "$output";
 
+		if($hip_files{$file}) {
+			$limit = $hip_files{$file}->[1]*2+2;
+			#print "$output";
+			for($i = 2; $i<$limit;$i+=2)
+			{
+				if($output !~ /Procedure $hip_files{$file}->[$i].* $hip_files{$file}->[$i+1]/)
+				{
+					print LOGFILE "Error found in $file $hip_files{$file}->[$i]\n";
+			 		$error_count++;
+					$error_files=$error_files."error at: $hip_files{$file}->[0] $hip_files{$file}->[$i]\n";
+				}
+			}
+		}
+		else{
 		if (($output =~ m/.*error.*/i)||($output =~ m/.*fail.*/i)) {
-		  print LOGFILE "Error found\n";
+		  print LOGFILE "Error found in the newly added file $file\n";
 		  $error_count++;
 		  $error_files = $error_files . " " . $file;
 		  if ($stop) {
 			exit(0);
 		  }
+		}
 		}
 	}
   }
@@ -189,18 +352,52 @@ sub sleek_process_file  {
       };
       if ($@ and $@ =~ /alarm clock restart/) {  $error_count++;$error_files = $error_files . " " . $file;print "!!!!!!!timed out\n"; }
       else {
-          print "$output";
+          print PROOFLOGFILE "$output";
           #print LOGFILE "\n======================================\n";
           #print LOGFILE "$output";
-
+		  if($sleek_files{$file}) 
+			{
+						$pos = 0;
+						$r = "";
+						while($pos >= 0)
+						{
+							$i = index($output, "Valid",$pos);
+							$j = index($output, "Fail",$pos);
+							if ($i==-1 && $j == -1)
+								{$pos = -1;}
+							else
+							{
+								if(($i<$j || $j==-1)&& ($i>=0))
+								{
+									$pos=$i+3;
+									$r = $r ."Valid.";
+								}
+								else
+								{
+									$pos=$j+3;
+									$r = $r ."Fail.";
+								}
+							}
+							if ($pos >=length($output)) 
+							{$pos = -1;}
+						}
+						if($r !~ /^$sleek_files{$file}$/)
+						{
+							print LOGFILE "Unexpected result with : $file got: $r expected $sleek_files{$file}\n";
+							$error_count++;
+							$error_files = $error_files . " " . $file;
+						}  
+			}
+		  else{
           if (($output =~ m/.*error.*/i)||($output =~ m/.*fail.*/i)) {
-	          print LOGFILE "Error found\n";
+	          print LOGFILE "Error found in the newly added file $file\n";
 	          $error_count++;
 	          $error_files = $error_files . " " . $file;
 	          if ($stop) {
                   exit(0);
 	          }
           }
+		}
       }
   }
 }
