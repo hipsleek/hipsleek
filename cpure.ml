0d19f2a8588a7a6e14497b189fcc2001b4bad878
Chin Wei Ngan <chinwn@gmail.com>
2009-03-05 18:19:06 +0000

    *** empty log message ***

--- cpure.ml
+++ cpure.ml
@@ -636,12 +636,90 @@ and subst_var_list sst (svs : spec_var list) = match svs with
 
 and subst_avoid_capture (fr : spec_var list) (t : spec_var list) (f : formula) =
   let st1 = List.combine fr t in
-  let f2 = subst st1 f in
+  (* let f2 = subst st1 f in *)
+  (* changing to a parallel substitution below *)
+  let f2 = apply_subs st1 f in
   f2
 
 and subst (sst : (spec_var * spec_var) list) (f : formula) : formula = match sst with
   | s::ss -> subst ss (apply_one s f) 				(* applies one substitution at a time *)
   | [] -> f
+
+and apply_subs (sst : (spec_var * spec_var) list) (f : formula) : formula = match f with
+  | BForm bf -> BForm (b_apply_subs subs bf)
+  | And (p1, p2, pos) -> And (apply_subs sst p1,
+							  apply_subs sst p2, pos)
+  | Or (p1, p2, pos) -> Or (apply_subs sst p1,
+							apply_subs sst p2, pos)
+  | Not (p, pos) -> Not (apply_subs sst p, pos)
+  | Forall (v, qf, pos) ->
+      let sst = diff sst v in
+      if (var_in_target v sst) then
+        let fresh_v = fresh_spec_var v in
+           Forall (fresh_v, apply_subs sst (apply_one (v, fresh_v) qf), pos)
+      else Forall (v, apply_subs sst qf, pos)
+  | Exists (v, qf, pos) ->
+      let sst = diff sst v in
+      if (var_in_target v sst) then
+        let fresh_v = fresh_spec_var v in
+           Exists  (fresh_v, apply_subs sst (apply_one (v, fresh_v) qf), pos)
+      else Exists  (v, apply_subs sst qf, pos)
+
+
+and diff sst v = filter (fun (a,_) -> a==v) sst
+
+and var_in_target v sst = foldl (fun (_,t) curr -> curr or t==v) false sst
+
+and b_apply_subs sst bf = match bf with
+  | BConst _ -> bf
+  | BVar (bv, pos) -> BVar (subs_one sst bv, pos)
+  | Lt (a1, a2, pos) -> Lt (e_apply_subs sst a1,
+							e_apply_subs sst a2, pos)
+  | Lte (a1, a2, pos) -> Lte (e_apply_subs sst a1,
+							  e_apply_subs sst a2, pos)
+  | Gt (a1, a2, pos) -> Gt (e_apply_subs sst a1,
+							e_apply_subs sst a2, pos)
+  | Gte (a1, a2, pos) -> Gte (e_apply_subs sst a1,
+							  e_apply_subs sst a2, pos)
+  | Eq (a1, a2, pos) -> Eq (e_apply_subs sst a1,
+							e_apply_subs sst a2, pos)
+  | Neq (a1, a2, pos) -> Neq (e_apply_subs sst a1,
+							  e_apply_subs sst a2, pos)
+  | EqMax (a1, a2, a3, pos) -> EqMax (e_apply_subs sst a1,
+									  e_apply_subs sst a2,
+									  e_apply_subs sst a3, pos)
+  | EqMin (a1, a2, a3, pos) -> EqMin (e_apply_subs sst a1,
+									  e_apply_subs sst a2,
+									  e_apply_subs sst a3, pos)
+	| BagIn (v, a1, pos) -> BagIn (subs_one sst v, e_apply_subs sst a1, pos)
+	| BagNotIn (v, a1, pos) -> BagNotIn (subs_one sst v, e_apply_subs sst a1, pos)
+	(* is it ok?... can i have a set of boolean values?... don't think so..*)
+  | BagSub (a1, a2, pos) -> BagSub (e_apply_subs sst a1, e_apply_subs sst a2, pos)
+  | BagMax (v1, v2, pos) -> BagMax (subs_one sst v1, subs_one sst v2, pos)
+	| BagMin (v1, v2, pos) -> BagMin (subs_one sst v1, subs_one sst v2, pos)
+
+subs_one sst v = foldl (fun (fr,t) old -> if fr==v then t else old) v sst
+
+and e_apply_subs sst e = match e with
+  | Null _ | IConst _ -> e
+  | Var (sv, pos) -> Var (subs_one sst sv, pos)
+  | Add (a1, a2, pos) -> Add (e_apply_subs sst a1,
+							  e_apply_subs sst a2, pos)
+  | Subtract (a1, a2, pos) -> Subtract (e_apply_subs sst  a1,
+										e_apply_subs sst a2, pos)
+  | Mult (c, a, pos) -> Mult (c, e_apply_subs sst a, pos)
+  | Max (a1, a2, pos) -> Max (e_apply_subs sst a1,
+							  e_apply_subs sst a2, pos)
+  | Min (a1, a2, pos) -> Min (e_apply_subs sst a1,
+							  e_apply_subs sst a2, pos)
+	(*| BagEmpty (pos) -> BagEmpty (pos)*)
+	| Bag (alist, pos) -> Bag ((e_apply_subs_bag sst alist), pos)
+	| BagUnion (alist, pos) -> BagUnion ((e_apply_subs_bag sst alist), pos)
+  | BagIntersect (alist, pos) -> BagIntersect ((e_apply_subs_bag sst alist), pos)
+  | BagDiff (a1, a2, pos) -> BagDiff (e_apply_subs sst a1,
+							  e_apply_subs sst a2, pos)
+
+and e_apply_subs_bag sst alist = map (e_apply_subs sst) alist
 
 and apply_one (fr, t) f = match f with
   | BForm bf -> BForm (b_apply_one (fr, t) bf)
