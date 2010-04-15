5134f05d9e23415d8f411a2288105dc1c2e96334
craciunm <devnull@localhost>
2010-04-15 11:54:42 +0000

    *** empty log message ***

--- typeclass.ml
+++ typeclass.ml
@@ -1,16 +1,16 @@
-module type EQ_m =
+module type EQ_B =
 sig
   type a
   val eq: a -> a -> bool
 end
 
-module type ORD_m =
+module type ORD_B =
 sig
-  include EQ_m
+  include EQ_B
   val lt: a -> a -> bool
 end
 
-module EQ_E (M : EQ_m) = struct
+module EQ (M : EQ_B) = struct
   include M
   let neq x y = not(eq x y)
   let member (y:a) (xs : a list) =
@@ -20,11 +20,12 @@ module EQ_E (M : EQ_m) = struct
     helper xs
 end
 
-module ORD_E (M : ORD_m) = struct
+module ORD (M : ORD_B) = struct
   include M
   let leq x y = (lt x y) || (eq x y)
   let max (xs : a list) =
   let rec helper xs = match xs with
+    | [] -> failwith "[] for max argument"
     | [x] -> x
     | x::xs -> let y=helper xs in
 	if lt x y then x else y in
@@ -32,19 +33,19 @@ module ORD_E (M : ORD_m) = struct
 end
 
   
-module I_Int_EQ = struct
+module I_Int_EQ_B = struct
   type a = int
   let eq = (=)
 end
   
-module I_Int_EQ_E = EQ_E(I_Int_EQ)
+module I_Int_EQ = EQ(I_Int_EQ_B)
 
 
 
-module I_Int_ORD = struct
-  include I_Int_EQ
+module I_Int_ORD_B = struct
+  include I_Int_EQ_B
   let lt = (<)
 end
 
-module I_Int_ORD_E = ORD_E(I_Int_ORD)
+module I_Int_ORD = ORD(I_Int_ORD_B)
 
