3e8dc85b625e2249face38024cd571f8a43bc768
lebinh <devnull@localhost>
2010-10-20 13:43:59 +0000

    Modified some command line arguments for Redlog

--- sleek.ml
+++ sleek.ml
@@ -85,6 +85,9 @@ let process_cmd_line () = Arg.parse [
   ("--pcp", Arg.Set Globals.print_core,"print core representation");
   ("--iw",  Arg.Set Globals.wrap_exists_implicit_explicit ,"existentially wrap instantiations after the entailment");
   ("--slk-err", Arg.Set Globals.print_err_sleek,"print sleek errors");
+  ("--rl-no-pseudo-ops", Arg.Set Redlog.no_pseudo_ops, "Do not pseudo-strengthen/weaken formulas before send to Redlog");
+  ("--rl-no-ee", Arg.Set Redlog.no_elim_exists, "Do not try to eliminate existential quantifier with Redlog");
+  ("--rl-timeout", Arg.Set_int Redlog.timeout, "Set timeout (in seconds) for is_sat or imply with Redlog");
   (*("--iv", Arg.Set_int Globals.instantiation_variants,"instantiation variants (0-default)->existentials,implicit, explicit; 1-> implicit,explicit; 2-> explicit;
   3-> existentials,implicit; 4-> implicit; 5-> existential,explicit;");*)
 ] set_source_file usage_msg
