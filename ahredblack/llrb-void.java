cfc4652f19486ea25de1741e547d4d478d543aa5
anhoa <devnull@localhost>
2011-07-12 14:24:59 +0800

    Make delete_internal works with a human-provable assumption using dprint.

--- ahredblack/llrb-void.java
+++ ahredblack/llrb-void.java
@@ -58,24 +58,6 @@ node insert(node h, int v)
 	dprint;
 }
 
-// Delete the value v in the red black tree if it appears.
-node delete(node h, int v)
-	requires h::rbc<n,1,bh,_> // root node is assumed to be always black
-	ensures res::rbc<resn,1,resbh,_> & n-1 <= resn <= n & bh - 1 <= resbh <= bh;
-{
-	if (h != null) {
-		// change 2 node into pseudo-4
-		if (!is_red(h.left)) h.color = 0;
-		
-		h = delete_internal(h, v);
-		
-		// restore the color of the root node
-		if (is_red(h)) h.color = 1;
-	}
-	
-	return h;
-}
-
 node delete_min(node h, ref int min_value)
 	requires h::rbc<n,1,bh,_> & n >= 1
 	ensures res::rbc<n-1,1,bh,_> or res::rbc<n-1,1,bh-1,_>;
@@ -94,94 +76,27 @@ node delete_min(node h, ref int min_value)
 	return h;
 }
 
+//Delete the value v in the red black tree if it appears.
+node delete(node h, int v)
+	requires h::rbc<n,1,bh,_> // root node is assumed to be always black
+	ensures res::rbc<resn,1,resbh,_> & n-1 <= resn <= n & bh - 1 <= resbh <= bh;
+{
+	if (h != null) {
+		// change 2 node into pseudo-4
+		if (!is_red(h.left)) h.color = 0;
+		
+		h = delete_internal(h, v);
+		
+		// restore the color of the root node
+		if (is_red(h)) h.color = 1;
+	}
+	
+	return h;
+}
+
 //////////////////////////////////////////
 //     HEIGHT-INVARIANT OPERATIONS      //
 //////////////////////////////////////////
-
-// Delete without changing the height
-node delete_internal(node h, int v)
-	requires h::rbc<n,_,bh,c> & c >= 2
-	case {
-		c = 2 -> ensures res::rbc<n1,_,bh,2> & n-1 <= n1 <= n 
-						or res::rbc<n1,_,bh,1> & n-1 <= n1 <= n;
-		c = 3 -> ensures res::rbc<n1,_,bh,3> & n-1 <= n1 <= n 
-						or res::rbc<n1,_,bh,2> & n-1 <= n1 <= n;
-		(c < 2 | c >= 4) -> ensures res::rbc<n1,0,bh,4> & n-1 <= n1 <= n
-						or res::rbc<n1,1,bh,3> & n-1 <= n1 <= n 
-		 				or res::rbc<n1,1,bh,0> & n-1 <= n1 <= n;
-	}
-{	
-	if (v < h.val) {
-		if (h.left != null) {
-//			if (!is_red(h.left) && !is_red(h.left.left))
-//				h = move_red_left(h);
-			
-			h.left = delete_internal(h.left, v);
-			
-			//fix_up(h); 
-			// what could possibly go wrong? only the left-leaning property!
-			if (is_red(h.right) && !is_red(h.left))
-				rotate_left(h);
-		}
-	} else {
-		if (v >= h.val) { // v >= h.val		
-			if (is_red(h.left))
-				rotate_right(h);
-	
-			if (v == h.val && (h.right == null))
-				h = null;
-			
-//			if (!is_red(h.right) && !is_red(h.right.left))
-//				h = move_red_right(h); 
-			
-			if (v == h.val) {
-				int m;
-				h.right = delete_min(h.right, m);
-				h.right.val = m;
-			}
-			else 
-				h.right = delete_internal(h.right, v);
-			
-			//fix_up(h); // what can go wrong?
-			if (is_red(h.right) && !is_red(h.left))
-				rotate_left(h);
-		}
-	
-	}
-
-	return h;
-}
-
-// Delete the node with minimum value under h
-// and return that minimum value.
-// NOTE: NO CHANGE IN HEIGHT
-node delete_min_internal(node h, ref int min_value)
-	requires h::rbc<n,_,bh,c> & c >= 2 & n >= 1
-	case {
-		c = 2 -> ensures res::rbc<n-1,1,bh,2> or res::rbc<n-1,1,bh,1>;
-		c = 3 -> ensures res::rbc<n-1,1,bh,3> or res::rbc<n-1,1,bh,2>;
-		(c < 2 | c >= 4) -> case {
-			n = 1 -> ensures res::rbc<n-1,1,bh,0> & bh = 1 & n = 1 & res = null;
-			n != 1 -> ensures res::rbc<n-1,0,bh,4> or res::rbc<n-1,1,bh,3> or res::rbc<n-1,1,bh,2>;
-		}
-	}
-{
-	if (h.left == null) {
-		min_value = h.val;
-		return null;
-	}
-
-	if (rbcase(h.left) == 1) 
-		move_red_left(h);
-
-	h.left = delete_min_internal(h.left, min_value);
-
-	if (is_red(h.right)) 
-		if (!is_red(h.left)) 
-			rotate_left(h);
-	
-	return h;
-}
 
 // Insert a value v to an INTERNAL node of a red-black tree.
 // Remark: NO height increment.
@@ -197,7 +112,7 @@ node insert_internal(node h, int v)
 		(c < 0 | c >= 4) -> ensures res::rbc<n+1,0,bh,4> or res::rbs<n+1,bh>;
 	}
 {
-	//assume c != 3; // ONLY verified WITH --eps option
+	assume c != 3; // ONLY verified WITH --eps option
 	//assume c = 3; // ONLY verified WITHOUT --eps option
 	if (h == null) {
 		return new node(v, 0, null, null); // RED node
@@ -227,9 +142,219 @@ node insert_internal(node h, int v)
 	}
 }
 
+//Delete the node with minimum value under h
+//and return that minimum value.
+//NOTE: NO CHANGE IN HEIGHT
+node delete_min_internal(node h, ref int min_value)
+	requires h::rbc<n,_,bh,c> & c >= 2 & n >= 1
+	case {
+		c = 2 -> ensures res::rbc<n-1,1,bh,2> or res::rbc<n-1,1,bh,1>;
+		c = 3 -> ensures res::rbc<n-1,1,bh,3> or res::rbc<n-1,1,bh,2>;
+		(c < 2 | c >= 4) -> case {
+			n = 1 -> ensures res::rbc<n-1,1,bh,0> & bh = 1 & n = 1 & res = null;
+			n != 1 -> ensures res::rbc<n-1,0,bh,4> or res::rbc<n-1,1,bh,3> or res::rbc<n-1,1,bh,2>;
+		}
+	}
+{
+	if (h.left == null) {
+		min_value = h.val;
+		return null;
+	}
+
+	if (rbcase(h.left) == 1) 
+		move_red_left(h);
+
+	h.left = delete_min_internal(h.left, min_value);
+
+	if (is_red(h.right)) 
+		if (!is_red(h.left)) 
+			rotate_left(h);
+	
+	return h;
+}
+
+//Delete without changing the height
+node delete_internal(node h, int v)
+	requires h::rbc<n,_,bh,c> & c >= 2
+	case {
+		c = 2 -> ensures res::rbc<n1,1,bh,2> & n-1 <= n1 <= n 
+						or res::rbc<n1,1,bh,1> & n-1 <= n1 <= n;
+		c = 3 -> ensures res::rbc<n1,1,bh,3> & n-1 <= n1 <= n 
+						or res::rbc<n1,1,bh,2> & n-1 <= n1 <= n;
+		(c < 2 | c >= 4) -> ensures res::rbc<n1,0,bh,4> & n-1 <= n1 <= n
+						or res::rbc<n1,1,bh,3> & n-1 <= n1 <= n 
+						or res::rbc<n1,1,bh,2> & n-1 <= n1 <= n 
+						or res::rbc<0,1,1,0> & n = 1 & bh = 1 & res = null;
+	}
+{
+	if (v < h.val) {
+		// h.left == null means we cannot find v in the tree!
+		if (h.left != null) {
+			if (rbcase(h.left) == 1)
+				move_red_left(h);
+			h.left = delete_internal(h.left, v);
+		}
+	} else {
+		if (rbcase(h) == 2) {
+			rotate_right(h);
+			h.right = delete_internal(h.right, v);
+		} else if (rbcase(h) == 3) {
+			if (v == h.val) {
+				int m;
+				h.right = delete_min_internal(h.right, m);
+				h.val = m;
+			} else
+				h.right = delete_internal(h.right, v);
+		} else {
+			if (h.right == null) {
+				node l = h.left;
+				assume l' = null; // This should be provable, but currently not!
+				if (v == h.val) 
+					h = null;
+			} else {
+				if (rbcase(h.right) == 1)
+					move_red_right(h);
+				if (v == h.val) {
+					int m;
+					h.right = delete_min_internal(h.right, m);
+					h.val = m;
+				} else
+					h.right = delete_internal(h.right, v);
+			}
+		}
+	}
+	
+	// Fix the left-leaning at h
+	if (h != null)
+		if (is_red(h.right))
+			if (!is_red(h.left))
+				rotate_left(h);
+	
+	return h;
+}
+
 //////////////////////////////////////////
 //          HELPER FUNCTIONS            //
 //////////////////////////////////////////
+
+// Enrich the right branch
+void move_red_right(node h)
+	requires h::node<_,0,l,r> * l::rbc<ln,1,bh,c> * r::rbc<rn,1,bh,1>
+	case {
+		c = 1 -> ensures h::rbc<1+ln+rn,1,bh,3>;
+		c != 1 -> ensures h::node<_,0,resl,resr> * 
+	 		resl::rbc<resln,1,bh,_> * 
+	 		resr::rbc<resrn,1,bh,2> & 
+	 		resln + resrn = ln + rn
+	 	or 	h::node<_,0,resl,resr> * 
+	 		resl::rbc<resln,1,bh,_> * 
+	 		resr::rbc<resrn,1,bh,3> & 
+	 		resln + resrn = ln + rn;
+	}
+{
+	color_flip(h);
+	
+	if (is_red(h.left.left)) {
+		rotate_right(h);
+		color_flip(h);
+		
+		// Ensure left leaning for the tree on the right branch.
+		if (!is_red(h.right.left))
+			rotate_left(h.right);
+	}
+}
+
+// Enrich the left branch
+void move_red_left(node h)
+	requires h::node<_,0,l,r> * l::rbc<ln,1,bh,1> * r::rbc<rn,1,bh,c>
+	case {
+		c  = 1 -> ensures h::node<_,1,resl,resr> * 
+						resl::rbc<ln,0,bh-1,4> * 
+						resr::rbc<rn,0,bh-1,4>;
+		c != 1 -> ensures h::node<_,0,resl,resr> * 
+	    				resl::rbc<resln,1,bh,2> * 
+	    				resr::rbc<resrn,1,bh,_> & 
+	    				resln + resrn = ln + rn;
+	}
+{
+	color_flip(h);
+	
+	if (is_red(h.right.left)) {
+		rotate_right(h.right);
+		rotate_left(h);
+		color_flip(h);
+		
+		// Ensure left-leaning on the right branch! Robert's code is more efficient as it does not requires this step but it is troublesome to write specification.
+		if (!is_red(h.right.left))
+			if (is_red(h.right.right))
+				rotate_left(h.right);
+	}
+}
+
+// Rotate the structure clockwise
+void rotate_right(node h)
+	requires h::node<v,c,l,r> * l::node<lv,0,ll,lr>
+	ensures h::node<lv,c,ll,r1> * r1::node<v,0,lr,r>;
+{
+	node nr = new node(h.val, 0, h.left.right, h.right);
+	h.val = h.left.val;
+	h.left = h.left.left;
+	h.right = nr;
+}
+
+// Rotate the structure counter clockwise
+void rotate_left(node h)
+	requires h::node<v,c,l,r> * r::node<rv,0,rl,rr>
+	ensures h::node<rv,c,l1,rr> * l1::node<v,0,l,rl>;
+{
+	node nl = new node(h.val, 0, h.left, h.right.left);
+	h.val = h.right.val;
+	h.left = nl;
+	h.right = h.right.right;
+}
+
+// Flip the color of a node and its two immediate children.
+void color_flip(node h)
+	requires h::node<v,c,l,r> * l::node<lv,lc,ll,lr> * r::node<rv,rc,rl,rr>
+	ensures h::node<v,1-c,l,r> * l::node<lv,1-lc,ll,lr> * r::node<rv,1-rc,rl,rr>;
+{
+	assert h != null;
+	h.color        = 1 - h.color;
+	h.left.color   = 1 - h.left.color;
+	h.right.color  = 1 - h.right.color;
+}
+
+// Check if h is a red node.
+bool is_red(node h)
+	case{
+		h  = null -> ensures !res;
+		h != null -> requires h::node<v,c,l,r>@I 
+		case {
+			c = 0 -> ensures res;
+			c != 0 -> ensures !res;
+		}
+	}
+{
+	if (h == null)
+		return false;
+	else 
+		return (h.color == 0);
+}
+
+//////////////////////////////////////////
+//      MISCELLANEOUS FUNCTIONS         //
+//////////////////////////////////////////
+
+// Compute the black height of a red black tree
+int rbheight(node h)
+	requires h::rbc<_,_,bh,_>@I
+	ensures res = bh;
+{
+	if (h == null)
+		return 1;
+	else
+		return h.color + rbheight(h.left);
+}
 
 // Identify the case of the red black tree rooted at h as in the definition.
 int rbcase(node h)
@@ -255,7 +380,7 @@ int rbcase(node h)
 	}
 }
 
-
+// Find the number of nodes under h.
 int rbsize(node h)
 	requires h::rbc<n,cl,bh,c>@I
 	ensures res = n;
@@ -268,128 +393,4 @@ int rbsize(node h)
 		int r = 1 + ls + rs;
 		return 1 + ls + rs;
 	}
-}
-
-
-// Check if h is a red node or not.
-bool is_red(node h)
-	case{
-		h  = null -> ensures !res;
-		h != null -> requires h::node<v,c,l,r>@I 
-//						ensures h::node<v,c,l,r> & c != 0 & !res 
-//        				or h::node<v,c,l,r> & c = 0 & res;
-		/** Replace a single ensure by case analysis **/
-		case {
-			c = 0 -> ensures res;
-			c != 0 -> ensures !res;
-		}
-	}
-{
-	if (h == null)
-		return false;
-	else 
-		return (h.color == 0);
-}
-
-
-// Flip the color of a node and its two children.
-void color_flip(node h)
-	requires h::node<v,c,l,r> * l::node<lv,lc,ll,lr> * r::node<rv,rc,rl,rr>
-	ensures h::node<v,1-c,l,r> * l::node<lv,1-lc,ll,lr> * r::node<rv,1-rc,rl,rr>;
-{
-	assert h != null;
-	h.color        = 1 - h.color;
-	h.left.color   = 1 - h.left.color;
-	h.right.color  = 1 - h.right.color;
-}
-
-
-void rotate_left(node h)
-	requires h::node<v,c,l,r> * r::node<rv,0,rl,rr>
-	ensures h::node<rv,c,l1,rr> * l1::node<v,0,l,rl>;
-{
-	node nl = new node(h.val, 0, h.left, h.right.left);
-	h.val = h.right.val;
-	h.left = nl;
-	h.right = h.right.right;
-}
-
-
-void rotate_right(node h)
-	requires h::node<v,c,l,r> * l::node<lv,0,ll,lr>
-	ensures h::node<lv,c,ll,r1> * r1::node<v,0,lr,r>;
-{
-	node nr = new node(h.val, 0, h.left.right, h.right);
-	h.val = h.left.val;
-	h.left = h.left.left;
-	h.right = nr;
-}
-
-
-// Compute the black height of a red black tree
-int black_height(node h)
-	requires h::rbc<_,_,bh,_>@I
-	ensures res = bh;
-{
-	if (h == null)
-		return 1;
-	else
-		return h.color + black_height(h.left);
-}
-
-//Assuming that h is red and both h.left and h.left.left
-//are black, make h.left or one of its children red.
-void move_red_left(node h)
-	requires h::node<_,0,l,r> * l::rbc<ln,1,bh,1> * r::rbc<rn,1,bh,c>
-	case {
-		c  = 1 -> ensures h::node<_,1,resl,resr> * 
-						resl::rbc<ln,0,bh-1,4> * 
-						resr::rbc<rn,0,bh-1,4>;
-		c != 1 -> ensures h::node<_,0,resl,resr> * 
-        				resl::rbc<resln,1,bh,2> * 
-        				resr::rbc<resrn,1,bh,_> & 
-        				resln + resrn = ln + rn;
-	}
-{
-	color_flip(h);
-
-	if (is_red(h.right.left)) {
-		rotate_right(h.right);
-		rotate_left(h);
-		color_flip(h);
-		
-		// Ensure left-leaning on the right branch! 
-		// Robert seemed not to notice this.
-		if (!is_red(h.right.left))
-			if (is_red(h.right.right))
-				rotate_left(h.right);
-	}
-}
-
-//Assuming that h is red and both h.right and h.right.left
-//are black, make h.right or one of its children red.
-node move_red_right(node h)
-	requires h::node<_,0,l,r> * l::rbc<ln,1,bh,c> * r::rbc<rn,1,bh,1>
-	ensures res::rbc<n,1,bh,3>
-     	or  res::node<_,0,resl,resr> * 
-     		resl::rbc<resln,1,bh,_> * 
-     		resr::rbc<resrn,1,bh,2> & 
-     		resln + resrn = ln + rn
-     	or 	res::node<_,0,resl,resr> * 
-     		resl::rbc<resln,1,bh,_> * 
-     		resr::rbc<resrn,1,bh,3> & 
-     		resln + resrn = ln + rn;
-{
-	color_flip(h);
-	
-	if (is_red(h.left.left)) {
-		rotate_right(h);
-		color_flip(h);
-		
-		// Ensure left leaning for the tree on the right branch.
-		if (!is_red(h.right.left))
-			rotate_left(h.right);
-	}
-	
-	return h;
 }
