e5a52ba676ac455d432254dadfc0a2043c187ed2
anhoa <vuanhoa@comp.nus.edu.sg>
2011-11-01 16:33:48 +0800

    Implement example on (recursive) algorithm to find shortest distance between two vertices in a graph.

--- examples/working/hip/array/arr_shortest_distance.java
+++ examples/working/hip/array/arr_shortest_distance.java
@@ -5,66 +5,81 @@
  * @author Vu An Hoa
  */
  
-data Graph {
-	int[][] A; 	// adjacency matrix: A[i][j] = direct distance between node <i> and <j>
-				// presumably all >= 0 and 0 if there is no edge between them
-	int n;		// number of vertices, index from 0 to n-1
-}
-
-relation non_negative(int[][] A, int n) == 
+// A[0..n-1,0..n-1] is >=0 with 0 indicating no path
+relation nonneg(int[,] A, int n) == 
 	forall(i, j : i < 0 | i >= n | j < 0 | j >= n | A[i,j] >= 0).
 
-// there is a path from s to e of length l
-relation has_path_length(int[][] A, int n, int s, int e, int l) ==
-	(A[i,j] = l | exists(u : 0 <= u < n & A[i,u] > 0 & 
-						has_path_length(A, n, u, e, l - A[i,u]))).
+// there is a path with <= k edges from i to j of length l
+relation kpath(int[,] A, int n, int i, int j, int k) ==
+	k >= 0 & (i = j | exists(u : 0 <= u < n & A[i,u] > 0 & kpath(A, n, u, j, k - 1))).
 
-// d is the shortest distance from s to e
-relation shortest_distance(int[][] A, int n, int s, int e, int d) ==
-	has_path_length(A, n, s, e, d) & forall(l : !(has_path_length(A, n, s, e, l)) | d <= l).
+// there is a path with <= k edges from i to j of length l
+relation kpathl(int[,] A, int n, int i, int j, int k, int l) ==
+	kpathlc(A, n, i, j, k, l, n - 1).	
+	//k >= 0 & (i = j & l = 0 | exists(u : 0 <= u < n & A[i,u] > 0 & kpathl(A, n, u, j, k - 1, l - A[i,u]))).
+	
+// there is a path with <= k edges from i to j of length l with the first vertex reach from
+// i is <= umax
+relation kpathlc(int[,] A, int n, int i, int j, int k, int l, int umax) ==
+	k >= 0 & umax < n & (i = j & l = 0 | exists(u : 0 <= u <= umax & A[i,u] > 0 & kpathlc(A, n, u, j, k - 1, l - A[i,u], n - 1))).
+	
+// there is a path with <= k edges from i to j of length l
+relation nokpath(int[,] A, int n, int i, int j, int k, int umax) ==
+	i != j & forall(u : u < 0 | u > umax | !(kpath(A, n, u, j, k - 1))).
+	
+// d is the length of shortest path within k edges from i --> j
+relation ksdist(int[,] A, int n, int i, int j, int k, int d) ==
+	ksdistvia(A, n, i, j, k, d, n - 1).
+	//kpathl(A, n, i, j, k, d) & forall(l : !(kpathl(A, n, i, j, k, l)) | d <= l).
+	
+relation ksdistvia(int[,] A, int n, int i, int j, int k, int d, int vmax) ==
+	kpathlc(A, n, i, j, k, d, vmax) & forall(l : !(kpathlc(A, n, i, j, k, l, vmax)) | d <= l).
 
-// d is the length of shortest path from s --> e
-relation shortest_distance_k(int[][] A, int n, int k, int s, int e, int d) ==
-	has_path_length(A, n, s, e, d) & forall(l : !(has_path_length(A, n, s, e, l)) | d <= l).	
+axiom nonneg(A,n) & i = j & k >= 0 ==> ksdist(A,n,i,j,k,0).
 
-/**
- Shortest distance algorithm using DP
- */ 
-int shortest_distance(Graph G, int s, int e)
-	requires G::Graph<A,n> & n >= 0 & 0 <= s < n & 0 <= e < n & non_negative(A, n)
-	ensures shortest_distance(A, n, s, e, d);
+axiom nokpath(A,n,i,j,k,n-1) ==> !(kpath(A,n,i,j,k)).
+
+axiom A[i,v] = 0 & nokpath(A,n,i,j,k,v-1) ==> nokpath(A,n,i,j,k,v).
+
+axiom A[i,v] = 0 & ksdistvia(A,n,i,j,k,d,v-1) ==> ksdistvia(A,n,i,j,k,d,v).
+
+// Compute shortest distance s --> e via <= k edges; 
+// -1 if there is no path from s --> e within k edges
+int sdk(int[,] A, int n, int s, int e, int k)
+	requires nonneg(A,n) & 0 <= s < n & 0 <= e < n & n >= 0 & k >= 0
+	ensures (res >= 0 & ksdist(A,n,s,e,k,res) | res < 0 & !(kpath(A,n,s,e,k)));
 {
-	int k, v, u;
+	if (s == e)
+		return 0;
+		
+	if (k == 0) // & s != e
+		return -1;
 	
-	// shortest[x] = shortest distance from x to e within k steps
-	// -1 to indicate infinity i.e. no path consisting <= k intermediate
-	// vertices joining x --> e
-	int[] shortest = new int[n];
-	
-	// initialize
-	k = 0;
-	v = 0;
-	while (v < n) {
-		if (v == e)
-			shortest[v] = 0;
-		else
-			shortest[v] = -1;
+	return sdkhelper(A, n, s, e, k, 0, -1);
+}
+
+int sdkhelper(int[,] A, int n, int s, int e, int k, int v, int m) 
+	requires nonneg(A, n) & 0 <= s < n & 0 <= e < n & n >= 0 & 0 <= v <= n & k > 0 & s != e
+	case {
+		m < 0 -> requires nokpath(A,n,s,e,k,v-1)
+				ensures (res >= 0 & ksdist(A,n,s,e,k,res) | res < 0 & !(kpath(A,n,s,e,k)));
+		m >= 0 -> requires ksdistvia(A,n,s,e,k,m,v - 1)
+				ensures (res >= 0 & ksdist(A,n,s,e,k,res) | res < 0 & !(kpath(A,n,s,e,k)));
 	}
-	
-	while (
-	// recursive computation
-	for(int k = 1; k <= n; k++) {
-		for(int v = 0; v < n; v++) {
-			for(int u = 0; u < n; u++) {
-				if (shortest[u] != -1 || G.A[v,u] != 0) {
-					int t = G.A[v,u] + shortest[u];
-					if (shortest[v] == -1 || t < shortest[v])
-						shortest[v] = t;
+{
+	if (v < n) {
+		if (A[s,v] != 0) {
+			int r = sdk(A, n, v, e, k - 1);
+			if (r >= 0) {
+				r = A[s,v] + r;
+				if (m < 0 || m > r) {
+					m = r;
 				}
 			}
 		}
+		assume m' < 0 | m' >= 0 & ksdistvia(A,n,s,e,k,m',v);
+		return sdkhelper(A, n, s, e, k, v + 1, m);
 	}
-	
-	return shortest[s];
-	
+	assume false;
+	return m;
 }
