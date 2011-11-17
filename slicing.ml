da3a98843d85fbdfd3ee04585e61bdeb563f74b4
Le Ton Chanh <letonchanh@gmail.com>
2011-11-17 16:03:42 +0800

    interface for slicing

--- slicing.ml
+++ slicing.ml
@@ -1,10 +1,29 @@
 (* Slicing Module *)
 
+module type FORMULA_TYPE = 
+sig
+  type t
+end;;
+
 module type SLICE_TYPE =
 sig
+  type t
+  val sameSlice: t -> t -> bool
+  val isRelevant: t -> t -> bool
 end;;
 
 (* Slicing Framework *)
+module SlicingFramework = 
+  functor (Formula: FORMULA_TYPE) -> 
+  functor (Slice: SLICE_TYPE) -> 
+sig
+  open Slice
+  open Formula
+  type s = Slice.t
+  type f = Formula.t
+  val split: f -> s list   
+  val getCtr: s -> s list -> s list
+end;;
 
 (* Automatic Slicing *)
 module AutoSlicing = 
