556918bb59840d1cc9781e790f32b8b657536209
Le Ton Chanh <letonchanh@gmail.com>
2011-11-17 14:48:26 +0800

    new branch for slicing module

--- slicing.ml
+++ slicing.ml
@@ -1,1 +1,19 @@
-(* Slicing module *)
+(* Slicing Module *)
+
+module type SLICE_TYPE =
+sig
+end;;
+
+(* Slicing Framework *)
+
+(* Automatic Slicing *)
+module AutoSlicing = 
+struct
+
+end;;
+
+(* Annotated Slicing *)
+module AnnoSlicing = 
+struct
+end;;
+
