532e8f5f5636eecf96edaf8a5d5baa8fb670ba45
Chin Wei Ngan <chinwn@gmail.com>
2011-11-24 01:16:23 +0800

    x

---
+++ hoaegs/fact.c
@@ -0,0 +1,51 @@
+relation fact(int n, int f) == 
+	(n = 0 & f = 1 |
+	n >= 1 & exists(f1 : fact(n-1,f1) & f = n*f1)).
+
+relation facta(int n, int f).
+
+axiom true ==> facta(0,1).
+axiom n >= 0 & facta(n-1,f1) ==> facta(n,n*f1).
+
+int computefact(int n)
+	requires n >= 0
+    ensures facta(n,res);
+//ensures fact(n,res);
+{
+	if (n < 1)
+		return 1;
+	else
+      return n*computefact(n-1);
+}
+
+int fact2(int n)
+  requires n>=0
+  ensures facta(n,res);
+{
+  return factit(n,0,1);
+}
+
+int factit(int n, int m, int acc)
+    requires facta(m,acc) & m>=0 & n >= 0
+    ensures facta(m+n,res);
+{
+	if (n < 1)
+		return acc;
+	else
+      return factit(n-1,m+1,(m+1)*acc);
+}
+
+int fact_while(int n)
+    requires n>=0
+    ensures facta(n,res);
+{
+  int r = 1;
+  while (n>0) 
+    requires n>=0 
+    ensures (exists f0: facta(n,f0) & r'=r*f0 & n'=0);
+    {
+      r = n*r;
+      n = n-1;
+    }
+  return r;
+}
