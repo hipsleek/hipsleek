2d8ad5f3d0e19c6fc1c34d2de0f03209085ff1b4
anhoa <vuanhoa@comp.nus.edu.sg>
2011-11-28 20:14:51 +0800

    Added hoabinomial.c implementing efficient binomial coefficient computation.

---
+++ hoaegs/hoabinomial.c
@@ -0,0 +1,71 @@
+// C(n,k) = number of ways to choose k objects from n objects
+// Note: the definition below is applicable to all integers k
+// and n, not just non-negative ones.
+relation binom(int n, int k, int c).
+
+axiom n >= 0 ==> binom(n,0,1).
+
+axiom k > 0 ==> binom(0,k,0).
+
+axiom 0 < k & 0 < n & binom(n-1,k-1,c1) & binom(n-1,k,c2) ==> binom(n,k,c1+c2).
+
+axiom binom(n,k,c1) & binom(n,k,c2) ==> c1 = c2.
+
+//checkentail true |- binom(1,1,1).
+
+//checkentail true |- binom(0,0,1).
+
+//checkentail true |- binom(1,0,1).
+
+//checkentail n >= 0 |- binom(n,0,1).
+//proof by induction:
+//checkentail n = 0 |- binom(n,0,1).
+//checkentail n > 0 & forall(m : m < 0 | m >= n | binom(m,0,1)) |- binom(n,0,1).
+
+//checkentail n >= 0 |- binom(n,n,1). 
+//proof by induction:
+//checkentail n = 0 |- binom(n,n,1).
+//checkentail n > 0 & forall(m : m < 0 | m >= n | binom(m,m,1)) |- binom(n,n,1).
+
+//checkentail 0 < k < n & binom(n,k-1,c1) & c * k = c1 * (n-k+1) |- binom(n,k,c).
+//checkentail n = 2 & 0 < k < n & binom(n,k-1,c1) & c * k = c1 * (n-k+1) |- binom(n,k,c).
+//checkentail n > 2 & 0 < k < n & binom(n,k-1,c1) & c * k = c1 * (n-k+1) & forall(k1,c3,c4 : k1 <= 0 | k1 >= n-1 | !(binom(n-1,k1-1,c3)) | c4 * k1 != c3 * (n - 1 - k1 + 1) | binom(n-1,k1,c4)) |- binom(n,k,c).
+//checkentail c1 = d1 + d2 & c2 = d2 + d3 & d1 * k = d2 * (n - k) & d2 * (k-1) = d3 * (n - k + 1) |- c1 * k = c2 * (n - k + 1).
+
+
+//checkentail 1 <= k <= n & binom(n-1,k-1,c1) & c*k = n*c1 |- binom(n,k,c).
+//proof by induction on n
+//checkentail n = 2 & 0 < k < n & binom(n-1,k-1,c1) & c*k = n*c1 & c2*k = (n-k)*c1 |- binom(n,k,c) & binom(n-1,k,c2).
+//checkentail n > 1 & 1 <= k <= n & binom(n-1,k-1,c1) & c1*k = n*c & forall(n1 : n1 < 1 | n1 >= n | forall(k1,c2,c3 : k1 < 1 | k1 > n1 | !(binom(n1-1,k1-1,c2)) | c3*k1 != n1*c2 | binom(n1,k1,c3))) |- binom(n,k,c).
+
+axiom n >= 0 ==> binom(n,n,1).
+
+axiom 0 < k <= n & binom(n,k-1,c1) & binom(n,k,c) ==> c1*(n-k+1) = c*k.
+
+//axiom 0 < k <= n & binom(n,k-1,c1) & c1*(n-k+1) = c*k ==> binom(n,k,c).
+
+int compute_binom(int n, int k)
+	requires 0 <= k <= n
+	ensures binom(n,k,res);
+{
+	if (n==k || k==0) 
+		return 1;
+	else {
+		int c = compute_binom(n-1,k-1);
+		int d = compute_binom(n-1,k);
+		assert d' * k = c' * (n - k);
+		assume d' * k = c' * (n - k);
+		assert binom(n,k,c'+d');
+		assume binom(n,k,c'+d');
+
+		int r = idiv(n * c, k);
+		assert r' = c'+d';
+		assume r' = c'+d';
+
+		return r;
+	}
+}
+
+int idiv(int x, int y)
+	requires y != 0
+	ensures res * y = x;
