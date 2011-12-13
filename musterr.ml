d4c6985eaa681915bc580ea0fc018ea34448a75b
Le Quang Loc <lequangloc@gmail.com>
2011-12-13 16:45:47 +0800

    add musterr.ml

---
+++ musterr.ml
@@ -0,0 +1,6 @@
+(*
+ 1. this file provides interfaces and implementations for
+   - must/may errors
+2. IMPORTANT (AVOID REDUNDANT): before implement new method, please go through
+  interfaces and UNUSED module to check whether your need is there.
+*)
