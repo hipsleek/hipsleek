c3aef1e7ada163bdd5a77250684ce5eb7ca695e9
Chin Wei Ngan <chinwn@gmail.com>
2011-12-19 00:35:54 +0800

    x

--- infer/hip/test-new.sh
+++ infer/hip/test-new.sh
@@ -1,8 +1,13 @@
 ../../hip t1-i.ss > t1-i.n
 ../../hip t2-i.ss > t2-i.n
 ../../hip t3-i.ss > t3-i.n
+../../hip t4-i.ss > t4-i.n
+../../hip t5-i.ss > t5-i.n
+../../hip t6-i.ss > t6-i.n
 ../../hip t1.ss > t1.n
 ../../hip todo.ss > todo.n
 ../../hip bug1.ss > bug1.n 
+../../hip bug2.ss > bug2.n 
+../../hip bugt5.ss > bugt5.n 
 
 
