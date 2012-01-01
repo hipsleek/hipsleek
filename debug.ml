746e1c36276b708ed72972ea5e3a8ebb3845630d
Chin Wei Ngan <chinwn@gmail.com>
2012-01-01 14:45:35 +0800

    added lazy tracing for better performance

--- debug.ml
+++ debug.ml
@@ -27,9 +27,10 @@ let pprint msg (pos:loc) =
 	print tmp
 
 (* system development debugging *)
-let devel_print s = 
+let ho_print (pr:'a->string) (m:'a) : unit = 
   let d = Gen.StackTrace.is_same_dd_get () in
   if !devel_debug_on  || not(d==None) then 
+    let s = (pr m) in
     let msg = match d with 
       | None -> ("\n!!!" ^ s)
       | Some cid -> ("\n@"^(string_of_int cid)^"!"^ s) 
@@ -40,15 +41,50 @@ let devel_print s =
       (print_string msg; flush stdout)
   else ()
 
-let devel_pprint msg (pos:loc) = 
+(* system development debugging *)
+let devel_print s = 
+  ho_print (fun x -> x) s 
+(* let d = Gen.StackTrace.is_same_dd_get () in *)
+(*   if !devel_debug_on  || not(d==None) then  *)
+(*     let msg = match d with  *)
+(*       | None -> ("\n!!!" ^ s) *)
+(*       | Some cid -> ("\n@"^(string_of_int cid)^"!"^ s)  *)
+(*     in *)
+(*     if !log_devel_debug then  *)
+(*       Buffer.add_string debug_log msg *)
+(*     else *)
+(*       (print_string msg; flush stdout) *)
+(*   else () *)
+
+let prior_msg pos =
   let tmp = pos.start_pos.Lexing.pos_fname ^ ":" ^ (string_of_int pos.start_pos.Lexing.pos_lnum) ^ ": " ^ (string_of_int (pos.start_pos.Lexing.pos_cnum-pos.start_pos.Lexing.pos_bol)) ^ ": " in
   let tmp = if is_no_pos !entail_pos then tmp 
-  else (tmp^"[entail:"^(string_of_int !entail_pos.start_pos.Lexing.pos_lnum)^"]"^"[post:"^(string_of_int (post_pos#get).start_pos.Lexing.pos_lnum)^"]") in
-  let tmp = tmp^ msg in
-	devel_print tmp
+  else (tmp^"[entail:"^(string_of_int !entail_pos.start_pos.Lexing.pos_lnum)^"]"^"[post:"^(string_of_int (post_pos#get).start_pos.Lexing.pos_lnum)^"]") 
+  in tmp
 
-let trace_pprint msg (pos:loc) = 
-	devel_print (" "^msg)
+let devel_pprint (msg:string) (pos:loc) =
+	ho_print (fun m -> (prior_msg pos)^m) msg
+
+let devel_zprint msg (pos:loc) =
+	ho_print (fun m -> (prior_msg pos)^(Lazy.force m)) msg
+
+let trace_pprint (msg:string) (pos:loc) : unit = 
+	ho_print (fun a -> " "^a) msg
+
+
+let trace_hprint (pr:'a->string) (m:'a) (pos:loc) = 
+	ho_print (fun x -> " "^(pr x)) m
+
+let trace_zprint m (pos:loc) = 
+	ho_print (fun x -> Lazy.force x) m
+
+
+(* let devel_zprint msg (pos:loc) = *)
+(* 	lazy_print (prior_msg pos) msg *)
+
+(* let trace_zprint msg (pos:loc) =  *)
+(* 	lazy_print (fun () -> " ") msg *)
+
 
 let print_info prefix str (pos:loc) = 
   let tmp = "\n" ^ prefix ^ ":" ^ pos.start_pos.Lexing.pos_fname ^ ":" ^ (string_of_int pos.start_pos.Lexing.pos_lnum) ^": " ^ (string_of_int (pos.start_pos.Lexing.pos_cnum-pos.start_pos.Lexing.pos_bol)) ^": " ^ str ^ "\n" in
