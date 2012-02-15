672c04c6e5c54cb553d8a15ffcf18b22908b5f18
Le Ton Chanh <letonchanh@gmail.com>
2012-02-15 13:32:19 +0800

    update script

--- slice/examples/working_examples/asankhs_benchmarks/diff.sh
+++ slice/examples/working_examples/asankhs_benchmarks/diff.sh
@@ -1,9 +1,22 @@
 for (( i = 10; i <= 20; i++ ))
 do
-	grep Fail spaguetti-$i.res.eps | sed 's/Entail (\([0-9]*\)).*/\1/' > f.eps
-	grep Fail spaguetti-$i.res.slc.eps | sed 's/Entail (\([0-9]*\)).*/\1/' > f.slc.eps
-	echo "spaguetti-$i"
-	diff f.eps f.slc.eps
+	grep Fail spaguetti-$i.z3.eps | sed 's/Entail (\([0-9]*\)).*/\1/' > z3.eps
+	grep Fail spaguetti-$i.z3.slc.eps | sed 's/Entail (\([0-9]*\)).*/\1/' > z3.slc.eps
+
+	grep Fail spaguetti-$i.spass.eps | sed 's/Entail (\([0-9]*\)).*/\1/' > spass.eps
+	grep Fail spaguetti-$i.spass.slc.eps | sed 's/Entail (\([0-9]*\)).*/\1/' > spass.slc.eps
+
+	echo "[z3] spaguetti-$i"
+	diff z3.eps z3.slc.eps
+
+	echo "[spass] spaguetti-$i"
+	diff spass.eps spass.slc.eps
+
+	echo "[z3-spass][eps] spaguetti-$i"
+	diff z3.eps spass.eps
+
+	echo "[z3-spass][slc.eps] spaguetti-$i"
+	diff z3.slc.eps spass.slc.eps
 done
 
 
--- slice/examples/working_examples/asankhs_benchmarks/run.sh
+++ slice/examples/working_examples/asankhs_benchmarks/run.sh
@@ -1,8 +1,13 @@
 for (( i = 10; i <= 20; i++ ))
 do
-	echo "spaguetti-$i with eps"
-	time ../../../../sleek --ufdp -tp z3 spaguetti-$i.slk --eps --dis-imm > spaguetti-$i.res.eps
-	echo "spaguetti-$i with slicing + eps"
-	time ../../../../sleek --ufdp -tp z3 spaguetti-$i.slk --eps --enable-slicing --slc-opt-ineq --dis-imm > spaguetti-$i.res.slc.eps
+	echo "[z3] spaguetti-$i with eps"
+	time ../../../../sleek --ufdp -tp z3 spaguetti-$i.slk --eps --dis-imm > spaguetti-$i.z3.eps
+	echo "[z3] spaguetti-$i with slicing + eps"
+	time ../../../../sleek --ufdp -tp z3 spaguetti-$i.slk --eps --enable-slicing --slc-opt-ineq --dis-imm > spaguetti-$i.z3.slc.eps
+
+	echo "[spass] spaguetti-$i with eps"
+	time ../../../../sleek --ufdp -tp spass spaguetti-$i.slk --eps --dis-imm > spaguetti-$i.spass.eps
+	echo "[spass] spaguetti-$i with slicing + eps"
+	time ../../../../sleek --ufdp -tp spass spaguetti-$i.slk --eps --enable-slicing --slc-opt-ineq --dis-imm > spaguetti-$i.spass.slc.eps
 done
 	
