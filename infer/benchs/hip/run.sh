ff8faa1f2fb4e9ba123232721f38564a868300cd
minh-thai trinh <thaitm52@gmail.com>
2012-02-25 16:43:27 +0800

    x

--- infer/benchs/hip/run.sh
+++ infer/benchs/hip/run.sh
@@ -4,9 +4,9 @@
 ../../../hip sll_ms.ss > res/sll_ms.res
 ../../../hip sll_msb.ss -tp mona --eps > res/sll_msb.res
 ../../../hip sll_mso.ss --eps > res/sll_mso.res
-../../../hip complete_mshb.ss -tp mona --eps > res/complete_mshb.res
 ../../../hip complete_msh.ss --eps > res/complete_msh.res
 ../../../hip complete_mshb.ss -tp mona --eps > res/complete_mshb.res
+../../../hip dll_ms.ss > res/dll_ms.res
 ../../../hip dll_size.ss --eps > res/dll_size.res
 ../../../hip dll_msb.ss -tp mona > res/dll_msb.res
 ../../../hip heaps_m.ss --eps > res/heaps_m.res
