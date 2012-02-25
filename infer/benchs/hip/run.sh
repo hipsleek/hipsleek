e4c9f11d2fe06fb7213660cd42ad67f893178cab
minh-thai trinh <thaitm52@gmail.com>
2012-02-25 15:59:03 +0800

    x

--- infer/benchs/hip/run.sh
+++ infer/benchs/hip/run.sh
@@ -1,7 +1,7 @@
 ../../../hip ll_m.ss > res/ll_m.res
 ../../../hip ll_ms.ss --eps > res/ll_ms.res
 ../../../hip ll_msb.ss -tp mona > res/ll_msb.res
-../../../hip sll_ms.ss --eps > res/sll_ms.res
+../../../hip sll_ms.ss > res/sll_ms.res
 ../../../hip sll_msb.ss -tp mona --eps > res/sll_msb.res
 ../../../hip sll_mso.ss --eps > res/sll_mso.res
 ../../../hip complete_mshb.ss -tp mona --eps > res/complete_mshb.res
