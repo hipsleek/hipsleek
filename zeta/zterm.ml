197f71c81a10af1181ed4c50bed1e8b65d6efbe8
AnHoa <anhoa@loris7.ddns>
2012-03-13 21:35:17 +0800

    refactor

--- zeta/zterm.ml
+++ zeta/zterm.ml
@@ -13,24 +13,241 @@
 open Zsort
 open Zutils
 
+module type NumeralType =
+	sig
+		type t
+	end
+	
+module type SymbolType =
+	sig
+		type t
+		val to_string : t -> string
+		val equal : t -> t -> bool
+		val compare : t -> t -> int
+	end
+
 (**
- * Data structure for LOGICAL TERM
- *
- * Note: For the case FunApp:
- * 
- * If func = GFunc then the first
- * argument must be a Var term whose
- * name is the function/array name.
- * 
- * If func = Exists or Forall then 
- * the first argument term is the
- * formula and the rest are variables
- * under the quantification.
- *
- * TODO consider removing sort from var
- * such information is deducible from
- * the context
+ * Modules for functions and logical
+ * operators.
  *)
+module Func =
+	struct
+		type func =
+			| Add
+			| Sub
+			| Mul
+			| Div
+			| Mod
+			| Lt
+			| Le
+			| Gt
+			| Ge
+			| Eq
+			| Ne
+			| Top
+			| Bot
+			| And
+			| Or
+			| Neg
+			| Implies
+			| Iff
+			| Exists
+			| Forall
+			| GFunc
+		
+		let precedence_of f = match f with
+			| Implies | Iff -> 1
+			| Or -> 2
+			| And -> 3
+			| Exists | Forall -> 4
+			| Neg -> 5
+			| Lt | Le | Gt | Ge | Eq | Ne -> 7
+			| Add | Sub -> 8
+			| Mul | Div | Mod -> 9			
+			| Top | Bot -> 10
+			| GFunc -> 10
+
+		let string_of_func f = match f with
+			| Add -> "+" | Sub -> "-" | Mul -> "*" 
+			| Div -> "/" | Mod -> "mod"
+			| Lt -> "<" | Le -> "<=" | Gt -> ">"
+			| Ge -> ">=" | Eq -> "=" | Ne -> "!="
+			| Top -> "T" | Bot -> "F" | And -> " /\\ " 
+			| Or -> " \\/ " | Neg -> "~" | Implies -> "->" 
+			| Iff -> "<->" | Exists -> "E" | Forall -> "A"
+			| GFunc -> ""
+		
+		let latex_of_func f = match f with
+			| Add -> "+" | Sub -> "-" | Mul -> "\\times"
+			| Div -> "\\div" | Mod -> "\\bmod"
+			| Lt -> "<" | Le -> "\\leq" | Gt -> ">"
+			| Ge -> "\\geq" | Eq -> "=" | Ne -> "\\not="
+			| Top -> "\\top" | Bot -> "\\bot" | And -> "\\land"
+			| Or -> "\\lor" | Neg -> "\\neg"
+			| Implies -> "\\rightarrow" | Iff -> "\\leftrightarrow"
+			| Exists -> "\\exists" | Forall -> "\\forall"
+			| GFunc -> ""
+	
+		(**
+		 * Get the sort of the result of function application from the functor
+		 *)
+		let sort_of_func f = match f with
+			| Add | Sub | Mul | Div | Mod -> SInt
+			| Lt | Le | Gt | Ge | Eq | Ne | Top | Bot | And | Or | Neg | Implies | Iff | Exists | Forall -> SBool
+			| GFunc -> SWild 0
+		
+		(**
+		 * Get common sort of all arguments from the functor
+		 *)
+		let argsort_of_func f = match f with
+			| Add | Sub | Mul | Div | Mod | Lt | Le | Gt | Ge | Eq | Ne -> SInt
+			| Top | Bot | And | Or | Neg | Implies | Iff -> SBool
+			| Exists | Forall | GFunc -> SWild 0	
+	
+	
+	end
+
+module Term = 
+	functor (*(n : NumeralType)*) (s : SymbolType) ->
+	struct
+		(**
+		 * Data structure for LOGICAL TERM
+		 *
+		 * Note: For the case FunApp:
+		 * 
+		 * If func = GFunc then the first
+		 * argument must be a Var term whose
+		 * name is the function/array name.
+		 * 
+		 * If func = Exists or Forall then 
+		 * the first argument term is the
+		 * formula and the rest are variables
+		 * under the quantification.
+		 *
+		 * TODO consider removing sort from var
+		 * such information is deducible from
+		 * the context
+		 *)
+		type term =
+			| Num of int (* Numeral constant *)
+			| Var of s (* Variable symbols *)
+			| Fx of func * term list (* Function application *)
+		
+		
+		(* TERM CONSTRUCTORS *)
+
+		let set_term_sort s t = match t with
+			| Var (_,v) -> Var (s, v)
+			| FunApp (GFunc, x) -> 
+				let o, x = List.hd x, List.tl x in
+				let d = signature_of_smap (sort_of_term o) in
+				let o = Var (SMap (s :: List.tl d), name_of_var o) in
+					FunApp (GFunc, o :: x)
+			| _ -> t
+		
+		(*
+		(* current number of wild sorts *)
+		let var_count = ref 0
+		
+		let inc v = v := !v + 1; !v
+		*)
+		
+		let mkNum n = Num n
+		
+		let mkVar v = Var v
+		
+		let mkTop () = Fx (Func.Top, [])
+		
+		let mkBot () = Fx (Func.Bot, [])
+		
+		let mkFx f x = Fx (f, x)
+			(*match f with
+			| GFunc -> (* replace the sort for operator so that any query sort_of_term is safe! *)
+				let o, x = List.hd xs, List.tl xs in
+				let sf = sort_of_term o in (* assume that o has unique sort identifier ==> can be reused *)
+				let o = Var (SMap (sf :: (List.map sort_of_term x)), name_of_var o) in
+					FunApp (GFunc, o :: x)
+			| Exists | Forall -> FunApp (f, (set_term_sort SBool (List.hd xs)) :: (List.tl xs))
+			| _ -> let xf = argsort_of_func f in FunApp (f, List.map (set_term_sort xf) xs)*)
+		
+		let mkBinFx f x1 x2 = mkFx f [x1; x2]
+		
+		let mkUnaFx f x = mkFx f [x]
+		
+		(* TERM UTILITIES *)
+		
+		let rec get_all_vars t = match t with
+			| Num _ -> []
+			| Var _ -> [t]
+			| Fx (_, x) ->
+				List.flatten (List.map get_all_vars x)
+		
+		let rec get_all_fx t = match t with
+			| Num _ | Var _ -> []
+			| Fx (f, x) ->
+				let fapps = List.flatten (List.map get_all_fx x) in
+				match f with
+					| GFunc -> t :: fapps
+					| _ -> fapps
+		
+		let is_gfx t = match t with
+			| Fx (GFunc, _) -> true
+			| _ -> false
+			
+		let get_functor_name t = match t with
+			| Fx (GFunc, x) -> name_of_var (List.hd x)
+			| _ -> failwith "[functor_name] : expect a function application"
+		
+		let get_fun_app_arg t = match t with
+			| Fx (GFunc, x) -> List.tl x
+			| _ -> failwith "[get_fun_app_arg] : expect a function application"
+		
+		(**
+		 * Get all free variables in a term t (with duplicates)
+		 *)
+		let rec frv t = match t with
+			| Num _ -> []
+			| Var _ -> [t]
+			| Fx (f, x) ->
+				let vx = List.map frv x in
+				let vl = match f with
+					| Exists | Forall ->
+						let qvars = List.flatten (List.tl vx) in
+							GList.remove_all_eq eq_var (List.hd vx) qvars
+					| _ -> List.flatten vx in
+				GList.remove_dups_eq eq_var vl
+		
+		(**
+		 * Substitute simultaneously [v/vt]
+		 * for all pairs (v,vt) in stl where
+		 * v is the variable name to be 
+		 * substituted, vt is the term to
+		 * substitute it with.
+		 *)
+		let rec subst stl t = if stl = [] then t 
+			else match t with
+				| Num _ -> t
+				| Var (_, v) -> begin
+					try 
+						List.assoc v stl
+					with 
+						| Not_found -> t end
+				| FunApp (f, x) -> match f with
+					| Exists | Forall ->
+						let fml = List.hd x in
+						let qvars = List.tl x in
+						let qvarsnames = List.map name_of_var qvars in
+						let stl = GList.remove_all_eq (fun (v,_) q -> v = q) stl qvarsnames in
+							FunApp (f, (subst stl fml) :: qvars)
+					| _ -> (* Warning : potential dangerous substitution of functors *)
+						FunApp (f, List.map (subst stl) x)
+						
+		let filter_defined_symbols st vl =
+			List.filter (fun x -> not (Hashtbl.mem st (name_of_var x))) vl
+
+	end;;
+
+
 type term =
 	| Num of int (* Numeral constant *)
 	| Var of sort * string (* Variables/Function symbols *)
