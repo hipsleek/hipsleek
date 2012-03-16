a47e4d4e8ffc4b649c898219b3478630a7865d8c
Le Ton Chanh <letonchanh@gmail.com>
2012-03-16 23:06:01 +0800

    x

--- slice/examples/working_examples/asankhs_benchmarks/run-stat.sh
+++ slice/examples/working_examples/asankhs_benchmarks/run-stat.sh
@@ -1,4 +1,5 @@
 TIMEOUT=600
+SLEEK_TIMEOUT=2.0
 
 echo "**********************" >> time.stat
 date >> time.stat
@@ -12,22 +13,22 @@ kill_proc () {
 	killall -v sleek
 }
 
-for (( i = 10; i <= 20; i++ ))
+for (( i = 10; i <= 10; i++ ))
 do
 	echo "[$1][.nslc] spaguetti-$i"
 	echo "[$1][.nslc] spaguetti-$i" >> time.stat
 	kill_proc
-	(time ../../../../sleek --ufdp -tp $1 spaguetti-$i.slk --eps --dis-slicing --dis-imm --ep-stat $2) 1> spaguetti-$i.$1.nslc.stat 2>> time.stat
+	(time ../../../../sleek --ufdp -tp $1 spaguetti-$i.slk --eps --dis-slicing --ep-stat --dis-provers-timeout --sleek-timeout $SLEEK_TIMEOUT $2) 1> spaguetti-$i.$1.nslc.stat 2>> time.stat
 
 	echo "[$1][.slc] spaguetti-$i"
 	echo "[$1][.slc] spaguetti-$i" >> time.stat
 	kill_proc
-	(time ../../../../sleek --ufdp -tp $1 spaguetti-$i.slk --eps --enable-slicing --dis-imm --ep-stat $2) 1> spaguetti-$i.$1.slc.stat 2>> time.stat
+	(time ../../../../sleek --ufdp -tp $1 spaguetti-$i.slk --eps --enable-slicing --ep-stat --dis-provers-timeout --sleek-timeout $SLEEK_TIMEOUT $2) 1> spaguetti-$i.$1.slc.stat 2>> time.stat
 
 	echo "[$1][.ineq] spaguetti-$i"
 	echo "[$1][.ineq] spaguetti-$i" >> time.stat
 	kill_proc
-	(time ../../../../sleek --ufdp -tp $1 spaguetti-$i.slk --eps --enable-slicing --slc-opt-ineq --dis-imm --ep-stat $2) 1> spaguetti-$i.$1.ineq.stat 2>> time.stat
+	(time ../../../../sleek --ufdp -tp $1 spaguetti-$i.slk --eps --enable-slicing --slc-opt-ineq --ep-stat --dis-provers-timeout --sleek-timeout $SLEEK_TIMEOUT $2) 1> spaguetti-$i.$1.ineq.stat 2>> time.stat
 
 done
 
