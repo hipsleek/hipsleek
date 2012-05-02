a51b4930038480c00868fe61e27bbd3297e2d3ec
Le Quang Loc <lequangloc@gmail.com>
2012-05-02 16:11:46 +0800

    x

---
+++ cparser/global1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+//run: ../cil/cil-1.4.0/obj/x86_LINUX/cilly.asm.exe global1.c --dohip --out  out/global1.c.i
+int x;
+
+int main() {
+  x = x+ 1;
+  printf("Hello world\n");
+  return 0;
+}
