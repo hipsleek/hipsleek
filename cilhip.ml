c6ffb9e9cc6420e640d9761deff56bed787c9332
Le Quang Loc <lequangloc@gmail.com>
2012-05-03 23:06:09 +0800

    x

--- cilhip.ml
+++ cilhip.ml
@@ -3,11 +3,12 @@
 ocamlopt -c -I cil-1.3.7/obj/x86_LINUX/ main.ml
 ocamlopt -ccopt -Lcil-1.3.7/obj/x86_LINUX/ -o main unix.cmxa str.cmxa cil-1.3.7/obj/x86_LINUX/cil.cmxa main.cmx
 ./main > output.c
+*)
 
 let _ = 
   let cil = Frontc.parse "input.c" () in
   let _ = Rmtmps.removeUnusedTemps cil in
   Cil.lineDirectiveStyle := None;
   Cprint.printLn := false;
-  Cil.dumpFile Cil.defaultCilPrinter stdout "output.cil" cil 
-*)
+  Cil.dumpFile (new Cil.defaultCilPrinterClass) stdout "output.cil" cil 
+
