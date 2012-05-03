28dded9f0916f488ad475264262a9f57807a809b
Le Quang Loc <lequangloc@gmail.com>
2012-05-04 00:38:26 +0800

    x

--- cilhip.sh
+++
@@ -1,2 +0,0 @@
-ocamlopt -c -I cil/cil-1.4.0/obj/x86_LINUX/ cilhip.ml
-ocamlopt -ccopt -Lcil/cil-1.4.0/obj/x86_LINUX/ -o cilhip unix.cmxa str.cmxa cil/cil-1.4.0/obj/x86_LINUX/cil.cmxa cilhip.cmx
--- cparser/cilhip.ml
+++ cparser/cilhip.ml
@@ -5,6 +5,8 @@ ocamlopt -ccopt -Lcil-1.3.7/obj/x86_LINUX/ -o main unix.cmxa str.cmxa cil-1.3.7/
 ./main > output.c
 *)
 
+open Frontc
+
 let _ = 
   let cil = Frontc.parse "input.c" () in
   let _ = Rmtmps.removeUnusedTemps cil in
