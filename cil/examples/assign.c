5546e9b8059a707df509e774528c6a49cc1f1dc9
Ta Quang Trung <taquangtrungvn@gmail.com>
2012-05-11 15:31:52 +0800

    add examples

---
+++ cil/examples/assign.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+//run: obj/x86_LINUX/cilly.byte.exe --noPrintLn examples/input.c --out examples/output.c
+
+int main() {
+  int x;
+  x++;
+  x--;
+  x += 2;
+  x -= 2;
+  x *= 2;
+  x /= 2;
+  return 0;
+}
