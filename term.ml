11cfaa015663de48555891d1e28a70a5a6cffd13
Ta Quang Trung <taquangtrungvn@gmail.com>
2012-05-22 16:49:12 +0800

    implement sequence decrease (10)

--- term.ml
+++ term.ml
@@ -376,7 +376,7 @@ let strip_termvar_mix_formula (mf: MCP.mix_formula) =
   (termvars, CP.join_conjunctions other_p)
 
 (* Termination: The boundedness checking for HIP has been done before *)  
-let check_term_lexvar_measures estate lhs_p xpure_lhs_h0 xpure_lhs_h1 rhs_p src_lv dst_lv t_ann_trans pos =
+let check_term_lexvar_measures_x estate lhs_p xpure_lhs_h0 xpure_lhs_h1 rhs_p src_lv dst_lv t_ann_trans pos =
   let ans  = norm_term_measures_by_length src_lv dst_lv in
   let l_pos = post_pos # get in
   let l_pos = if l_pos == no_pos then pos else l_pos in (* Update pos for SLEEK output *)
@@ -519,7 +519,7 @@ let check_term_lexvar_measures estate lhs_p xpure_lhs_h0 xpure_lhs_h1 rhs_p src_
             (add_str "rhs" pr) 
             (add_str "rank_fml" (pr_option pr1)) 
             (es, lhs, rhs, rank_fml))  
-      (fun _ _ _ _ _ -> check_term_lexvar_measures estate lhs_p xpure_lhs_h0 xpure_lhs_h1 rhs_p src_lv dst_lv t_ann_trans pos) 
+      (fun _ _ _ _ _ -> check_term_lexvar_measures_x estate lhs_p xpure_lhs_h0 xpure_lhs_h1 rhs_p src_lv dst_lv t_ann_trans pos) 
         estate lhs_p rhs_p src_lv dst_lv
 
 (* To handle LexVar formula *)
@@ -604,11 +604,88 @@ let check_term_lexvar_rhs estate lhs_p xpure_lhs_h0 xpure_lhs_h1 rhs_p pos =
     (fun (es, lhs, rhs, _) -> pr_triple pr2 pr pr (es, lhs, rhs))  
       (fun _ _ _ -> check_term_lexvar_rhs estate lhs_p xpure_lhs_h0 xpure_lhs_h1 rhs_p pos) estate lhs_p rhs_p
 
-(* Termination: check sequence var *)  
-let check_term_seqvar_measures_x estate lhs_p xpure_lhs_h0 xpure_lhs_h1 rhs_p (trans : term_trans)  =
+let check_term_seqvar_decrease_measures estate lhs_p xpure_lhs_h0 xpure_lhs_h1 rhs_p (trans : term_trans option) =
+  let (seq_src, seq_dst) = match trans with
+                           | Some (CP.SeqVar seq1, CP.SeqVar seq2) -> seq1, seq2
+                           | _ -> raise SeqVar_Not_found in
+  let elm_src = seq_src.CP.seq_element in
+  let fp_src = seq_src.CP.seq_fix_point in
+  let lb_src = List.hd seq_src.CP.seq_bounds in 
+  let elm_dst = seq_dst.CP.seq_element in
+  let fp_dst = seq_dst.CP.seq_fix_point in
+  let lb_dst = List.hd seq_dst.CP.seq_bounds in
+  let pos_dst = seq_dst.CP.seq_loc in
+  let l_pos = post_pos # get in
+  let l_pos = if l_pos == no_pos then pos_dst else l_pos in (* Update pos for SLEEK output *)
+  let term_pos = (l_pos, proving_loc # get) in
+  (* constraint 1: fp_src = fp_dst *)
+  let cst1 = CP.mkPure (CP.mkEq fp_src fp_dst no_pos) in 
+  (* constraint 2: lb_src = lb_dst *)
+  let cst2 = CP.mkPure (CP.mkEq lb_src lb_dst no_pos) in
+  (* elm_src = fp_src => elm_dst = fp_dst *)
+  let tmp1 = CP.mkNot_s (CP.mkPure (CP.mkEq elm_src fp_src no_pos)) in
+  let tmp2 = CP.mkPure (CP.mkEq elm_dst fp_dst no_pos) in
+  let cst3 = CP.mkOr tmp1 tmp2 None no_pos in
+  (* elm_src > elm_dst *)
+  let cst4 = CP.mkPure (CP.mkGt elm_src elm_dst no_pos) in
+  (* elm_src > lb_src *)
+  let cst5 = CP.mkPure (CP.mkGt elm_src lb_src no_pos) in
+  (* lb_src > fp_src *)
+  let cst6 = CP.mkPure (CP.mkGt lb_src fp_src no_pos) in
+  (* (elm_dst - fp_dst) < K * (elm_src - fp_src) *)
+  let k = CP.mkFConst 0.9999999999999999999999999999999999999999999999999999999999999 no_pos in
+  let tmp1 = CP.mkMult k (CP.mkSubtract elm_src fp_src no_pos) no_pos in
+  let tmp2 = CP.mkSubtract elm_dst fp_dst no_pos in
+  let cst7 = CP.mkPure (CP.mkGt tmp1 tmp2 no_pos) in
+  (* collect constraints *)
+  let cst_lst = [cst2; cst3; cst4; cst5; cst6; cst7] in
+  let constrains = List.fold_left (fun a b -> CP.mkAnd a b no_pos) cst1 cst_lst in
+  let lhs = MCP.pure_of_mix (MCP.merge_mems lhs_p xpure_lhs_h1 true) in
+  let entail_res, _, _ = TP.imply lhs constrains "" false None in
+  let orig_ante = estate.es_formula in 
+  let term_measures, term_res, term_err_msg, rank_formula =
+    if entail_res then 
+      Some (CP.SeqVar seq_src), 
+      (term_pos, trans, Some orig_ante, Term_S (Decreasing_Measure trans)),
+      None, 
+      None
+    else
+      Some (CP.SeqVar {seq_src with CP.seq_ann = Fail TermErr_May}),
+      (term_pos, trans, Some orig_ante, MayTerm_S (Not_Decreasing_Measure trans)),
+      Some (string_of_term_res (term_pos, trans, None, MayTerm_S (Not_Decreasing_Measure trans))),
+      None
+  in 
+  let term_err = match estate.es_term_err with
+    | None -> term_err_msg
+    | Some _ -> estate.es_term_err 
+  in
+  let n_estate = { estate with
+    es_var_measures = term_measures;
+    (* es_var_stack = term_stack; *)
+    es_term_err = term_err
+  } in
+  add_term_res_stk term_res;
+  (n_estate, lhs_p, rhs_p, rank_formula)
+
+let check_term_seqvar_oscillate_measures estate lhs_p xpure_lhs_h0 xpure_lhs_h1 rhs_p (trans : term_trans option) =
   (estate, lhs_p, rhs_p, None)
 
-let check_term_seqvar_measures estate lhs_p xpure_lhs_h0 xpure_lhs_h1 rhs_p (trans : term_trans)  =
+(* Termination: check sequence var *)  
+let check_term_seqvar_measures_x estate lhs_p xpure_lhs_h0 xpure_lhs_h1 rhs_p (trans : term_trans option)  =
+  (* (estate, lhs_p, rhs_p, None) *)
+  let (seq_src, seq_dst) = match trans with
+                           | Some (CP.SeqVar seq1, CP.SeqVar seq2) -> seq1, seq2
+                           | _ -> raise SeqVar_Not_found in
+  let vari_src = seq_src.CP.seq_variation in
+  let vari_dst = seq_dst.CP.seq_variation in
+  if (vari_src = vari_dst) && (vari_src = CP.SeqDec) then 
+    check_term_seqvar_decrease_measures estate lhs_p xpure_lhs_h0 xpure_lhs_h1 rhs_p trans
+  else if (vari_src = vari_dst) && (vari_src = CP.SeqOsc) then
+    check_term_seqvar_oscillate_measures estate lhs_p xpure_lhs_h0 xpure_lhs_h1 rhs_p trans
+  else
+    report_error no_pos "[term.ml] Invalid variation of sequences."
+
+let check_term_seqvar_measures estate lhs_p xpure_lhs_h0 xpure_lhs_h1 rhs_p (trans : term_trans option) =
   let pr = !print_mix_formula in
   let pr1 = !CP.print_formula in
   let pr2 = !print_entail_state in
@@ -654,7 +731,7 @@ let check_term_seqvar_rhs_x estate lhs_p xpure_lhs_h0 xpure_lhs_h1 rhs_p pos =
       | (Term, Term)
       | (Fail TermErr_May, Term) ->
           (* Check wellfoundedness of the transition *)
-          check_term_seqvar_measures estate lhs_p xpure_lhs_h0 xpure_lhs_h1 rhs_p trans
+          check_term_seqvar_measures estate lhs_p xpure_lhs_h0 xpure_lhs_h1 rhs_p trans_opt
       | (Term, _)
       | (Fail TermErr_May, _) ->
           let term_res = (term_pos, trans_opt, Some estate.es_formula,
