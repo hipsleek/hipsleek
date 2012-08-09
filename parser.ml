9734e5d022a20a93fec34fb4bd4a82ef9329b1a5
Le Quang Loc <lequangloc@gmail.com>
2012-08-09 17:12:33 +0800

    fix array bugs at revision 4970

--- parser.ml
+++ parser.ml
@@ -223,7 +223,22 @@ let cexp_to_pure2 fct f1 f2 = match (f1,f2) with
                                     | _ -> (
                                         let typ1 = P.typ_of_exp f1 in 
                                         let typ2 = P.typ_of_exp f2 in
-                                        if (typ1 = typ2) || (typ1 == UNK) || (typ2 == UNK) then 
+                                         (* let _ = print_endline ("typ1:" ^ (string_of_typ typ1 )) in *)
+                                        (* let _ = print_endline ("typ2:" ^ (string_of_typ typ2 )) in *)
+                                         let arr_typ_check typ1 typ2 =
+                                         ( match typ1 with
+                                            | Array (t1,_) -> if t1== UNK || t1 == typ2 then true else
+                                                  ( match typ2 with
+                                                    | Array (t2,_) -> if t2== UNK || t1==t2 then true else false
+                                                    | _ -> false
+                                                  )
+                                            | _ -> ( match typ2 with
+                                                  | Array (t,_) -> if t== UNK then true else false
+                                                  | _ -> false
+                                            )
+                                         )
+                                        in
+                                        if (typ1 = typ2) || (typ1 == UNK) || (typ2 == UNK) || (arr_typ_check typ1 typ2) then 
                                           Pure_f (P.BForm(((fct f1 f2), None), None))
                                         else
                                           report_error (get_pos 1) "with 2 convert expected the same cexp types, found different types"
@@ -301,10 +316,13 @@ let peek_try =
  let peek_invocation = 
  SHGram.Entry.of_parser "peek_invocation" 
      (fun strm ->
-       match Stream.npeek 2 strm with
-          | [_; OPAREN,_] -> ()
+         match Stream.npeek 5 strm with
+           | [_; OPAREN,_;_;_;_] -> ()
+       (* match Stream.npeek 2 strm with *)
+       (*    | [_; OPAREN,_] -> () *)
           (* | [_; OBRACE,_] -> () *)
-          | [_; OSQUARE,_] -> ()
+           | [_; OSQUARE,_; _; CSQUARE, _ ; OPAREN,_] -> ()
+          (* | [_; OSQUARE,_] -> () *)
           | _ -> raise Stream.Failure)
 		  
  let peek_member_name = 
@@ -1763,7 +1781,8 @@ bind_statement:
 java_statement: [[ `JAVA s -> Java { exp_java_code = s;exp_java_pos = get_pos_camlp4 _loc 1 }]];
 
 expression_statement: [[(* t=statement_expression -> t *)
-        t= invocation_expression -> t
+        (* t= invocation_expression -> t *)
+        peek_invocation; t= invocation_expression -> t
       | t= object_creation_expression -> t
       | t= post_increment_expression -> t
       | t= post_decrement_expression -> t
@@ -2033,13 +2052,13 @@ cast_expression:
              exp_cast_pos = get_pos_camlp4 _loc 1 }]];
 
 invocation_expression:
- [[ peek_invocation; qi=qualified_identifier; `OPAREN; oal=opt_argument_list; `CPAREN ->
+ [[ (* peek_invocation; *) qi=qualified_identifier; `OPAREN; oal=opt_argument_list; `CPAREN ->
 	  CallRecv { exp_call_recv_receiver = fst qi;
                exp_call_recv_method = snd qi;
                exp_call_recv_arguments = oal;
                exp_call_recv_path_id = None;
                exp_call_recv_pos = get_pos_camlp4 _loc 1 }
-  | peek_invocation; `IDENTIFIER id; l = opt_lock_info ; `OPAREN; oal=opt_argument_list; `CPAREN ->
+  | (* peek_invocation; *) `IDENTIFIER id; l = opt_lock_info ; `OPAREN; oal=opt_argument_list; `CPAREN ->
     CallNRecv { exp_call_nrecv_method = id;
                 exp_call_nrecv_lock = l;
                 exp_call_nrecv_arguments = oal;
@@ -2101,7 +2120,8 @@ primary_expression_no_array_no_parenthesis :
            exp_member_fields = [id];
            exp_member_path_id = None ;
            exp_member_pos = get_pos_camlp4 _loc 3 }
-  | t = invocation_expression -> t
+  (* | t = invocation_expression -> t *)
+  | peek_invocation; t = invocation_expression -> t
   | t = new_expression -> t
   | `THIS _ -> This{exp_this_pos = get_pos_camlp4 _loc 1} 
   ]
