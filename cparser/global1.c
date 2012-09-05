f307ee6689a32464846a70104b11cbecf7c5d79d
Ta Quang Trung <taquangtrungvn@gmail.com>
2012-09-05 14:36:08 +0800

    x

--- cparser/global1.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-//run: ../cil/cil-1.4.0/obj/x86_LINUX/cilly.asm.exe global1.c --noPrintLn --dohip --out  out/global1.c.i
-int x;
-
-int main() {
-  x = x+ 1;
-  printf("Hello world\n");
-  return 0;
-}
