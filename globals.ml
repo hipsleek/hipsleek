67411d780acba6e7f5163fac350a6afd6f538ceb
Chin Wei Ngan <chinwn@gmail.com>
2012-11-27 11:39:46 +0800

    x

--- globals.ml
+++ globals.ml
@@ -848,32 +848,6 @@ let fresh_strict_branch_point_id (s:string) : control_path_id_strict = (fresh_fo
 
 let eq_formula_label (l1:formula_label) (l2:formula_label) : bool = fst(l1)=fst(l2)
 
-(* let tmp_files_path = ref "" *)
-
-(* (\*path for the temporary files used by the prover. If you change this path here it is  *)
-(*   mandatory to also change the value of TMP_FILES_PATH in Makefile accordingly to the changes made here*\) *)
-(* let set_tmp_files_path () = 	 *)
-(* 	begin *)
-(*       (try *)
-(* 		ignore (Unix.mkdir ("/tmp/" ^ Unix.getlogin()) 0o766;)		  *)
-(*       with *)
-(* 		Unix.Unix_error (_, _, _) -> (); ); *)
-(* 	  (try *)
-(* 		ignore (Unix.chmod ("/tmp/" ^ Unix.getlogin()) 0o766;)		  *)
-(*       with *)
-(* 		Unix.Unix_error (_, _, _) -> (); ); *)
-(*       (try *)
-(* 		ignore (Unix.mkdir ("/tmp/" ^ Unix.getlogin() ^ "/prover_tmp_files/") 0o766)  *)
-(*       with *)
-(* 		Unix.Unix_error (_, _, _) -> ();); *)
-(* 	  (try *)
-(* 		ignore (Unix.chmod ("/tmp/" ^ Unix.getlogin() ^ "/prover_tmp_files/") 0o766;)		  *)
-(*       with *)
-(* 		Unix.Unix_error (_, _, _) -> ();); *)
-(* 	tmp_files_path := ("/tmp/" ^ Unix.getlogin() ^ "/prover_tmp_files/") *)
-(* 	end *)
-
-
 let fresh_int () =
   seq_number := !seq_number + 1;
   !seq_number
@@ -1007,41 +981,6 @@ let bin_to_list (fn : 'a -> (string * ('a list)) option)
     | None -> "", [t]
     | Some (op, _) -> op,(bin_op_to_list op fn t)
 
-(* (\*type of process used for communicating with the prover*\) *)
-(* type prover_process_t = {name:string; pid: int; inchannel: in_channel; outchannel: out_channel; errchannel: in_channel } *)
-
-(* (\*methods that need to be defined in order to use a prover incrementally - if the prover provides this functionality*\) *)
-(* class type ['a] incremMethodsType = object *)
-(*   val process: prover_process_t option ref *)
-(*   method start_p: unit -> prover_process_t *)
-(*   method stop_p:  prover_process_t -> unit *)
-(*   method push: prover_process_t -> unit *)
-(*   method pop: prover_process_t -> unit *)
-(*   method popto: prover_process_t -> int -> unit *)
-(*   method imply: (prover_process_t option * bool) option -> 'a -> 'a -> string -> bool *)
-(*   method set_process: prover_process_t -> unit *)
-(*   method get_process: unit -> prover_process_t option *)
-(*   (\* method add_to_context: 'a -> unit *\) *)
-(* end *)
-
-(* (\* An Hoa : option to print proof *\) *)
-(* let print_proof = ref false *)
-
-(* (\* Create a quoted version of a string, for example, hello --> "hello" *\) *)
-(* let strquote s = "\"" ^ s ^ "\"" *)
-
-
-(* let open_log_out s =  *)
-(*  (try *)
-(* 	Unix.mkdir "logs" 0o750 *)
-(*  with _ -> ()); *)
-(*  open_out ("logs/"^s) *)
-
-(* let norm_file_name str = *)
-(* 	for i = 0 to (String.length str) - 1 do *)
-(* 		if str.[i] = '.' || str.[i] = '/' then str.[i] <- '_' *)
-(* 	done; *)
-(* 	str *)
 
 (* An Hoa : option to print proof *)
 let print_proof = ref false
