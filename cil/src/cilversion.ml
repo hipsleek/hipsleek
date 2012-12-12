19e1e2cbb337aba8578acac1bde4d2e6b5fedd4b
Ta Quang Trung <taquangtrungvn@gmail.com>
2012-12-12 13:28:01 +0800

    x

--- cil/src/cilversion.ml
+++
@@ -1,6 +0,0 @@
-(* obj/x86_LINUX/cilversion.ml.  Generated from cilversion.ml.in by configure. *)
-
-let cilVersionMajor = 1
-let cilVersionMinor = 5
-let cilVersionRev   = 1
-let cilVersion      = "1.5.1"
