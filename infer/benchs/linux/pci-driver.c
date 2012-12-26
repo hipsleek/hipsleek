3339432a72921515a3aeea250ad6a3bad048b878
trinhmt <devnull@localhost>
2012-12-26 12:57:39 +0800

    x

--- infer/benchs/linux/pci-driver.c
+++ infer/benchs/linux/pci-driver.c
@@ -76,6 +76,11 @@ static inline void list_del(struct list_head *entry)
     entry->prev = NULL;
 }
 
+static inline void INIT_LIST_HEAD(struct list_head *list)
+{
+    list->next = list;
+    list->prev = list;
+}
 /****************************************************************************/
 
 struct pci_device_id {
@@ -96,25 +101,138 @@ struct pci_dynids {
 
 struct bus_type {
     const char *name;
-    //  int (*probe)(struct device *dev);
-    //  int (*remove)(struct device *dev);
+
+//  struct bus_attribute *bus_attrs;
+//  struct device_attribute *dev_attrs;
+//  struct driver_attribute *drv_attrs;
+//
+//  int (*match)(struct device *dev, struct device_driver *drv);
+//  int (*uevent)(struct device *dev, struct kobj_uevent_env *env);
+//  int (*probe)(struct device *dev);
+//  int (*remove)(struct device *dev);
+//  void (*shutdown)(struct device *dev);
+//
+//  int (*suspend)(struct device *dev, pm_message_t state);
+//  int (*resume)(struct device *dev);
+//
+//  const struct dev_pm_ops *pm;
+//
+//  struct bus_type_private *p;
 };
 
 struct device_driver {
     const char *name;
     struct bus_type *bus;
-    //  int (*probe)(struct device *dev);
-    //  int (*remove)(struct device *dev);
+
+    struct module *owner;
+    const char *mod_name;
+
+//  bool suppress_bind_attrs;
+//  int (*probe) (struct device *dev);
+//  int (*remove) (struct device *dev);
+//  void (*shutdown) (struct device *dev);
+//  int (*suspend) (struct device *dev, pm_message_t state);
+//  int (*resume) (struct device *dev);
+//  const struct attribute_group **groups;
+//  const struct dev_pm_ops *pm;
+//  struct driver_private *p;
 };
 
 struct pci_driver {
     struct list_head node;
     char *name;
     const struct pci_device_id *id_table;
-    //  int (*probe)(struct pci_dev *dev, const struct pci_device_id *id);
-    //  void (*remove)(struct pci_dev *dev);
+//  int (*probe) (struct pci_dev *dev, const struct pci_device_id *id);
+//  void (*remove) (struct pci_dev *dev);
+//  int (*suspend) (struct pci_dev *dev, pm_message_t state);
+//  int (*suspend_late) (struct pci_dev *dev, pm_message_t state);
+//  int (*resume_early) (struct pci_dev *dev);
+//  int (*resume) (struct pci_dev *dev);
+//  void (*shutdown) (struct pci_dev *dev);
+//  struct pci_error_handlers *err_handler;
     struct device_driver driver;
     struct pci_dynids dynids;
+};
+
+struct pci_dev {
+//  struct list_head bus_list;
+//  struct pci_bus *bus;
+//  struct pci_bus *subordinate;
+//  void *sysdata;
+//  struct proc_dir_entry *procent;
+//  struct pci_slot *slot;
+//  unsigned int devfn;
+    unsigned short vendor;
+    unsigned short device;
+    unsigned short subsystem_vendor;
+    unsigned short subsystem_device;
+    unsigned int class;
+//  u8 revision;
+//  u8 hdr_type;
+//  u8 pcie_cap;
+//  u8 pcie_type;
+//  u8 rom_base_reg;
+//  u8 pin;
+//  struct pci_driver *driver;
+//  u64 dma_mask;
+//  struct device_dma_parameters dma_parms;
+//  pci_power_t current_state;
+//  int pm_cap;
+//  unsigned int pme_support:5;
+//  unsigned int pme_interrupt:1;
+//  unsigned int d1_support:1;
+//  unsigned int d2_support:1;
+//  unsigned int no_d1d2:1;
+//  unsigned int wakeup_prepared:1;
+//  unsigned int d3_delay;
+//  pci_channel_state_t error_state;
+//  struct device dev;
+//  int cfg_size;
+//  unsigned int irq;
+//  struct resource resource[DEVICE_COUNT_RESOURCE];
+//  resource_size_t fw_addr[DEVICE_COUNT_RESOURCE];
+//  unsigned int transparent:1;
+//  unsigned int multifunction:1;
+//  unsigned int is_added:1;
+//  unsigned int is_busmaster:1;
+//  unsigned int no_msi:1;
+//  unsigned int block_ucfg_access:1;
+//  unsigned int broken_parity_status:1;
+//  unsigned int irq_reroute_variant:2;
+//  unsigned int msi_enabled:1;
+//  unsigned int msix_enabled:1;
+//  unsigned int ari_enabled:1;
+//  unsigned int is_managed:1;
+//  unsigned int is_pcie:1;
+//  unsigned int needs_freset:1;
+//  unsigned int state_saved:1;
+//  unsigned int is_physfn:1;
+//  unsigned int is_virtfn:1;
+//  unsigned int reset_fn:1;
+//  unsigned int is_hotplug_bridge:1;
+//  unsigned int __aer_firmware_first_valid:1;
+//  unsigned int __aer_firmware_first:1;
+//  pci_dev_flags_t dev_flags;
+//  atomic_t enable_cnt;
+//  u32 saved_config_space[16];
+//  struct hlist_head saved_cap_space;
+//  struct bin_attribute *rom_attr;
+//  int rom_attr_enabled;
+//  struct bin_attribute *res_attr[DEVICE_COUNT_RESOURCE];
+//  struct bin_attribute *res_attr_wc[DEVICE_COUNT_RESOURCE];
+//  struct pci_vpd *vpd;
+};
+
+struct bus_type pci_bus_type = {
+    .name       = "pci",
+//  .match      = pci_bus_match,
+//  .uevent     = pci_uevent,
+//  .probe      = pci_device_probe,
+//  .remove     = pci_device_remove,
+//  .shutdown   = pci_device_shutdown,
+//  .dev_attrs  = pci_dev_attrs,
+//  .bus_attrs  = pci_bus_attrs,
+//  .pm     = PCI_PM_OPS_PTR,
 };
 
 /****************************************************************************/
@@ -124,11 +242,8 @@ struct pci_driver {
  * @drv: driver.
  */
 struct device_driver *get_driver(struct device_driver *drv) {
-    if (drv) {
-        struct device_driver *a;
-        return a;
-    }
-    return NULL;
+    struct device_driver *a;
+    return a;
 }
 /**
  * put_driver - decrement driver's refcount.
@@ -151,6 +266,35 @@ int driver_attach(struct device_driver *drv) {
     return 0;
 }
 
+void prefetch(void *x)
+{
+    return;
+}
+
+/**
+ * driver_register - register driver with bus
+ * @drv: driver to register
+ *
+ * We pass off most of the work to the bus_add_driver() call,
+ * since most of the things we have to do deal with the bus
+ * structures.
+ */
+int driver_register(struct device_driver *drv )
+{
+    int a;
+    return a;
+}
+
+/**
+ * driver_unregister - remove driver from system.
+ * @drv: driver.
+ *
+ * Again, we pass off most of the work to the bus-level call.
+ */
+void driver_unregister(struct device_driver *drv)
+{
+ return;
+}
 /****************************************************************************/
 
 /**
@@ -264,6 +408,182 @@ static int store_remove_id(struct device_driver *driver, const char *buf, int co
     if (retval)
         return retval;
     return count;
+}
+
+/**
+ * pci_match_one_device - Tell if a PCI device structure has a matching
+ *                        PCI device id structure
+ * @id: single PCI device id structure to match
+ * @dev: the PCI device structure to match against
+ *
+ * Returns the matching pci_device_id structure or %NULL if there is no match.
+ */
+static inline const struct pci_device_id *
+pci_match_one_device(const struct pci_device_id *id, const struct pci_dev *dev)
+{
+    if ((id->vendor == (~0) || id->vendor == dev->vendor) &&
+        (id->device == (~0) || id->device == dev->device) &&
+        (id->subvendor == (~0) || id->subvendor == dev->subsystem_vendor) &&
+        (id->subdevice == (~0) || id->subdevice == dev->subsystem_device) &&
+        !((id->class ^ dev->class) & id->class_mask))
+        return id;
+    return NULL;
+}
+
+/**
+ * pci_match_id - See if a pci device matches a given pci_id table
+ * @ids: array of PCI device id structures to search in
+ * @dev: the PCI device structure to match against.
+ *
+ * Used by a driver to check whether a PCI device present in the
+ * system is in its list of supported devices.  Returns the matching
+ * pci_device_id structure or %NULL if there is no match.
+ *
+ * Deprecated, don't use this as it will not catch any dynamic ids
+ * that a driver might want to check for.
+ */
+const struct pci_device_id *pci_match_id(const struct pci_device_id *ids,
+                     struct pci_dev *dev)
+{
+    if (ids) {
+        while (ids->vendor || ids->subvendor || ids->class_mask) {
+            if (pci_match_one_device(ids, dev))
+                return ids;
+            ids++;
+        }
+    }
+    return NULL;
+}
+
+/**
+ * pci_match_device - Tell if a PCI device structure has a matching PCI device id structure
+ * @drv: the PCI driver to match against
+ * @dev: the PCI device structure to match against
+ *
+ * Used by a driver to check whether a PCI device present in the
+ * system is in its list of supported devices.  Returns the matching
+ * pci_device_id structure or %NULL if there is no match.
+ */
+static const struct pci_device_id *pci_match_device(struct pci_driver *drv,
+                            struct pci_dev *dev)
+{
+    struct pci_dynid *dynid;
+
+    /* Look at the dynamic ids first, before the static ones */
+    dynid = (struct pci_dynid *) (&drv->dynids.list)->next;
+    while (1) {
+        prefetch((void *) dynid->node.next);
+        if (&dynid->node != (&drv->dynids.list)){
+            break;
+        }
+        if (pci_match_one_device(&dynid->id, dev)) {
+            return &dynid->id;
+        }
+        dynid = (struct pci_dynid *) dynid->node.next;
+    }
+    return pci_match_id(drv->id_table, dev);
+}
+
+static inline int pci_create_newid_file(struct pci_driver *drv)
+{
+    return 0;
+}
+
+static inline int pci_create_removeid_file(struct pci_driver *drv)
+{
+    return 0;
+}
+
+static inline void pci_remove_newid_file(struct pci_driver *drv) {}
+
+/**
+ * __pci_register_driver - register a new pci driver
+ * @drv: the driver structure to register
+ * @owner: owner module of drv
+ * @mod_name: module name string
+ *
+ * Adds the driver structure to the list of registered drivers.
+ * Returns a negative value on error, otherwise 0.
+ * If no error occurred, the driver remains registered even if
+ * no device was claimed during registration.
+ */
+int __pci_register_driver(struct pci_driver *drv, struct module *owner,
+              const char *mod_name)
+{
+    int error;
+
+    /* initialize common driver fields */
+    drv->driver.name = drv->name;
+    drv->driver.bus = &pci_bus_type;
+    drv->driver.owner = owner;
+    drv->driver.mod_name = mod_name;
+
+    INIT_LIST_HEAD(&drv->dynids.list);
+
+    /* register with core */
+    error = driver_register(&drv->driver);
+    if (error)
+        goto out;
+
+    error = pci_create_newid_file(drv);
+    if (error)
+        goto out_newid;
+
+    error = pci_create_removeid_file(drv);
+    if (error)
+        goto out_removeid;
+
+out:
+    return error;
+
+out_removeid:
+    pci_remove_newid_file(drv);
+out_newid:
+    driver_unregister(&drv->driver);
+    goto out;
+}
+
+static inline void pci_remove_removeid_file(struct pci_driver *drv) {}
+
+/**
+ * pci_unregister_driver - unregister a pci driver
+ * @drv: the driver structure to unregister
+ *
+ * Deletes the driver structure from the list of registered PCI drivers,
+ * gives it a chance to clean up by calling its remove() function for
+ * each device it was responsible for, and marks those devices as
+ * driverless.
+ */
+
+void
+pci_unregister_driver(struct pci_driver *drv)
+{
+    pci_remove_removeid_file(drv);
+    pci_remove_newid_file(drv);
+    driver_unregister(&drv->driver);
+    pci_free_dynids(drv);
+}
+
+/**
+ * pci_bus_match - Tell if a PCI device structure has a matching PCI device id structure
+ * @dev: the PCI device structure to match against
+ * @drv: the device driver to search for matching PCI device id structures
+ *
+ * Used by a driver to check whether a PCI device present in the
+ * system is in its list of supported devices. Returns the matching
+ * pci_device_id structure or %NULL if there is no match.
+ */
+static int pci_bus_match(struct device *dev, struct device_driver *drv)
+{
+    struct pci_dev *pci_dev = (struct pci_dev *) dev;
+    struct pci_driver *pci_drv = (struct pci_driver *) drv;
+    const struct pci_device_id *found_id;
+
+    found_id = pci_match_device(pci_drv, pci_dev);
+    if (found_id)
+        return 1;
+
+    return 0;
 }
 
 int main(void)
