69a2626d79344182fa1bd510ac0d5213f8647700
khanh <khanh@sunfire>
2012-12-28 20:51:27 +0800

    para5 now works well with --eps, tested with ./run-fast-test.sh sleek,hip,sleek_vperm,hip_vperm,parahip

---
+++ examples/working/ldk-run.sh
@@ -0,0 +1,26 @@
+set -x
+
+echo "*************************************"
+echo "*************************************"
+./run-fast-tests.pl sleek
+echo "*************************************"
+./run-fast-tests.pl sleek -flags "--eps --dis-para"
+echo "*************************************"
+echo "*************************************"
+#./run-fast-tests.pl hip -flags "--dis-para" #VERY SLOW
+./run-fast-tests.pl hip -flags "--eps --dis-para"
+echo "*************************************"
+echo "*************************************"
+./run-fast-tests.pl sleek_vperm
+echo "*************************************"
+./run-fast-tests.pl sleek_vperm -flags "--eps"
+echo "*************************************"
+echo "*************************************"
+./run-fast-tests.pl hip_vperm
+echo "*************************************"
+./run-fast-tests.pl hip_vperm -flags "--eps"
+echo "*************************************"
+echo "*************************************"
+./run-fast-tests.pl parahip
+echo "*************************************"
+./run-fast-tests.pl parahip -flags "--eps"
