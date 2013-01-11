45d75445e4d67ba9392fb365976b6cc3be8008aa
trinhmt <devnull@localhost>
2013-01-11 15:15:32 +0800

    x

--- typechecker.ml
+++ typechecker.ml
@@ -427,6 +427,7 @@ and check_specs_infer_a (prog : prog_decl) (proc : proc_decl) (ctx : CF.context)
                         new_spec,new_args
               end
             in
+            let pre_vars,post_vars = CF.get_pre_post_vars_simp [] new_formula_inf_continuation in
             let vars, new_formula_inf_continuation = 
               if old_vars = [] then
                 begin
@@ -434,7 +435,7 @@ and check_specs_infer_a (prog : prog_decl) (proc : proc_decl) (ctx : CF.context)
                     | None -> 
                           if new_args = [] then [],new_formula_inf_continuation 
                           else
-                            let pre_vars,_ = CF.get_pre_post_vars_simp [] new_formula_inf_continuation in
+(*                            let pre_vars,_ = CF.get_pre_post_vars_simp [] new_formula_inf_continuation in*)
                             let pre_args, _ = List.partition (fun x -> List.mem x pre_vars) new_args in
                             let new_rel_pre = CP.fresh_spec_var_rel () in
                             let new_rel_post = CP.fresh_spec_var_rel () in
@@ -447,7 +448,7 @@ and check_specs_infer_a (prog : prog_decl) (proc : proc_decl) (ctx : CF.context)
                     | Some pflag -> 
                           if not(pflag) then 
                             if new_args = [] then 
-                              let pre_vars,_ = CF.get_pre_post_vars_simp [] new_formula_inf_continuation in
+(*                              let pre_vars,_ = CF.get_pre_post_vars_simp [] new_formula_inf_continuation in*)
                               pre_vars,new_formula_inf_continuation
                             else 
                               let new_rel = CP.fresh_spec_var_rel () in
@@ -474,20 +475,22 @@ and check_specs_infer_a (prog : prog_decl) (proc : proc_decl) (ctx : CF.context)
             in
             let _ = proc.proc_stk_of_static_specs # push einfer in
             let _ = if old_vars=[] then Debug.info_hprint (add_str "TRANSLATED SPECS" pr_spec) einfer no_pos else () in
-            let _ = Debug.ninfo_hprint (add_str "VARS" !print_svl) vars no_pos in
+            let new_fml_fv = CF.struc_fv new_formula_inf_continuation in
+            let pre_post_vars = CP.remove_dups_svl (pre_vars @ post_vars @ new_fml_fv) in
+            let _ = Debug.ninfo_hprint (add_str "all vars" !print_svl) pre_post_vars no_pos in
+            let _ = Debug.ninfo_hprint (add_str "inf vars" !print_svl) vars no_pos in
             let (vars_rel,vars_inf) = List.partition (fun v -> is_RelT(CP.type_of_spec_var v) ) vars in
             let _ = Debug.ninfo_hprint (add_str "vars_rel" !print_svl) vars_rel no_pos in
-            let new_fml_fv = CF.struc_fv new_formula_inf_continuation in
             let classify_rel v = 
               let rel_decl = Cast.look_up_rel_def_raw prog.Cast.prog_rel_decls (CP.name_of_spec_var v) in
               if CP.isConstTrue rel_decl.rel_formula then true else false in              
             let (unknown_rel,known_rel) = List.partition classify_rel 
-              (CP.remove_dups_svl ((List.filter CP.is_rel_var new_fml_fv)@vars_rel)) in
+              (CP.remove_dups_svl ((List.filter CP.is_rel_var pre_post_vars)@vars_rel)) in
             let _ = Debug.ninfo_hprint (add_str "unknown_rel" !print_svl) unknown_rel no_pos in
             let _ = Debug.ninfo_hprint (add_str "known_rel" !print_svl) known_rel no_pos in
             let inf_pos = b.CF.formula_inf_pos in
             let _ =
-              if not(CP.subset vars new_fml_fv) then
+              if not(CP.subset vars pre_post_vars) then
                 report_error inf_pos "Inferable vars include some external variables!"
               else
               if not(CP.subset unknown_rel vars_rel) then
