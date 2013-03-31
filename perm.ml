8025c2133d685e5cedc59fd71e52f520fd608cc8
khanh <khanh@g4>
2013-03-31 23:04:16 +0800

    x

--- perm.ml
+++ perm.ml
@@ -163,7 +163,7 @@ struct
                   match f with
                     | Ipure.Var _ -> (f,[])
 		            | _ ->
-                        let nn_perm = ((perm_name^(string_of_int pos.start_pos.Lexing.pos_lnum)^(fresh_trailer ())),Unprimed) in
+                        let nn_perm = (("bperm_"^(string_of_int pos.start_pos.Lexing.pos_lnum)^(fresh_trailer ())),Unprimed) in
 			            let nv_perm = Ipure.Var (nn_perm,pos) in
                         let npf_perm = Ipure.BForm ((Ipure.Eq (nv_perm,f,pos), None), None) in (*TO CHECK: slicing for permissions*)
                         (nv_perm,[(nn_perm,npf_perm)])
