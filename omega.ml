bc572c633c1c06ca45f89fd1b3e30fac21f065d6
Chin Wei Ngan <chinwn@gmail.com>
2013-06-16 15:54:40 +0800

    x

--- omega.ml
+++ omega.ml
@@ -582,7 +582,7 @@ let imply_ops pr_weak pr_strong (ante : formula) (conseq : formula) (imp_no : st
 
 let imply_ops pr_weak pr_strong (ante : formula) (conseq : formula) (imp_no : string) timeout : bool =
   let pr = !print_formula in
-  Debug.ho_2 "[omega.ml]imply_ops_1" pr pr string_of_bool
+  Debug.no_2 "[omega.ml]imply_ops_1" pr pr string_of_bool
   (fun _ _ -> imply_ops pr_weak pr_strong ante conseq imp_no timeout) ante conseq
 
 let imply (ante : formula) (conseq : formula) (imp_no : string) timeout : bool =
