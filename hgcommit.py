c7df5c8ac7818e8d6c49fd48f105ac79aa134cde
Andreea Costea <andre.costea@gmail.com>
2013-06-18 16:15:22 +0800

    -v option for hgcommit.py

--- hgcommit.py
+++ hgcommit.py
@@ -2,45 +2,74 @@
 import subprocess
 import sys
 import os
+import getopt
+
 
 SIZE_LIMIT = 1048576 # commit files of max 1MB, otherwise warn the user
 #number of files limit: 
 NOF_LIMIT = 10 # commit max 10 modified files without warning the user 
 
-log_message = "x"
-if len(sys.argv) > 1 :
-    log_message = sys.argv[1]
 
-commit_command = "hg commit -m \"" + log_message + "\""
-
-temp_file="tmp"
-
-print "Checking hg stat for added/modified files... "
-subprocess.call("hg stat -man  > " + temp_file, executable="bash", shell=True)
-b = os.path.getsize(temp_file)
-
+temp_file = "commit_temp"
+log_message = "x" # default commit message
+commit_command = "hg commit -m \"" + log_message + "\"" # the commit command
+nof = 0 #number of modified/added files
 size_warning = False
-#number of files 
-nof = 0
+force_verif  = False 
+
+# parse command line arguments
+opts, args = getopt.getopt(sys.argv[1:],"hv",[])
+for opt, arg in opts:
+    if opt == '-h':
+        print './hgcommit.py [-v] [<commit message>] '
+    elif opt == '-v':
+        force_verif = True
+
+if len(args) > 0 :
+    log_message = args[0]
+
+
+#check for modified files (size of modified files does not produce any warning)
+print "Checking hg stat for added/modified files... "
+subprocess.call("hg stat -mn  > " + temp_file, executable="bash", shell=True)
 
 f = open(temp_file, 'r+')
 for line in f:
     nof += 1
-    print line.rstrip('\n')
+    print "M " + line.rstrip('\n')
+    # don't check modified files for size
+    # b = os.path.getsize(line.rstrip('\n'))
+    # if (b > SIZE_LIMIT):
+    #     size_warning = True
+
+
+#check for added files
+subprocess.call("hg stat -an  > " + temp_file, executable="bash", shell=True)
+f = open(temp_file, 'r+')
+for line in f:
+    nof += 1
     b = os.path.getsize(line.rstrip('\n'))
+    print "A " + line.rstrip('\n') + " (" + str(b) + " bytes)"
     if (b > SIZE_LIMIT):
         size_warning = True
 
-commit = True
+
 answ = "Continue"
+question = "Are you sure you want to continue with this commit? (Continue/no)"
+
 
 if (nof >= NOF_LIMIT):
-    answ = raw_input ("Trying to commit more than " + str(nof) +  " files. Are you sure you want to continue this commit? Continue/no ")
+    answ = raw_input ("WARNING: Trying to commit more than " + str(nof) +  " files.\n" + question)
 elif (size_warning):
-    answ = raw_input ("Trying to commit files larger than 1 MB. Are you sure you want to continue this commit? Continue/no ")
+    answ = raw_input ("WARNING: Trying to commit files larger than 1 MB. \n" + question)
+elif (force_verif):
+    answ = raw_input (question)
+
 
 if answ == "Continue":
     subprocess.call( commit_command, executable="bash", shell=True)
 else:
-    print "Aborting commit to repository..."
+    print "Commit aborted"
 
+
+subprocess.call("rm " + temp_file, executable="bash", shell=True)
