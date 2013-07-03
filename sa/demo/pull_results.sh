b1c4443200d3d9b1e9dc39464aa3aad72791f612
Cristian Gherghina <cris.gherghina@gmail.com>
2013-07-03 19:42:22 +0100

    x

--- sa/demo/pull_results.sh
+++ sa/demo/pull_results.sh
@@ -2,7 +2,7 @@
 
 function one 
 {
-	sll_out=`../../hip $1`
+	sll_out=`../../hip --en-texify $1`
 	rels=`echo "$sll_out" | awk '/rstart/,/rstop/'`
 	defs=`echo "$sll_out" | awk '/dstart/,/dstop/'`
 	echo "========$1=========="
@@ -14,6 +14,6 @@ one set-tail-2.ss
 one sll-dll.ss
 one last-2.ss
 one dll-append_paper.ss
-one zip_paper_leq.ss
+one "zip_paper_leq.ss --sa-en-sp-split --pred-en-dangling"
 one tll3.ss
 
