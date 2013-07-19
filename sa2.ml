f415d5a96416d642ab4319e2721ca83dd04961bd
Le Quang Loc <lequangloc@gmail.com>
2013-07-19 15:37:59 +0800

    turn on classic mode for obligations

--- sa2.ml
+++ sa2.ml
@@ -32,8 +32,11 @@ let collect_ho_ass cprog is_pre def_hps (acc_constrs, post_no_def) cs=
   if infer_hps = [] then (acc_constrs, post_no_def) else
     let log_str = if is_pre then PK_Pre_Oblg else PK_Post_Oblg in
     let  _ = DD.info_pprint ((string_of_proving_kind log_str) ^ ":\n" ^ (Cprinter.string_of_hprel_short cs)) no_pos in
+    let tmp = !Globals.do_classic_frame_rule in
+    let _ = Globals.do_classic_frame_rule := true in
     let f = wrap_proving_kind log_str (SAC.do_entail_check infer_hps cprog) in
     let new_constrs = f cs in
+    let _ = Globals.do_classic_frame_rule := tmp in
     (acc_constrs@new_constrs, post_no_def@linfer_hps)
 
 (*input in fb
