4d5af3b26468991c8e6c71c82e6e82a29564220d
Le Quang Loc <lequangloc@gmail.com>
2013-08-03 15:44:00 +0800

    to fix reuse bugs by using lemma mechanism

---
+++ lemma.ml
@@ -0,0 +1,13 @@
+open Globals
+open Wrapper
+open Gen
+open Others
+open Label_only
+
+module AS = Astsimp
+module C  = Cast
+module CF = Cformula
+module CP = Cpure
+module H  = Hashtbl
+module I  = Iast
+
