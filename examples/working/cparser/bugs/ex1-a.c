11b00f3f2f8f3f9ee27d5d403b17dcff1a5309e1
Le Quang Loc <lequangloc@gmail.com>
2013-10-22 13:56:30 +0800

    to support HeapPred in cparser

---
+++ examples/working/cparser/bugs/ex1-a.c
@@ -0,0 +1,9 @@
+
+struct node {
+  int val;
+  struct node* next;
+}
+/*
+HeapPred H(node a).
+HeapPred G(node a).
+*/
---
+++ examples/working/cparser/bugs/ex1.c
@@ -0,0 +1,36 @@
+
+struct node {
+  int val;
+  struct node* next;
+}
+/*
+H<> == self::node<_,p> * p::ll<>
+  inv true;
+
+ll<> == self=null
+	or self::node<_, q> * q::ll<> & self!=null
+	inv true;
+
+lseg<p> == self=p
+  or self::node<_, q> * q::lseg<p>
+  inv true;
+
+
+H1<> == self::node<_,p> & p=null
+	or self::node<_, q> * q::H1<>
+	inv true;
+*/
+HeapPred H(node a).
+HeapPred G(node a).
+
+void foo(struct node* x)
+/*@
+ infer [H,G]
+ requires H(x)
+ ensures  G(x);
+*/
+ {
+   if (x) {
+     foo(x.next);
+   }
+ }
