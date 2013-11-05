1fee898b63fd4ff653c23a4f90dbf2a6740e5628
Chin Wei Ngan <chinwn@gmail.com>
2013-11-06 01:34:38 +0800

    x

--- sa/validate/c/run-all.sh
+++ sa/validate/c/run-all.sh
@@ -1,67 +1,68 @@
+HIP=../../../hip
 echo "ll-trav.ss"
-../../../hip ll-trav.c -validate ../o/ll-trav.cp | grep Validate
+$HIP ll-trav.c -validate ../o/ll-trav.cp | grep Validate
 echo "ll-trav-1.ss: ll with at least one node"
-../../../hip ll-trav-1.c -validate ../o/ll-trav-1.cp | grep Validate
+$HIP ll-trav-1.c -validate ../o/ll-trav-1.cp | grep Validate
 echo "sll-reverse"
-../../hip sll-reverse.c -validate ../o/sll-reverse.cp | grep Validate
+$HIP sll-reverse.c -validate ../o/sll-reverse.cp | grep Validate
 echo "zip_paper_eq"
-../../hip zip_paper_eq.c -validate ../o/zip_paper_eq.cp | grep Validate
+$HIP zip_paper_eq.c -validate ../o/zip_paper_eq.cp | grep Validate
 echo "sll+head"
-../../hip check-sll-head.c -validate ../o/check-sll-head.cp | grep Validate
+$HIP check-sll-head.c -validate ../o/check-sll-head.cp | grep Validate
 echo "check-sll-head"
-../../hip check-sll-head.c -validate ../o/check-sll-head.cp | grep Validate
+$HIP check-sll-head.c -validate ../o/check-sll-head.cp | grep Validate
 echo "skip2-list"
-../../hip skip2-list.c -validate ../o/skip2-list.cp | grep Validate
+$HIP skip2-list.c -validate ../o/skip2-list.cp | grep Validate
 echo "skip3-list"
-#../../hip skip3-list.c -validate ../o/skip3-list.cp | grep Validate
+#$HIP skip3-list.c -validate ../o/skip3-list.cp | grep Validate
 echo "check-sorted"
-../../hip check-sorted.c --sa-en-pure-field -validate ../o/check-sorted.cp | grep Validate
+$HIP check-sorted.c --sa-en-pure-field -validate ../o/check-sorted.cp | grep Validate
 echo "sll-insertsort: to check result of G"
-#../../hip sll-insertsort.c -validate ../o/sll-insertsort.cp | grep Validate
+#$HIP sll-insertsort.c -validate ../o/sll-insertsort.cp | grep Validate
 echo "CSll"
-../../hip cll.c -validate ../o/cll.cp | grep Validate
+$HIP cll.c -validate ../o/cll.cp | grep Validate
 echo "check-CSll: can not verify"
-../../hip check-cll.c -validate ../o/check-cll.cp | grep Validate
+$HIP check-cll.c -validate ../o/check-cll.cp | grep Validate
 echo " 0/1 SLLs"
-../../hip sll-01-slls.c -validate o/sll-01-slls.cp | grep Validate
+$HIP sll-01-slls.c -validate o/sll-01-slls.cp | grep Validate
 echo "sll2dll"
-../../hip sll-dll.c -validate ../o/sll-dll.cp | grep Validate
+$HIP sll-dll.c -validate ../o/sll-dll.cp | grep Validate
 echo "check-dll"
-../../hip check-dll.c -validate ../o/check-dll.cp | grep Validate
+$HIP check-dll.c -validate ../o/check-dll.cp | grep Validate
 echo "bt-search-2."
-../../hip bt-search-2.c -validate  ../o/bt-search-2.cp | grep Validate
+$HIP bt-search-2.c -validate  ../o/bt-search-2.cp | grep Validate
 echo "tll"
-../../hip tll.c --sa-dnc --pred-en-eup -validate ../o/tll.cp | grep Validate
+$HIP tll.c --sa-dnc --pred-en-eup -validate ../o/tll.cp | grep Validate
 echo "rose-tree"
-../../hip rose-tree-1.c -validate ../o/rose-tree-1.cp | grep Validate
+$HIP rose-tree-1.c -validate ../o/rose-tree-1.cp | grep Validate
 echo "check mcf"
-../../hip check-mcf.c -validate o/check-mcf.cp | grep Validate
+$HIP check-mcf.c -validate o/check-mcf.cp | grep Validate
 echo "check-tll: to check"
-#../../hip check-tll.ss --sa-dnc --pred-en-dangling -validate o/check-tll.cp | grep Validate
+#$HIP check-tll.ss --sa-dnc --pred-en-dangling -validate o/check-tll.cp | grep Validate
 echo "ll-back.ss"
-#../../hip ll-back.ss
+#$HIP ll-back.ss
 #sa-dangling
 echo "dll-app"
-../../hip dll-append_paper.c --classic --pred-en-dangling -validate ../o/dll-append_paper.cp | grep Validate
+$HIP dll-append_paper.c --classic --pred-en-dangling -validate ../o/dll-append_paper.cp | grep Validate
 echo "tll"
-../../hip tll.c --sa-dnc --pred-en-dangling --pred-en-eup -validate ../o/tll_dang.cp | grep Validate
+$HIP tll.c --sa-dnc --pred-en-dangling --pred-en-eup -validate ../o/tll_dang.cp | grep Validate
 #pred-disj-unify
 echo "ll-trav-1.ss --pred-disj-unify"
-../../hip ll-trav-1.c --pred-disj-unify -validate ../o/ll-trav-1-unify.cp | grep Validate
+$HIP ll-trav-1.c --pred-disj-unify -validate ../o/ll-trav-1-unify.cp | grep Validate
 
 #pred-en-split
 echo "cyc-lseg"
-../../hip cyc-lseg.c  -validate ../o/cyc-lseg.cp --pred-en-split | grep Validate
+$HIP cyc-lseg.c  -validate ../o/cyc-lseg.cp --pred-en-split | grep Validate
 echo "zip_paper_eq"
-../../hip zip_paper_eq.c  -validate ../o/zip_paper_eq-split.cp --pred-en-split | grep Validate
+$HIP zip_paper_eq.c  -validate ../o/zip_paper_eq-split.cp --pred-en-split | grep Validate
 
 #pred-unify-post
 echo "cyc-tree (search)"
-../../hip cyc-tree-1.c --pred-unify-post -validate ../o/cyc-tree-1.cp | grep Validate
+$HIP cyc-tree-1.c --pred-unify-post -validate ../o/cyc-tree-1.cp | grep Validate
 echo "bt-search-2.ss"
-../../hip bt-search-2.c -validate --pred-unify-post  ../o/bt-search-2.cp | grep Validate
+$HIP bt-search-2.c -validate --pred-unify-post  ../o/bt-search-2.cp | grep Validate
 #sa-dnc
 
 #--pred-en-equiv
 echo "cyc-lseg"
-#../../hip ll-trav.c --pred-en-equiv -validate ../o/ll-trav-equiv.cp --pred-en-split | grep Validate
+#$HIP ll-trav.c --pred-en-equiv -validate ../o/ll-trav-equiv.cp --pred-en-split | grep Validate
--- sa/validate/run-all.sh
+++ sa/validate/run-all.sh
@@ -1,67 +1,68 @@
+HIP=../../hip
 echo "ll-trav.ss"
-../../hip ll-trav.ss -validate o/ll-trav.cp | grep Validate
+$HIP ll-trav.ss -validate o/ll-trav.cp | grep Validate
 echo "ll-trav-1.ss: ll with at least one node"
-../../hip ll-trav-1.ss -validate o/ll-trav-1.cp | grep Validate
+$HIP ll-trav-1.ss -validate o/ll-trav-1.cp | grep Validate
 echo "sll-reverse"
-../../hip sll-reverse.ss -validate o/sll-reverse.cp | grep Validate
+$HIP sll-reverse.ss -validate o/sll-reverse.cp | grep Validate
 echo "zip_paper_eq"
-../../hip zip_paper_eq.ss -validate o/zip_paper_eq.cp | grep Validate
+$HIP zip_paper_eq.ss -validate o/zip_paper_eq.cp | grep Validate
 echo "sll+head"
-../../hip check-sll-head.ss -validate o/check-sll-head.cp | grep Validate
+$HIP check-sll-head.ss -validate o/check-sll-head.cp | grep Validate
 echo "check-sll-head"
-../../hip check-sll-head.ss -validate o/check-sll-head.cp | grep Validate
+$HIP check-sll-head.ss -validate o/check-sll-head.cp | grep Validate
 echo "skip0"
-../../hip skip0.ss -validate o/skip0.cp | grep Validate
+$HIP skip0.ss -validate o/skip0.cp | grep Validate
 echo "skip2-list"
-../../hip skip2-list.ss -validate o/skip2-list.cp | grep Validate
+$HIP skip2-list.ss -validate o/skip2-list.cp | grep Validate
 echo "skip3-list"
-#../../hip skip3-list.ss -validate o/skip3-list.cp | grep Validate
+#$HIP skip3-list.ss -validate o/skip3-list.cp | grep Validate
 echo "check-sorted"
-../../hip check-sorted.ss --sa-en-pure-field -validate o/check-sorted.cp | grep Validate
+$HIP check-sorted.ss --sa-en-pure-field -validate o/check-sorted.cp | grep Validate
 #echo "sll-insertsort.ss"
-#../../hip sll-insertsort.ss -validate o/sll-insertsort.cp | grep Validate
+#$HIP sll-insertsort.ss -validate o/sll-insertsort.cp | grep Validate
 echo "CSll"
-../../hip cll.ss -validate o/cll.cp | grep Validate
+$HIP cll.ss -validate o/cll.cp | grep Validate
 echo "check-CSll"
-../../hip check-cll.ss -validate o/check-cll.cp | grep Validate
+$HIP check-cll.ss -validate o/check-cll.cp | grep Validate
 echo " 0/1 SLLs"
-../../hip sll-01-slls.ss -validate o/sll-01-slls.cp | grep Validate
+$HIP sll-01-slls.ss -validate o/sll-01-slls.cp | grep Validate
 echo "sll2dll"
-../../hip sll-dll.ss -validate o/sll-dll.cp | grep Validate
+$HIP sll-dll.ss -validate o/sll-dll.cp | grep Validate
 echo "check-dll"
-../../hip check-dll.ss -validate o/check-dll.cp | grep Validate
+$HIP check-dll.ss -validate o/check-dll.cp | grep Validate
 echo "bt-search-2.ss"
-../../hip bt-search-2.ss --pred-unify-post -validate  o/bt-search-2.cp | grep Validate
+$HIP bt-search-2.ss --pred-unify-post -validate  o/bt-search-2.cp | grep Validate
 echo "tll"
-../../hip tll.ss --sa-dnc --pred-en-eup -validate o/tll.cp | grep Validate
+$HIP tll.ss --sa-dnc --pred-en-eup -validate o/tll.cp | grep Validate
 echo "rose-tree"
-../../hip rose-tree-1.ss -validate o/rose-tree-1.cp | grep Validate
+$HIP rose-tree-1.ss -validate o/rose-tree-1.cp | grep Validate
 echo "check mcf: (to fix bugs of validation)"
-../../hip check-mcf.ss -validate o/check-mcf.cp | grep Validate
+$HIP check-mcf.ss -validate o/check-mcf.cp | grep Validate
 echo "check-tll: to check"
-#../../hip check-tll.ss --sa-dnc --pred-en-dangling -validate o/check-tll.cp | grep Validate
+#$HIP check-tll.ss --sa-dnc --pred-en-dangling -validate o/check-tll.cp | grep Validate
 echo "ll-back.ss"
-#../../hip ll-back.ss
+#$HIP ll-back.ss
 #sa-dangling
 echo "dll-app"
-../../hip dll-append_paper.ss --classic --pred-en-dangling -validate o/dll-append_paper.cp | grep Validate
+$HIP dll-append_paper.ss --classic --pred-en-dangling -validate o/dll-append_paper.cp | grep Validate
 echo "tll sa-dangling"
-../../hip tll.ss --sa-dnc --pred-en-dangling --pred-en-eup -validate o/tll_dang.cp | grep Validate
+$HIP tll.ss --sa-dnc --pred-en-dangling --pred-en-eup -validate o/tll_dang.cp | grep Validate
 #pred-disj-unify
 echo "ll-trav-1.ss --pred-disj-unify"
-../../hip ll-trav-1.ss --pred-disj-unify -validate o/ll-trav-1-unify.cp | grep Validate
+$HIP ll-trav-1.ss --pred-disj-unify -validate o/ll-trav-1-unify.cp | grep Validate
 
 #pred-en-split
 echo "cyc-lseg"
-../../hip cyc-lseg.ss  -validate o/cyc-lseg.cp --pred-en-split | grep Validate
+$HIP cyc-lseg.ss  -validate o/cyc-lseg.cp --pred-en-split | grep Validate
 echo "zip_paper_eq"
-../../hip zip_paper_eq.ss  -validate o/zip_paper_eq-split.cp --pred-en-split | grep Validate
+$HIP zip_paper_eq.ss  -validate o/zip_paper_eq-split.cp --pred-en-split | grep Validate
 
 #pred-unify-post
 echo "cyc-tree"
-../../hip cyc-tree-1.ss --pred-unify-post -validate o/cyc-tree-1.cp | grep Validate
+$HIP cyc-tree-1.ss --pred-unify-post -validate o/cyc-tree-1.cp | grep Validate
 #sa-dnc
 
 #--pred-en-equiv
 echo "cyc-lseg"
-#../../hip ll-trav.ss --pred-en-equiv -validate o/ll-trav-equiv.cp --pred-en-split | grep Validate
+#$HIP ll-trav.ss --pred-en-equiv -validate o/ll-trav-equiv.cp --pred-en-split | grep Validate
