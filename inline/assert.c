ae70d21d4b1de383156512bd35783a14dc8c6d8c
longph <Long>
2013-11-06 11:00:46 +0800

    fixed addr_x translation

--- inline/assert.c
+++ inline/assert.c
@@ -1,12 +1,15 @@
 void f() {
   int x=0;
-  int *p;
+  int y=0;
+  int *p = &y;
 
-  p = &x;
-  p = &x; // Just another mention of &x.
+  //p = &x;
+  //p = &x; // Just another mention of &x.
   *p = 0;
   int t = x;
+  //@ dprint;
   //@ assert (x' = 0);
+  //@ dprint;
 
   return;
 }
