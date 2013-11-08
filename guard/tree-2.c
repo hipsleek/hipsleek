8ff0afaf4ef3bd1e50177c5bb1d3d33c7cf1c627
Chin Wei Ngan <chinwn@gmail.com>
2013-11-08 13:06:27 +0800

    x

---
+++ guard/tree-2.c
@@ -0,0 +1,58 @@
+// simpler tll working example
+
+struct node{
+  struct node* parent;
+  struct node* left;
+  struct node* right;
+};
+
+/*@
+
+// predicate for a non-empty tree with chained leaf list
+
+// predicate for a non-empty tree
+ tree<p> == self::node<p,_,null>
+	or self::node<p,l,r> * l::tree<self> * r::tree<self>
+	inv self!=null;
+
+ gtree<p> == self=null 
+        or self::node<p,l,r> * l::gtree<self> * r::gtree<self>
+	 inv true;
+*/
+
+// initializes the linked list fields
+/*
+HeapPred H(node a, node@NI b).
+HeapPred G(node a, node@NI b, node c).
+*/
+void trav (struct node* p, struct node* x)
+//infer [H,G] requires H(x,t) ensures G(x,res,t);
+/*@
+//requires x::tree<p> ensures x::tree<p>;
+requires x::gtree<p> ensures x::gtree<p>;
+*/
+{
+  struct node* xp = x->parent;
+  struct node* xl = x->left;
+  struct node* xr = x->right;
+  //@ assert p=xp' assume p=xp';
+  if (!x->right)
+    {
+      //		assert xl'=null;
+      ;
+    }
+  else
+    {
+//		assert xr'!=null & xl'!=null;
+      trav(x,xr);
+      trav(x,xl);
+    }
+}
+
+/*
+
+
+
+
+
+*/
