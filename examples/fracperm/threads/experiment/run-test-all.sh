0c028cc5b44108702d4641e9ecbfcf342c995dab
khanh <khanh@sunfire>
2014-03-05 16:17:50 +0800

    some pending changes

--- examples/fracperm/threads/experiment/run-test-all.sh
+++ examples/fracperm/threads/experiment/run-test-all.sh
@@ -15,11 +15,14 @@ HIP=../../../../hip
 PARAHIP_RSR=~/hg/para5-threads2/sleekex/examples/fracperm/threads/parahip-rsr
 FLAG1="--en-para --en-thrd-resource -tp parahip --en-lsmu-infer"
 
+# Threads as resource
+EXAMPLE=~/hg/para5-threads2/sleekex/examples/fracperm/threads
+
 # Threads as AND-conjunction
 PARAHIP_ORG=~/hg/para5-threads2/sleekex/examples/fracperm/threads/parahip-org
 FLAG2="--en-para --en-thrd-and-conj -tp parahip --en-lsmu-infer"
 
-ITERATIONS=1
+ITERATIONS=10
 LOG=run-all-logs_$(date +%b%d_%H%M)
 #log all run
 LOGRUN=$LOG-run.xls
@@ -27,6 +30,29 @@ LOGRUN=$LOG-run.xls
 LOGAVG=$LOG-avg.xls
 echo "Writing all runs to $LOGRUN ..."
 echo "Writing average to $LOGAVG ..."
+
+echo -e "Program\tIteration\tTime"  >> $LOGRUN
+
+echo -e "Program\tLOC\tAvgTime"  >> $LOGAVG
+
+echo "Running programs in $EXAMPLE ..."
+for prog in $( ls $EXAMPLE/*.ss );
+do
+    sum=0
+    for (( i = 1; i <= $ITERATIONS; i++ ))
+    do
+	    echo "============================="
+	    echo "===$prog : $i"
+	    echo "============================="
+        result=$($HIP $FLAG1 $prog | grep "Total verification" |  awk '{print $4}')
+        echo -e "$prog\t$i\t$result" >> $LOGRUN
+        sum=$(echo "$sum + $result" | bc -l)
+    done
+    avg=$(echo "$sum / $ITERATIONS" | bc -l)
+    l=$(wc -l $prog |  awk '{print $1}')
+    echo -e "$prog\t$l\t$avg" >> $LOGAVG
+done
+
 
 echo "Running programs in $PARAHIP_RSR ..."
 for prog in $( ls $PARAHIP_RSR/*.ss );
@@ -42,7 +68,8 @@ do
         sum=$(echo "$sum + $result" | bc -l)
     done
     avg=$(echo "$sum / $ITERATIONS" | bc -l)
-    echo -e "$prog\t$avg" >> $LOGAVG
+    l=$(wc -l $prog |  awk '{print $1}')
+    echo -e "$prog\t$l\t$avg" >> $LOGAVG
 done
 
 echo "Running programs in $PARAHIP_ORG ..."
@@ -59,5 +86,6 @@ do
         sum=$(echo "$sum + $result" | bc -l)
     done
     avg=$(echo "$sum / $ITERATIONS" | bc -l)
-    echo -e "$prog\t$avg" >> $LOGAVG
+    l=$(wc -l $prog |  awk '{print $1}')
+    echo -e "$prog\t$l\t$avg" >> $LOGAVG
 done
