f36d2a93f0d28dc772af32924923d35f9f5ecf99
Le Quang Loc <lequangloc@gmail.com>
2014-03-17 11:26:36 +0800

    removed dups var declaration when trans to smtv2

--- smtsolver.ml
+++ smtsolver.ml
@@ -654,9 +654,10 @@ let string_of_logic logic =
   | UFNIA -> "UFNIA"
 
 (* output for smt-lib v2.0 format *)
-let to_smt_v2 pr_weak pr_strong ante conseq fvars info =
+let to_smt_v2 pr_weak pr_strong ante conseq fvars0 info =
   (* Variable declarations *)
   (*let _ = List.map (fun c -> print_string(" "^(!print_ty_sv c))) fvars in*)
+  let fvars = Cpure.remove_dups_svl fvars0 in
   let smt_var_decls = List.map (fun v ->
     let tp = (CP.type_of_spec_var v)in
     let t = smt_of_typ tp in
