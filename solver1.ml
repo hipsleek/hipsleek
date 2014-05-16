1203aa42a729d44d1261df95659a0ba98f31fd78
Ta Quang Trung <taquangtrungvn@gmail.com>
2014-05-16 23:03:10 +0800

    need to extract the model from SMT solvers

--- solver1.ml
+++ solver1.ml
@@ -194,41 +194,24 @@ let rec collect_primitive_preds (hf: CF.h_formula) : CF.h_formula list =
  *   - consider only conjunction (And, Exists formula)  
  *   - otherwise raise exception
  *)
-let compute_wellformed_condition (f: CF.formula) : CP.formula =
-  (* compute wellformed condition of a h_formula *)
-  let wellformed_condition_of_hformula (hf: CF.h_formula) : CP.formula = (
-    let prim_preds = collect_primitive_preds hf in
-    let sound_conds = List.map get_sound_condition prim_preds in
-    let separated_conds = (
-      let rec collect_pairs (l: 'a list) : ('a * 'a) list = (
-        match l with
-        | hd::tl -> 
-            let list1 = List.map (fun x -> (hd, x)) tl in
-            let list2 = collect_pairs tl in
-            list1 @ list2
-        | [] -> []
-      ) in
-      let pred_pairs = collect_pairs prim_preds in
-      List.map (fun (x,y) -> compute_separated_condition x y) pred_pairs
+let compute_wellformed_condition (hf: CF.formula) : CP.formula =
+  let prim_preds = collect_primitive_preds hf in
+  let sound_conds = List.map get_sound_condition prim_preds in
+  let separated_conds = (
+    let rec collect_pairs (l: 'a list) : ('a * 'a) list = (
+      match l with
+      | hd::tl -> 
+          let list1 = List.map (fun x -> (hd, x)) tl in
+          let list2 = collect_pairs tl in
+          list1 @ list2
+      | [] -> []
     ) in
-    List.fold_left (
-      fun f cond -> CP.mkAnd f cond no_pos
-    ) (CP.mkTrue no_pos) (sound_conds @ separated_conds) 
+    let pred_pairs = collect_pairs prim_preds in
+    List.map (fun (x,y) -> compute_separated_condition x y) pred_pairs
   ) in
-  match f with
-  | CF.Base bf ->
-      let pos = bf.CF.formula_base_pos in
-      let heap = bf.CF.formula_base_heap in
-      let wellformed_heap = wellformed_condition_of_hformula heap in
-      let pure = MCP.pure_of_mix (bf.CF.formula_base_pure) in
-      (CP.mkAnd wellformed_heap pure pos)
-  | CF.Exists _ ->
-      let msg = "compute_wellformed_condition: handle Exists later" in
-      raise (Solver_exception msg)
-  | CF.Or _ ->
-      let msg = "compute_wellformed_condition: "
-                ^ "expect conjunction (And, Exists formula)" in
-      raise (Solver_exception msg)
+  List.fold_left (
+    fun f cond -> CP.mkAnd f cond no_pos
+  ) (CP.mkTrue no_pos) (sound_conds @ separated_conds) 
 
 (* 
  * Compute the allocating function of a spatial formula 
@@ -358,11 +341,11 @@ let check_statifiablity (tbl_stack: (CP.spec_var, int) Hashtbl.t) (f: CP.formula
 
 (*
  * Find a empty heap in a spatial formula against a stack model
- *   Return: - remainder of the spatial formula
+ *   Return: - remainder of the spatial formula (if the formula is HEmp, no need to split)
  *           - empty condition of the found empty heap 
  *)
 let rec split_empty_heap (tbl_stack: (CP.spec_var, int) Hashtbl.t) (hf: CF.h_formula)
-    : (CF.formula * CP.formula) option =
+    : (CF.h_formula * CP.formula) option =
   match hf with
   | CF.Star sf -> (
       let f1 = sf.CF.h_formula_star_h1 in
@@ -394,20 +377,67 @@ let rec split_empty_heap (tbl_stack: (CP.spec_var, int) Hashtbl.t) (hf: CF.h_for
           let empty_cond = CP.mkAnd empty_cond1 empty_cond2 pos in
           Some (remainder, empty_cond)
     )
-  | CF.DataNode _ | CF.ViewNode _ | CF.HEmp ->
+  | CF.DataNode _ | CF.ViewNode _ ->
       let empty_cond = get_empty_condition hf in
       if (check_statifiablity tbl_stack empty_cond) then
         Some (CF.HEmp, empty_cond)
       else None
+  | CF.HEmp -> None
   | _ ->
       let msg = "split_empty_heap: allow only spatial conjunction "
                 ^ "of primitive predicates " in
       raise (Solver_exception msg)
 
 
-let match_spatial_formula (tbl_stack: (CP.spec_var, int) Hashtbl.t) 
-    (alloc: (CP.spec_var -> CP.formula)) (f1: CF.formula) (f2: CF.formula)
-    : CF.formula =
+(*
+ * Check separated condition of two primitive predicates
+ *)
+let check_separated_heaps (tbl_stack: (CP.spec_var, int) Hashtbl.t) 
+    (f1: CF.h_formula) (f2: CF.h_formula) : bool =
+  let separated_cond = compute_separated_condition f1 f2 in
+  let f = substitute_formula tbl_stack separated_cond in
+  let mf = MCP.mix_of_pure f in
+  TP.is_sat_raw mf
+
+
+(*
+ * Split a spatial formula into a primitive predicate and the remainders
+ *   Return: a list of possible splits. Each element of the list contains
+ *           a primitive predicate and the remainder of spatial formula
+ *)
+let rec split_primitive_heap (hf: CF.h_formula)
+    : (CF.h_formula * CF.h_formula) list =
+  match hf with
+  | CF.Star sf ->
+      let f1 = sf.CF.h_formula_star_h1 in
+      let f2 = sf.CF.h_formula_star_h2 in
+      let pos = sf.CF.h_formula_star_pos in
+      let split_f1 = split_primitive_heap f1 in
+      let res1 = List.map (fun (prim_pred, remainder) ->
+        let full_remainder = match remainder with
+          | CF.HEmp -> f2
+          | _ -> (CF.mkStarH remainder f2 pos) in
+        (prim_pred, full_remainder) 
+      ) split_f1 in
+      let split_f2 = split_primitive_heap f2 in
+      let res2 = List.map (fun (prim_pred, remainder) ->
+        let full_remainder = match remainder with
+          | CF.HEmp -> f1
+          | _ -> (CF.mkStarH f1 remainder pos) in
+        (prim_pred, full_remainder) 
+      ) split_f2 in
+      res1 @ res2
+  | CF.DataNode _ | CF.ViewNode _ | CF.HEmp ->
+      [(hf, CF.HEmp)]
+  | _ ->
+      let msg = "split_primitive_heap: allow only spatial conjunction "
+                ^ "of primitive predicates" in
+      raise (Solver_exception msg)
+
+
+let rec match_spatial_formula (tbl_stack: (CP.spec_var, int) Hashtbl.t) 
+    (alloc: (CP.spec_var -> CP.formula)) (f1: CF.h_formula) (f2: CF.h_formula)
+    : CP.formula =
   match (split_empty_heap tbl_stack f1) with
   | Some (newf1, empty_cond1) ->
       let match_res = match_spatial_formula tbl_stack alloc newf1 f2 in
@@ -418,29 +448,77 @@ let match_spatial_formula (tbl_stack: (CP.spec_var, int) Hashtbl.t)
           let match_res = match_spatial_formula tbl_stack alloc f1 newf2 in
           (CP.mkAnd empty_cond2 match_res no_pos)
       | None ->
-          CP.HEmp(* TODO: temporary *)
+          let split1 = split_primitive_heap f1 in
+          let split2 = split_primitive_heap f2 in
+          let pred1 = ref CF.HEmp in 
+          let rest1 = ref CF.HEmp in
+          let pred2 = ref CF.HEmp in
+          let rest2 = ref CF.HEmp in
+          if (List.exists (fun (p1, r1) ->
+                List.exists (fun (p2, r2) ->
+                  pred1 := p1; rest1 := r1;
+                  pred2 := p2; rest2 := r2;
+                  not (check_separated_heaps tbl_stack p1 p2)
+                ) split2 
+              ) split1 ) then (
+            let (remainder, subtract_cond) =
+                subtract_nonempty_predicate !pred1 !pred2 alloc in
+            let sound_cond = get_sound_condition remainder in
+            if (check_statifiablity tbl_stack sound_cond) then (
+              let separated_cond = compute_separated_condition !pred1 !pred2 in
+              let not_separated = CP.mkNot separated_cond None no_pos in
+              let match_res = match_spatial_formula tbl_stack alloc !rest1 !rest2 in
+              let temp1 = CP.mkAnd not_separated sound_cond no_pos in
+              let temp2 = CP.mkAnd temp1 subtract_cond no_pos in
+              CP.mkAnd temp2 match_res no_pos
+            )
+            else (CP.mkFalse no_pos)
+          )
+          else if ((f1 = CF.HEmp) && (f2 = CF.HEmp)) then
+            CP.mkTrue no_pos
+          else CP.mkFalse no_pos
     )
-        
 
+let prove_entailment (f1: CF.formula) (f2: CF.formula) : bool =
+  match f1, f2 with
+  | CF.Base bf1, CF.Base bf2 ->
+      let pf1 = MCP.pure_of_mix (bf1.CF.formula_base_pure) in
+      let hf1 = bf1.CF.formula_base_heap in
+      let pf2 = MCP.pure_of_mix (bf2.CF.formula_base_pure) in
+      let hf2 = bf2.CF.formula_base_heap in
+      let hf1_wellformed = compute_wellformed_condition hf1 in
+      let ante_approx = CP.mkAnd pf1 hf1_wellformed no_pos in
+      let not_pf2 = CP.mkNot pf2 None no_pos in
+      let temp = CP.mkAnd ante_approx not_pf2 no_pos in
+      let mtemp = MCP.mix_of_pure subs_f in
+      if (TP.is_sat_raw mtemp) then false  (* return invalid *)
+      else (
+        let alloc = compute_allocating_function hf1 in
+        let ante = ref ante_approx in
+        (* TODO: if a formula is SAT, collect its model *)
+      )
+  | _, _ ->
+      let msg = "prove_entailment: unexpected formula!" in
+      raise (Solver_exception msg) 
 
 (*
  * Preprocess to check the validity of input formula
  * Allow only a conjunction of primitive predicates
  * Primitive prediates: x::node<y>, x::lseg<y,n>, emp
  *)
-let rec preprocess_hformula (hf: CF.h_formula) : boolean =
+let rec preprocess_hformula (hf: CF.h_formula) : bool =
   match hf with
   | CF.Star sf ->
       let f1 = sf.CF.h_formula_star_h1 in
       let f2 = sf.CF.h_formula_star_h2 in
-      (proprocess_hformula f1) && (preprocess_hformula f2)
+      (preprocess_hformula f1) && (preprocess_hformula f2)
   | CF.ViewNode _ -> true
   | CF.DataNode _ -> true
   | CF.HEmp -> true
   | _ -> false
   
 
-let preprocess_formula (f: CF.formula) : boolean =
+let preprocess_formula (f: CF.formula) : bool =
   match f with
   | CF.Base bf ->
       let hf = bf.CF.formula_base_heap in
