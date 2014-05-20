91b7fca92bd1c18f3a2a871ae771f063919c1427
asankhs <devnull@localhost>
2014-05-20 15:43:02 +0800

    edit example for dag dispose

--- wand/dagdispose.c
+++ wand/dagdispose.c
@@ -10,7 +10,7 @@ relation lookup(abstract D, node x, int d, node l, node r).
 
 dag<D> == self = null
        or self::node<v,l,r> * (l::dag<D> U* r::dag<D>)
-	& lookup(D,self,v,l,r);
+	   & lookup(D,self,v,l,r);
 
 tree<T> == self = null
         or self::node<v,l,r> * l::tree<T> * r::tree<T>
@@ -23,31 +23,8 @@ rlemma x::dag<D1> * x::dag<D> --@ (x::dag<D> U* y::dag<D>)
       & subset_reach(D,x,D1) & eq_notreach(D,x,D1)
       -> x::dag<D1> U* y::dag<D1>;
 
-relation mark(abstract D,node x,abstract D1).
+relation unmarked(abstract D).
 
-axiom true ==> mark(D,null,D).
-
-axiom lookup(D,x,1,l,r) ==> mark(D,x,D).
-
-axiom mark(D,x,D1) ==> subset_reach(D,x,D1) & eq_notreach(D,x,D1).
-
-axiom lookup(D,x,v,l,r) & update(D,x,1,l,r,D1) & v != 1 & //v is unmarked skipped in paper
-mark(D1,l,D2) & mark(D2,r,D3) ==> mark(D,x,D3) & lookup(D3,x,1,l,r).
-
-axiom lookup(D,x,v,l,r) & update(D,x,1,l,r,D1) & v != 1 & //v is unmarked skipped in paper
-mark(D1,r,D2) & mark(D2,l,D3) ==> mark(D,x,D3) & lookup(D3,x,1,l,r).
-
-axiom lookup(D,x,v,l,r) & mark(D,l,D1) & v != 1
-& mark(D1,r,D2) & update(D2,x,1,l,r,D3) ==> mark(D,x,D3) & lookup(D3,x,1,l,r).
-
-axiom lookup(D,x,v,l,r) & mark(D,r,D1) & v != 1
-& mark(D1,l,D2) & update(D2,x,1,l,r,D3) ==> mark(D,x,D3) & lookup(D3,x,1,l,r).
-
-axiom lookup(D,x,v,l,r) & mark(D,l,D1) & v != 1
-& mark(D2,r,D3) & update(D1,x,1,l,r,D2) ==> mark(D,x,D3) & lookup(D3,x,1,l,r).
-
-axiom lookup(D,x,v,l,r) & mark(D,r,D1) & v != 1
-& mark(D2,l,D3) & update(D1,x,1,l,r,D2) ==> mark(D,x,D3) & lookup(D3,x,1,l,r).
 */
 
 void spanning(struct node *x)
