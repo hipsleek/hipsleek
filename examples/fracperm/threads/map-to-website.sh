2e32aa209b864df8e45925b1e6239b4ee61e1007
khanh <khanh@sunfire>
2014-05-29 09:46:20 +0800

    some pending

--- examples/fracperm/threads/map-to-website.sh
+++ examples/fracperm/threads/map-to-website.sh
@@ -5,20 +5,21 @@ WEB=/home/project/public_html/threadhip/src/hip/
 # cp ../../../prelude.ss $WEB/../../cgi-bin/
 # cp ../../../hip $WEB/../../cgi-bin/threadhip
 
-# #===================
-# cp threadhip/fibonacci.ss $WEB/1.ss
-# cp threadhip/parallel-mergesort.ss $WEB/2.ss
+#===================
+cp threadhip/fibonacci.ss $WEB/1.ss
+cp threadhip/parallel-mergesort.ss $WEB/2.ss
+cp threadhip/deadpool.ss $WEB/3.ss
 # cp threadhip/parallel-quicksort.ss $WEB/3.ss
-# cp threadhip/oracle.ss $WEB/4.ss
-# cp threadhip/owicki-gries.ss $WEB/5.ss
+cp threadhip/oracle.ss $WEB/4.ss
+cp threadhip/owicki-gries.ss $WEB/5.ss
 
-# #===================
+#===================
 
-# cp threadhip/multicast.ss $WEB/6.ss
-# cp threadhip/threadpool.ss $WEB/7.ss
-# cp threadhip/multi-join1.ss $WEB/8.ss
-# cp threadhip/multi-join2.ss $WEB/9.ss
-# cp threadhip/no-deadlock-nonlexical2.ss $WEB/10.ss
+cp threadhip/multicast.ss $WEB/6.ss
+cp threadhip/threadpool.ss $WEB/7.ss
+cp threadhip/multi-join1.ss $WEB/8.ss
+cp threadhip/multi-join2.ss $WEB/9.ss
+cp threadhip/no-deadlock-nonlexical2.ss $WEB/10.ss
 
 # #===================
 # # Spare for future use
