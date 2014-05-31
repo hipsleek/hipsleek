21f092554ee370c2662944e37ce73091648d6243
Chin Wei Ngan <chinwn@gmail.com>
2014-05-31 12:57:38 +0800

    reverted baga_xpure

--- cvutil.ml
+++ cvutil.ml
@@ -626,8 +626,8 @@ and xpure_heap_mem_enum_new
     (* to call xpure_heap_enum_baga *)
     let disj = xpure_heap_enum_baga (prog : prog_decl) (h0 : h_formula) (p0: mix_formula) (which_xpure :int) in
     let ans = conv_from_ef_disj disj in
-    ans
-    (* xpure_heap_mem_enum_x (prog : prog_decl) (h0 : h_formula) (p0: mix_formula) (which_xpure :int)  *)
+    (* ans *)
+    xpure_heap_mem_enum_x (prog : prog_decl) (h0 : h_formula) (p0: mix_formula) (which_xpure :int)
 
 
 and xpure_heap_mem_enum(*_debug*) (prog : prog_decl) (h0 : h_formula) (p0: mix_formula) (which_xpure :int) : (MCP.mix_formula * CF.mem_formula) =
