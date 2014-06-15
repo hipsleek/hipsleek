49c2eae47e8c1d5fa1786cf731a4799fad5db478
longph <devnull@localhost>
2014-06-16 00:40:32 +0800

    sort in builf_ef_view

--- expure.ml
+++ expure.ml
@@ -1412,6 +1412,7 @@ let build_ef_view_x (view_decl : Cast.view_decl) (all_views : Cast.view_decl lis
       let disj = build_ef_formula cf all_views in
       disj
   ) view_decl.Cast.view_un_struc_formula) in
+  let disj = List.sort EPureI.epure_compare disj in
   let disj_n = EPureI.norm_disj disj in
   disj_n
 
