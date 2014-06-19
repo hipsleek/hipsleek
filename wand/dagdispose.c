3ad439bee8757830ba478e8f09fa88a1562f6078
asankhs <devnull@localhost>
2014-06-19 13:33:32 +0800

    working on dag dispose

--- wand/dagdispose.c
+++ wand/dagdispose.c
@@ -9,8 +9,8 @@ relation update(abstract D, node x, int d, node l, node r, abstract D1).
 relation lookup(abstract D, node x, int d, node l, node r).
 
 dag<D> == self = null
-       or self::node<v,l,r> * (l::dag<D> U* r::dag<D>)
-	   & lookup(D,self,v,l,r);
+	or self::node<v,l,r> * (l::dag<D> U* r::dag<D>)
+	& lookup(D,self,v,l,r);
 
 tree<T> == self = null
         or self::node<v,l,r> * l::tree<T> * r::tree<T>
@@ -19,37 +19,38 @@ tree<T> == self = null
 relation subset_edges(abstract T, abstract D).
 relation reach_eq(abstract T, node x, abstract D).
 
-relation pr(abstract D, node x, bag(node) S).
+//relation pr(abstract D, node x, bag(node) S).
 
-axiom true ==> pr(D,null,{}).
+//axiom true ==> pr(D,null,{}).
 
-axiom pr(D,l,Sl) & pr(D,r,Sr) & lookup(D,x,0,l,r) & S = union(Sl,Sr) ==> pr(D,x,S).
+//axiom pr(D,l,Sl) & pr(D,r,Sr) & lookup(D,x,0,l,r) & S = union(Sl,Sr) ==> pr(D,x,S).
 
-axiom lookup(D,x,0,null,null) ==> pr(D,x,{x}).
+//axiom lookup(D,x,0,null,null) ==> pr(D,x,{x}).
  
 rlemma x::tree<T> * x::dag<D> --@ (x::dag<D> U* y::dag<D>)
       &	subset_edges(T,D) & reach_eq(T,x,D)
-      -> x::tree<T> * x::dag<T> U* y::dag<T>;
+      -> x::tree<T> * (x::dag<T> U* y::dag<T>);
 
 relation unmarked(abstract D, node x).
 
-axiom unmarked(D,x) ==> lookup(D,x,0,_,_).
+axiom unmarked(D,x) ==> lookup(D,x,0,l,r) & unmarked(D,l) & unmarked(D,r).
 
 */
 
 void spanning(struct node *x)
 /*@
-requires x::dag<D> & unmarked(D,x);
+requires x::dag<D> & unmarked(D,x) & x != null
 ensures x::tree<T> & subset_edges(T,D) & reach_eq(T,x,D);
 */
 {
-struct node *l,*r;
- l = x->left;
- r = x->right;
- if(l !=NULL && l->val != 1)
-  spanning(l);
- else x->left = 0;
- if(r != NULL && r->val !=1)
-  spanning(r);
- else x->right = 0;
+  struct node *l,*r;
+  l = x->left;
+  r = x->right;
+  x->val = 1;
+  if(l !=NULL && l->val != 1)
+    spanning(l);
+  else x->left = NULL;
+  if(r != NULL && r->val !=1)
+    spanning(r);
+  else x->right = NULL;
 }
