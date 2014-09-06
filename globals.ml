83573e98b0013c04dd20bb256d3c5b4045cd9289
longph <devnull@localhost>
2014-09-06 20:06:48 +0800

    print tidy

--- globals.ml
+++ globals.ml
@@ -1153,10 +1153,10 @@ let print_mvars = ref false
 
 let print_type = ref false
 
-let print_en_tidy = ref false
+let print_en_tidy = ref true
 (* not stable - this flag is not working!*)
 
-let print_en_inline = ref false
+let print_en_inline = ref true
 (* not stable - this flag is not working!*)
 
 let print_html = ref false
