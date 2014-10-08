ac948c2c2375998a2cab3973c33f51826ba08bec
Le Ton Chanh <letonchanh@gmail.com>
2014-10-08 14:03:21 +0800

    Makefile

--- cil/config.mk
+++ cil/config.mk
@@ -1,6 +1,6 @@
 # A bunch of variables  -*- Mode: makefile -*-
 export ARCHOS=x86_LINUX
 export HAS_MSVC=no
-export CILHOME=/home/trungtq/Workspace/sleekex-cparser/cil
+export CILHOME=/home/chanhle/hg/java_parser/cil
 export DEFAULT_COMPILER=_GNUCC
 export _GNUCC=1
