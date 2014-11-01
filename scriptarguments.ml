f006bf010d7b303d5268fefe966d3af4d0d96eec
Ta Quang Trung <taquangtrungvn@gmail.com>
2014-11-01 18:00:17 +0800

    change setting for --svcomp-mode

--- scriptarguments.ml
+++ scriptarguments.ml
@@ -788,7 +788,8 @@ let common_arguments = [
           (* print_endline "inside svcomp-compete setting"; *)
           Globals.compete_mode:=true; (* main flag *)
           Globals.svcomp_compete_mode:=true; (* main flag *)
-          Globals.show_unexpected_ents := false;
+          (* Globals.show_unexpected_ents := false; *)
+          (* diable printing *)
           Globals.trace_failure := false;
           Debug.trace_on := false;
           Debug.devel_debug_on:= false;
@@ -796,22 +797,25 @@ let common_arguments = [
           Globals.silence_output:=true;
           Globals.enable_count_stats:=false;
           Globals.enable_time_stats:=false;
-          Globals.lemma_gen_unsafe:=true;
-          Globals.lemma_syn := true;
-          Globals.acc_fold := true;
-          Globals.smart_lem_search := true;
+          
+          (* Globals.lemma_gen_unsafe:=true;    *)
+          (* Globals.lemma_syn := true;         *)
+          (* Globals.acc_fold := true;          *)
+          (* Globals.smart_lem_search := true;  *)
           (* Globals.gen_baga_inv := true; *)
-          Globals.en_pred_sat ();
+          (* Globals.en_pred_sat (); *)
           (* Globals.do_infer_inv := true; *)
           (* Globals.lemma_gen_unsafe := true; *)
-          Globals.graph_norm := true;
+          (* Globals.graph_norm := true; *)
+          
           Globals.is_solver_local := true;
           Omega.omegacalc:= "./oc";
           Fixcalc.fixcalc_exe := "./fixcalc ";
           Smtsolver.smtsolver_path := "./z3-4.3.2";
           Globals.disable_failure_explaining := false;
           Globals.return_must_on_pure_failure := true;
-          Globals.dis_impl_var := true),
+          (* Globals.dis_impl_var := true *)
+      ),
    "SVCOMP14 competition mode - essential printing only");
   ("--smt-compete",
      Arg.Unit
