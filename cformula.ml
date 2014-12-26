cc10fd676d33a39225874b86aab73807942d26a2
Le Quang Loc <lequangloc@gmail.com>
2014-12-26 16:37:47 +0800

    x

--- cformula.ml
+++ cformula.ml
@@ -10200,16 +10200,17 @@ and get_failure_branch bfl=
               )
 
 and get_failure_partial_context_x ((bfl:branch_fail list), succs): (string option*failure_kind*error_type list)=
-  let ((s_opt, ft, e) as r) = get_failure_branch bfl in
-  if !bug_detect then r else
-    match s_opt with
-      | None -> r
-      | Some s -> begin
-          match ft with
-            | Failure_Must s1 -> if succs = [] then r else
-                (Some ("(may) cause: [" ^s^",valid]"), Failure_May ("[" ^ s1^",valid]" ), e)
-            |  _ -> r
-        end
+  let (* ((s_opt, ft, e) as r) *)r = get_failure_branch bfl in
+  r
+  (* if !bug_detect then r else *)
+  (*   match s_opt with *)
+  (*     | None -> r *)
+  (*     | Some s -> begin *)
+  (*         match ft with *)
+  (*           | Failure_Must s1 -> if succs = [] then r else *)
+  (*               (Some ("(may) cause: [" ^s^",valid]"), Failure_May ("[" ^ s1^",valid]" ), e) *)
+  (*           |  _ -> r *)
+  (*       end *)
 
 and get_failure_partial_context a: (string option*failure_kind*error_type list)=
   let pr1 = !print_partial_context in
