451c6410031aeb90048c36e99711e45b36205d5c
Chin Wei Ngan <chinwn@gmail.com>
2015-03-18 10:42:48 +0800

    fixed compile problem

--- typechecker.ml
+++ typechecker.ml
@@ -2010,14 +2010,14 @@ and check_exp_a (prog : prog_decl) (proc : proc_decl) (ctx : CF.list_failesc_con
 	    (* let ctx1 = prune_ctx_failesc_list prog ctx in *)
             let ctx2 = list_failesc_context_and_unsat_now prog ctx in
             let ctx = ctx2 in
-            let ctx_simp = Cfout.simplify_failesc_context_list ctx in
+            (* let ctx_simp = Cfout.simplify_failesc_context_list ctx in *)
             (* let ctx1 = if !Globals.print_en_tidy then CF.rearrange_failesc_context_list ctx else ctx in *)
             (* Debug.info_hprint (add_str "dprint ctx0:" Cprinter.string_of_list_failesc_context) ctx0 pos; *)
             (* Debug.info_hprint (add_str "dprint ctx1:" Cprinter.string_of_list_failesc_context) ctx1 pos; *)
             (* Debug.info_hprint (add_str "dprint ctx2:" Cprinter.string_of_list_failesc_context) ctx2 pos; *)
             if str = "" then begin
               let str1 = (Cprinter.string_of_list_failesc_context ctx) in
-              let str2 = (Cprinter.string_of_list_failesc_context ctx_simp) in
+              (* let str2 = (Cprinter.string_of_list_failesc_context ctx_simp) in *)
 	      (if (Gen.is_empty ctx) then
                 (print_string_quiet ("\ndprint:"^pos.start_pos.Lexing.pos_fname
                 ^ ":" ^ (string_of_int pos.start_pos.Lexing.pos_lnum) ^" empty context"))
@@ -2026,7 +2026,7 @@ and check_exp_a (prog : prog_decl) (proc : proc_decl) (ctx : CF.list_failesc_con
                   ^ ":" ^ (string_of_int pos.start_pos.Lexing.pos_lnum) ^ ": ctx: " ^ str1 ^ "\n" in
                 let tmp1 = if (previous_failure ()) then ("failesc context: "^tmp1) else tmp1 in
                 let tmp2 = "\ndprint after: " ^ pos.start_pos.Lexing.pos_fname
-                  ^ ":" ^ (string_of_int pos.start_pos.Lexing.pos_lnum) ^ ": ctx: " ^ str2 ^ "\n" in
+                  ^ ":" ^ (string_of_int pos.start_pos.Lexing.pos_lnum) ^ ": ctx: " (* ^ str2 *) ^ "\n" in
                 let tmp2 = if (previous_failure ()) then ("failesc context: "^tmp2) else tmp2 in
                 print_string_quiet (tmp1 ^ tmp2));
               ctx
