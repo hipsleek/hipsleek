2e5742680173d5f0f7f0adf7e70fe1d05c586bc3
longph <devnull@localhost>
2015-03-18 15:44:20 +0800

    use baga

--- scriptarguments.ml
+++ scriptarguments.ml
@@ -628,6 +628,8 @@ let common_arguments = [
   ("--dis-unexpected",Arg.Clear Globals.show_unexpected_ents,"do not show unexpected results");
   ("--double-check",Arg.Set Globals.double_check,"double checking new syn baga");
   ("--dis-double-check",Arg.Clear Globals.double_check,"disable double-checking new syn baga");
+  ("--use-baga",Arg.Set Globals.use_baga,"use baga only (no inv infer)");
+  ("--dis-use-baga",Arg.Clear Globals.use_baga,"disable use baga only (no inv infer)");
   ("--inv-baga",Arg.Set Globals.gen_baga_inv,"generate baga inv from view");
   ("--dis-inv-baga",Arg.Clear Globals.gen_baga_inv,"disable baga inv from view");
   ("--pred-sat", Arg.Unit Globals.en_pred_sat ," turn off oc-simp for pred sat checking");
