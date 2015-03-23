e4e066be07a9cf89a7aedaf7fbeaa25c929ab0ba
Chin Wei Ngan <chinwn@gmail.com>
2015-03-23 11:08:41 +0800

    using \s instead of space

--- cppo/c_num_one.sh
+++ cppo/c_num_one.sh
@@ -1,8 +1,8 @@
 # this is to help locate module names
 #echo "s/$2 "/###x_add $2 "/"
-echo "s/= *"$1\.$2" /= x_add "$1\.$2" /"
-echo "s/("$1\.$2" /(x_add "$1\.$2" /"
-echo "s/( *"$1\.$2" /(x_add "$1\.$2" /"
+echo "s/=\s*"$1\.$2"\s/= x_add "$1\.$2" /"
+# echo "s/("$1\.$2" /(x_add "$1\.$2" /"
+echo "s/(\s*"$1\.$2"\s/(x_add "$1\.$2" /"
 # echo "s/= xx_add /= x_add /"
 # echo "s/= xxx_add /= x_add /"
 # echo "s/= xxxx_add /= x_add /"
