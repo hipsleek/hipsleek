26d22b4b4d5c746d26b28b76a55c62d8f6429700
longph <devnull@localhost>
2015-04-06 14:08:15 +0800

    m

--- global_var.ml
+++ global_var.ml
@@ -8,8 +8,6 @@ let eq_pair_id = fun (x1,y1) (x2,y2) -> (x1=x2) && (y1=y2)
 
 let stk_vars = new Gen.stack_pr (pr_sv) CP.eq_spec_var_nop
 
-let stk_num_trailer = new Gen.stack_pr (Gen.pr_id) (=)
-
 let stk_renamed_vars = new Gen.stack_pr pr_pair_id eq_pair_id
 
 let stk_evars = new Gen.stack_pr (pr_sv) CP.eq_spec_var_nop
