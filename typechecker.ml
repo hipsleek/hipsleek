942b28e3d41b65b03e0c7e82e7e4ed5c5e5fd4ca
Le Quang Loc <lequangloc@gmail.com>
2015-04-10 00:13:25 +0800

    check reachable Error

--- typechecker.ml
+++ typechecker.ml
@@ -2975,7 +2975,14 @@ and check_post_x_x (prog : prog_decl) (proc : proc_decl) (ctx0 : CF.list_partial
           Prooftracer.pop_div ();
           (* print_endline "DONE!" *)
         end in
-    if (CF.isSuccessListPartialCtx_new rs) then
+    let is_succ = CF.isSuccessListPartialCtx_new rs in
+    let is_reachable_succ = if not f1 then
+      is_succ
+    else
+      (*if error post, check reachable *)
+      is_succ && (CF.exist_reachable_states rs)
+    in
+    if ((* CF.isSuccessListPartialCtx_new rs *) is_reachable_succ) then
       rs
     else begin
       (* get source code position of failed branches *)
