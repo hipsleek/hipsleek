cbb962c928eea9e9e2d047450385c1e3e1224959
evan <evan@loris-5>
2015-06-17 16:51:07 +0800

    use remove_quantifiers

--- immutable.ml
+++ immutable.ml
@@ -3235,19 +3235,19 @@ let remove_abs_nodes_formula_helper form =
   | CF.Exists e -> 
      let (new_heap, coll_imm) = transform_h form e.CF.formula_exists_heap in
      let svl = imm_to_svl coll_imm in
-     let new_qvars = List.filter (fun s -> List.mem s svl) e.CF.formula_exists_qvars in
+     let removed_qvars = List.filter (fun s -> not (List.mem s svl)) e.CF.formula_exists_qvars in
      let new_pure =
        MCP.mix_of_pure (CP.filter_var_new (MCP.pure_of_mix e.CF.formula_exists_pure) svl) in
-     if new_qvars != [] then
-       Some (CF.Exists {
-                 e with
-                 CF.formula_exists_qvars = new_qvars;
-                 CF.formula_exists_heap = new_heap;
-                 CF.formula_exists_pure = new_pure })
-     else
-       Some (CF.Base { (CF.formula_base_of_formula_exists e) with
-                 CF.formula_base_heap = new_heap;
-                 CF.formula_base_pure = new_pure })
+     (match (CF.remove_quantifiers removed_qvars form) with
+      | CF.Base b ->
+         Some (CF.Base { b with
+                         CF.formula_base_heap = new_heap;
+                         CF.formula_base_pure = new_pure})
+      | CF.Exists e ->
+         Some (CF.Exists { e with
+                           CF.formula_exists_heap = new_heap;
+                           CF.formula_exists_pure = new_pure })
+      | CF.Or _ -> None)
   | CF.Or _ -> None
 
 let remove_abs_nodes_formula formula =
