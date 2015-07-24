94954865e92399a6f704ec4d3244a28739578417
Le Ton Chanh <letonchanh@gmail.com>
2015-07-25 00:01:40 +0800

    try to remove some dda

--- globals.ml
+++ globals.ml
@@ -1382,7 +1382,8 @@ let infer_const = ref ""
 (* TNT Inference *)
 let tnt_verbosity = ref 1
 let tnt_infer_lex = ref true
-let tnt_add_post = ref false (* disabled with @term_wo_post or --dis-term-add-post *)
+let tnt_add_post = ref true (* disabled with @term_wo_post or --dis-term-add-post *)
+let inc_add_post = ref false
 let tnt_abd_strategy = ref 0 (* by default: abd_templ *)
 let tnt_infer_nondet = ref true
 
