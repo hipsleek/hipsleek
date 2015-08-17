ffec8e8d83ca941fcefdd5dcdd4d89bc9721a727
Toan Nguyen Thanh <thanhtoantnt@gmail.com>
2015-08-17 17:54:01 +0800

    add new_estate scripts

--- post/scripts.sh
+++ post/scripts.sh
@@ -19,7 +19,7 @@ if [ $s = "hip" ]; then
         while IFS= read -r line
         do
             ../hip $line --assert-unsound-false > result.txt;
-            grepresult=$(grep -n "Unsound false in SLEEK?" result.txt);
+            grep_result=$(grep -n "Unsound false in SLEEK?" result.txt);
             if [ $? -eq 0 ]; then
                 echo $line
             fi
@@ -35,10 +35,42 @@ if [ $s = "sleek" ]; then
         while IFS= read -r line
         do
             ../sleek $line --assert-unsound-false > result.txt;
-            grepresult=$(grep -n "Unsound false in SLEEK?" result.txt);
+            grep_result=$(grep -n "Unsound false in SLEEK?" result.txt);
             if [ $? -eq 0 ]; then
                 echo $line
             fi
         done <<< "$result" 
     fi
 fi
+
+if [ $s = "hip" ]; then
+    result=$(find $d -type f -name "*.ss");
+    if [ $p = "new-estate" ]; then
+        while IFS= read -r line
+        do
+            ../hip $line > result.txt;
+            grep_result=$(grep -n "To add this to new_estate.es_infer_rel\|if important : need to add to estate.es_infer_rel" result.txt);
+            if [ $? -eq 0 ]; then
+                echo $line
+            fi
+        done <<< "$result" 
+    fi
+fi
+
+
+
+if [ $s = "sleek" ]; then
+    result=$(find $d -type f -name "*.slk");
+    if [ $p = "new-estate" ]; then
+        while IFS= read -r line
+        do
+            ../sleek $line > result.txt;
+            grep_result=$(grep -n "To add this to new_estate.es_infer_rel\|if important : need to add to estate.es_infer_rel" result.txt);
+            if [ $? -eq 0 ]; then
+                echo $line
+            fi
+        done <<< "$result" 
+    fi
+fi
+
+rm result.txt
