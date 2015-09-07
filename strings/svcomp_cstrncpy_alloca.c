95c7526ca14d304a837ab98ede5b984a5af964eb
tranmuoi <tranmuoi@loris-10>
2015-09-07 13:04:15 +0800

    to remove possible unsatisfiable state of post condition proving

--- strings/svcomp_cstrncpy_alloca.c
+++ strings/svcomp_cstrncpy_alloca.c
@@ -1,31 +1,66 @@
 #include <stdlib.h>
 
+/*@
+WFS<> ==
+  self::char_star<0,q>*q::BADS<> 
+  or self::char_star<v,q>*q::WFS<> & v!=0 
+  inv true;
+
+WFSeg<p> ==
+  self=p 
+  or self::char_star<v,q>*q::WFSeg<p> & v!=0
+  inv true;
+
+BADS<> ==
+  self::char_star<v,q>*q::BADS<> 
+  inv true;
+
+lemma_safe self::WFS<> -> self::BADS<>.
+*/
+
 extern int __VERIFIER_nondet_int(void);
 
 char *(cstrncpy)(char *s1, const char *s2, int n)
+/*@
+    requires s1::BADS<> * s2::WFS<> & n > 0
+    ensures s1::WFSeg<pp>*pp::char_star<0,_> & res=s1;
+*/
  {
      char *dst = s1;
      const char *src = s2;
      char *us;
      int n2;
      /* Copy bytes, one at a time.  */
-     while (n > 0) {
+     while (n > 0)
+       /*@
+          requires dst::BADS<> * src::WFS<> & n>0
+          ensures src::WFSeg<qq>*qq::char_star<0,src'>*src'::BADS<> * dst::WFSeg<pp>*pp::char_star<0,dst'>*dst'::BADS<> & n' > 0
+               or n' = 0;
+       */
+     {
          n--;
          if ((*dst++ = *src++) == '\0') {
              /* If we get here, we found a null character at the end
                 of s2, so use memset to put null bytes at the end of
                 s1.  */
-             us = dst;
+             /*us = dst;
              n2 = n;
              while (n2-- != 0)
+               /*
+                  requires us::BADS<> & n2>=0
+                  ensures us'::BADS<> & n2' = -1;
+               /
+                {
                  *us++ = '\0';
+                }*/
              break;
          }
+        //@dprint;
      }
      return s1;
  }
 
-int main() {
+/*int main() {
   int length = __VERIFIER_nondet_int();
   int n = __VERIFIER_nondet_int();
   if (length < 1) {
@@ -39,4 +74,4 @@ int main() {
   nondetString[length-1] = '\0';
   cstrncpy(nondetArea, nondetString, n);
   return 0;
-}
+}*/ 
