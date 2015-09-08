2ebbdd55b487030470157da245f829f4cdc4c131
tranmuoi <tranmuoi@loris-10>
2015-09-09 02:12:17 +0800

    to fix pre-condition in specs of svcomp_sctrpbrk.c

--- strings/svcomp_cstrpbrk_true_alloca.c
+++ strings/svcomp_cstrpbrk_true_alloca.c
@@ -4,26 +4,62 @@
  */
 #include <stdlib.h>
 
+/*@
+WFS<> ==
+  self::char_star<0,q>*q::BADS<> 
+  or self::char_star<v,q>*q::WFS<> & v!=0 
+  inv true;
+
+WFSeg<p> ==
+  self=p 
+  or self::char_star<v,q>*q::WFSeg<p> & v!=0
+  inv true;
+
+BADS<> ==
+  self::char_star<v,q>*q::BADS<> 
+  inv true;
+
+lemma_safe self::WFS<> -> self::BADS<>.
+
+*/
+
 extern int __VERIFIER_nondet_int(void);
 
 
 char *(cstrpbrk)(const char *s1, const char *s2)
+  /*@
+    requires s1::WFS<>*s2::WFS<>
+    ensures true;
+  */
  {
      const char *sc1;
      const char *s;
      int c;
-     for (sc1 = s1; *sc1 != '\0'; sc1++) {
+     for (sc1 = s1; *sc1 != '\0'; sc1++) 
+       /*@
+          requires sc1::WFS<>
+          ensures sc1::WFSeg<sc1'>*sc1'::char_star<0,q>*q::BADS<>; & flow __norm
+               or eres::ret_char_star<p>*sc1::WFSeg<p>*p::char_star<x,q>*q::WFS<> & flow ret_char_star;
+       */
+     {
          s = s2;
          c = *sc1;
          while (*s != '\0' && *s != (char)c)
-             s++;
-         if (*s != c)
-             return (char *)sc1;
+           /*@
+             requires s::WFS<>
+             ensures s::WFSeg<s'>*s'::char_star<0,q>*q::BADS<>
+                  or s::WFSeg<s'>*s'::char_star<c,q>*q::WFS<>; 
+           */
+         {    
+           s++;
+         }
+         /*if (*s != c)
+             return (char *)sc1;*/
      }
      return 0;                /* terminating nulls match */
  }
 
-int main() {
+/*int main() {
     int length1 = __VERIFIER_nondet_int();
     int length2 = __VERIFIER_nondet_int();
     if (length1 < 1) {
@@ -39,5 +75,25 @@ int main() {
     cstrpbrk(nondetString1,nondetString2);
     return 0;
 }
+
+*/
+
+
+/*=========================================================
+#svcomp_cstrpbrk.c
+Why s = s2 but failed to prove s::WFS<> in the precondition?
+
+Checking procedure while_38_5$int~char_star~char_star~char_star... 
+Proving precondition in method while_47_9$int~char_star Failed.
+  (may) cause: do_unmatched_rhs : s'::WFS<>@M(may)
+
+Context of Verification Failure: svcomp_cstrpbrk_true_alloca.c_43:18_43:65
+
+Last Proving Location: svcomp_cstrpbrk_true_alloca.c_47:9_55:10
+
+Procedure while_38_5$int~char_star~char_star~char_star FAIL.(2)
+
+
+Exception Failure("Proving precond failed") Occurred!
 
 
