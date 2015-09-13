a8c970c87c1cd7db4edbd1786bdce7aa9b32c1b9
Chin Wei Ngan <chinwn@gmail.com>
2015-09-13 21:16:28 +0800

    m

--- inferHP.ml
+++ inferHP.ml
@@ -45,50 +45,52 @@ let return_out_of_inst estate lhs_b extended_hps =
   (true, n_estate,lhs_b)
 
 
-let rec gen_inst prog estate lhds lhvs sst acc_p=
-  match sst with
+let gen_inst prog estate lhds lhvs sst acc_p =
+  let rec aux sst acc_p =
+    match sst with
     | [] -> true,acc_p
     | (sv1,sv2)::rest ->
-          let sv2_orig = CP.subs_one estate.CF.es_rhs_eqset sv2 in
-          if CP.eq_spec_var sv1 sv2_orig then
-            gen_inst prog estate lhds lhvs rest acc_p
-          else
-            (*str-inf/ex16c3d(8). exists free vars -> fail*)
-            let reach_vs = CF.look_up_reachable_ptr_args prog lhds lhvs [sv1] in
-            if CP.mem_svl sv2_orig reach_vs then
-              if !Globals.old_infer_complex_lhs then
-                gen_inst prog estate lhds lhvs rest acc_p
-              else false, acc_p
-            else
-              let p = CP.mkEqVar sv1 sv2 no_pos in
-              let new_acc = CP.mkAnd acc_p p no_pos in
-              gen_inst prog estate lhds lhvs rest new_acc
+      let sv2_orig = CP.subs_one estate.CF.es_rhs_eqset sv2 in
+      if CP.eq_spec_var sv1 sv2_orig then
+        aux (* gen_inst *) (* prog estate lhds lhvs *) rest acc_p
+      else
+        (*str-inf/ex16c3d(8). exists free vars -> fail*)
+        let reach_vs = CF.look_up_reachable_ptr_args prog lhds lhvs [sv1] in
+        if CP.mem_svl sv2_orig reach_vs then
+          if !Globals.old_infer_complex_lhs then
+            aux (* gen_inst *) (* prog estate lhds lhvs *) rest acc_p
+          else false, acc_p
+        else
+          let p = CP.mkEqVar sv1 sv2 no_pos in
+          let new_acc = CP.mkAnd acc_p p no_pos in
+          aux (* gen_inst *) (* prog estate lhds lhvs *) rest new_acc
+  in aux sst acc_p
 
- let do_inst prog estate lhs_b largs rargs extended_hps=
-   try
-     if rargs = [] then return_out_of_inst estate lhs_b extended_hps
-     else
-       let p = (MCP.pure_of_mix lhs_b.CF.formula_base_pure) in
-       let fvp = CP.fv p in
-       let () = Debug.ninfo_hprint (add_str  "fvp" !CP.print_svl) fvp no_pos in
-       let () = Debug.ninfo_hprint (add_str  "rargs" !CP.print_svl) rargs no_pos in
-       if CP.intersect_svl rargs fvp != [] then false,estate, lhs_b
-       else
-         let sst = List.combine largs rargs in
-         let lhds, lhvs, _ = CF.get_hp_rel_bformula lhs_b in
-         let is_succ, p = gen_inst prog estate lhds lhvs sst (CP.mkTrue no_pos) in
-         if not is_succ then
-           is_succ, estate, lhs_b
-         else
-           let () = Debug.ninfo_hprint (add_str  "p" !CP.print_formula) p no_pos in
-           let mf = (MCP.mix_of_pure p) in
-           let () = Debug.ninfo_hprint (add_str  "lhs_b" !CF.print_formula_base) lhs_b no_pos in
-           (true,
+let do_inst prog estate lhs_b largs rargs extended_hps=
+  try
+    if rargs = [] then return_out_of_inst estate lhs_b extended_hps
+    else
+      let p = (MCP.pure_of_mix lhs_b.CF.formula_base_pure) in
+      let fvp = CP.fv p in
+      let () = Debug.ninfo_hprint (add_str  "fvp" !CP.print_svl) fvp no_pos in
+      let () = Debug.ninfo_hprint (add_str  "rargs" !CP.print_svl) rargs no_pos in
+      if CP.intersect_svl rargs fvp != [] then false,estate, lhs_b
+      else
+        let sst = List.combine largs rargs in
+        let lhds, lhvs, _ = CF.get_hp_rel_bformula lhs_b in
+        let is_succ, p = gen_inst prog estate lhds lhvs sst (CP.mkTrue no_pos) in
+        if not is_succ then
+          is_succ, estate, lhs_b
+        else
+          let () = Debug.ninfo_hprint (add_str  "p" !CP.print_formula) p no_pos in
+          let mf = (MCP.mix_of_pure p) in
+          let () = Debug.ninfo_hprint (add_str  "lhs_b" !CF.print_formula_base) lhs_b no_pos in
+          (true,
            {estate with CF.es_formula = CF.mkAnd_pure estate.CF.es_formula mf no_pos;
-               CF.es_infer_vars_hp_rel = estate.CF.es_infer_vars_hp_rel@extended_hps;
+                        CF.es_infer_vars_hp_rel = estate.CF.es_infer_vars_hp_rel@extended_hps;
            },
            CF.mkAnd_base_pure lhs_b mf no_pos)
-   with _ -> return_out_of_inst estate lhs_b extended_hps
+  with _ -> return_out_of_inst estate lhs_b extended_hps
 
 
 (*
@@ -106,8 +108,6 @@ type: (CF.entail_state ->
   CP.spec_var list ->
   'e -> VarGen.loc -> Cformula.list_context * Prooftracer.proof
 *)
-
-
 let infer_unfold pm_aux action (* caller prog *) estate (* conseq *) lhs_b rhs_b (* a *) (rhs_h_matched_set: CP.spec_var list) (* is_folding *) pos
   : (Cformula.list_context * Prooftracer.proof) =
   let prog = () in
