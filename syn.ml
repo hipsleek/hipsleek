6663e8d3da559d6a3743513d7f61e0e824567e1c
Chin Wei Ngan <chinwn@gmail.com>
2015-09-17 18:47:12 +0800

    add logging info

--- syn.ml
+++ syn.ml
@@ -64,7 +64,7 @@ let add_dangling_hprel prog (hpr: CF.hprel) =
   Debug.no_1 "Syn:add_dangling_hprel" pr (pr_pair pr string_of_bool) (add_dangling_hprel prog) hpr
 
 let add_dangling_hprel_list prog (hpr_list: CF.hprel list) =
-  fst (List.split (List.map (add_dangling_hprel prog) hpr_list))
+  fst (List.split (List.map (x_add add_dangling_hprel prog) hpr_list))
 
 (*********************)
 (***** UNFOLDING *****)
@@ -135,7 +135,7 @@ let unfolding_one_hrel_def prog ctx hrel (hrel_def: CF.hprel) =
   in
   let guard_h, guard_p, _, _, _, _ = CF.split_components guard_f in
   let guard_h_f = CF.mkBase_simp guard_h ex_lhs_p in
-  let rs, residue = heap_entail_formula prog ctx guard_h_f in
+  let rs, residue = x_add heap_entail_formula prog ctx guard_h_f in
   if rs then
     let _, ctx_p, _, _, _, _ = CF.split_components ctx in
     if is_sat (MCP.merge_mems ctx_p guard_p true) then
@@ -179,7 +179,7 @@ let unfolding_one_hrel prog ctx hprel_name hrel hprel_groups =
       let non_inst_unguarded_hrel_defs, unguarded_hrel_defs = List.partition (is_non_inst_hprel prog) unguarded_hrel_defs in
       (* Only unfolding guarded hrel or non-inst hrel *)
       let unfolding_ctx_list = List.fold_left (fun acc hrel_def ->
-          let unfolding_ctx = unfolding_one_hrel_def prog ctx hrel hrel_def in
+          let unfolding_ctx = x_add unfolding_one_hrel_def prog ctx hrel hrel_def in
           match unfolding_ctx with
           | None -> acc
           | Some ctx -> acc @ [ctx]) [] (guarded_hrel_defs @ non_inst_unguarded_hrel_defs)
@@ -205,7 +205,7 @@ let unfolding_hrel_list prog ctx hprel_name hrel_list hprel_groups =
     match hrel_list with
     | [] -> [ctx]
     | hr::hrl ->
-      let ctx_list = unfolding_one_hrel prog ctx hprel_name hr hprel_groups in
+      let ctx_list = x_add unfolding_one_hrel prog ctx hprel_name hr hprel_groups in
       List.concat (List.map (fun ctx -> helper ctx hrl) ctx_list)
   in
   let non_inst_hrel_list, norm_hrel_list = List.partition (is_non_inst_hrel prog) hrel_list in
@@ -224,7 +224,7 @@ let rec unfolding_hprel prog hprel_groups (hpr: CF.hprel): CF.hprel list =
   let rhs_h, rhs_p, _, _, _, _ = CF.split_components hpr_rhs in
   let rhs_hrels, rhs_hpreds = List.partition CF.is_hrel (CF.split_star_conjunctions rhs_h) in
   let ctx = CF.mkBase_simp (CF.join_star_conjunctions rhs_hpreds) rhs_p in
-  let unfolding_ctx_list = unfolding_hrel_list prog ctx hpr_name rhs_hrels hprel_groups in
+  let unfolding_ctx_list = x_add unfolding_hrel_list prog ctx hpr_name rhs_hrels hprel_groups in
   let unfolding_hpr_list = List.map (fun unfolding_rhs -> 
       { hpr with hprel_rhs = unfolding_rhs }) unfolding_ctx_list in
   unfolding_hpr_list
@@ -269,32 +269,32 @@ let rec update_hprel_id_groups hprel_id hprel_sv hprel_id_list hprel_id_groups =
     else 
       (hpr_sv, hpri_group)::(update_hprel_id_groups hprel_id hprel_sv hprel_id_list hpri_groups)
 
-let rec helper_unfolding_hprel_list_x prog hprel_id_groups hprel_id_list =
+let rec helper_unfolding_hprel_list prog hprel_id_groups hprel_id_list =
   match hprel_id_list with
   | [] -> []
   | hpri::hpril ->
     let hprel_groups = List.map (fun (hprel_sv, hprel_id_list) ->
         (hprel_sv, List.map (fun hpri -> hpri.hprel_constr) hprel_id_list)
       ) hprel_id_groups in
-    let unfolding_hpr = unfolding_hprel prog hprel_groups hpri.hprel_constr in
+    let unfolding_hpr = x_add unfolding_hprel prog hprel_groups hpri.hprel_constr in
     let unfolding_hpri = List.map mk_hprel_id unfolding_hpr in
     let updated_hprel_id_groups = update_hprel_id_groups 
       hpri.hprel_id (name_of_hprel hpri.hprel_constr) unfolding_hpri hprel_id_groups in
     unfolding_hpr @ (helper_unfolding_hprel_list prog updated_hprel_id_groups hpril)
 
-and helper_unfolding_hprel_list prog hprel_id_groups hprel_id_list =
+let helper_unfolding_hprel_list prog hprel_id_groups hprel_id_list =
   let pr1 = Cprinter.string_of_hprel_list_short in
   let pr2 hpril = pr1 (List.map (fun hpri -> hpri.hprel_constr) hpril) in
   let pr3 = pr_list (pr_pair !CP.print_sv pr2) in
   Debug.no_2 "Syn:helper_unfolding_hprel_list" pr2 pr3 pr1
-    (fun _ _ -> helper_unfolding_hprel_list_x prog hprel_id_groups hprel_id_list)
+    (fun _ _ -> helper_unfolding_hprel_list prog hprel_id_groups hprel_id_list)
     hprel_id_list hprel_id_groups
 
 let unfolding_hprel_list prog hprel_list =
   let sorted_hprel_list = sort_hprel_list hprel_list in
   let hprel_id_list = List.map mk_hprel_id sorted_hprel_list in
   let hprel_id_groups = partition_hprel_list hprel_id_list in
-  helper_unfolding_hprel_list prog hprel_id_groups hprel_id_list
+  x_add helper_unfolding_hprel_list prog hprel_id_groups hprel_id_list
 
 let selective_unfolding prog other_hprels hprels = 
   let pre_hprels, post_hprels = List.partition is_pre_hprel hprels in
@@ -302,7 +302,7 @@ let selective_unfolding prog other_hprels hprels =
   let hprel_id_list = List.map mk_hprel_id sorted_hprel_list in
   let other_hprel_id_list = List.map mk_hprel_id other_hprels in
   let hprel_id_groups = partition_hprel_list (hprel_id_list @ other_hprel_id_list) in 
-  (helper_unfolding_hprel_list prog hprel_id_groups hprel_id_list) @ post_hprels
+  (x_add helper_unfolding_hprel_list prog hprel_id_groups hprel_id_list) @ post_hprels
 
 let selective_unfolding prog other_hprels hprels = 
   let pr = Cprinter.string_of_hprel_list_short in
@@ -372,7 +372,7 @@ let dangling_parameterizing_hprel (hpr: CF.hprel) =
   let is_pre = is_pre_hprel hpr in
   let param_f = if is_pre then hpr.hprel_rhs else hpr.hprel_lhs in 
   
-  let n_param_f, dangling_vars = remove_dangling_heap_formula param_f in
+  let n_param_f, dangling_vars = x_add_1 remove_dangling_heap_formula param_f in
   if is_empty dangling_vars then hpr, idf
   else
     let fresh_dangling_vars = CP.fresh_spec_vars dangling_vars in
@@ -381,8 +381,8 @@ let dangling_parameterizing_hprel (hpr: CF.hprel) =
     let hpr_name = name_of_hprel hpr in
     let f_update_params_hprel hpr =
       { hpr with
-        CF.hprel_lhs = add_dangling_params hpr_name dangling_params hpr.CF.hprel_lhs;
-        CF.hprel_rhs = add_dangling_params hpr_name dangling_params hpr.CF.hprel_rhs;
+        CF.hprel_lhs = x_add add_dangling_params hpr_name dangling_params hpr.CF.hprel_lhs;
+        CF.hprel_rhs = x_add add_dangling_params hpr_name dangling_params hpr.CF.hprel_rhs;
       }
     in
     let f_update_params_hprel hpr =
@@ -405,7 +405,7 @@ let rec dangling_parameterizing hprels =
     match hprels with
     | [] -> acc
     | hpr::hprl ->
-      let hpr_wo_dangling, f_update_params = dangling_parameterizing_hprel hpr in
+      let hpr_wo_dangling, f_update_params = x_add_1 dangling_parameterizing_hprel hpr in
       let n_hpr = f_update_params hpr_wo_dangling in
       let n_hprl = List.map f_update_params hprl in
       let n_acc = List.map f_update_params acc in
@@ -430,7 +430,7 @@ let dangling_parameterizing hprels =
 let syn_pre_preds prog (is: CF.infer_state) = 
   if !Globals.new_pred_syn then
     let () = x_binfo_pp ">>>>> Step 1: Adding dangling references <<<<<" no_pos in
-    let is_all_constrs, has_dangling_vars = List.split (List.map (add_dangling_hprel prog) is.CF.is_all_constrs) in
+    let is_all_constrs, has_dangling_vars = List.split (List.map (x_add add_dangling_hprel prog) is.CF.is_all_constrs) in
     let has_dangling_vars = or_list has_dangling_vars in
     let prog =
       if has_dangling_vars then
@@ -445,13 +445,13 @@ let syn_pre_preds prog (is: CF.infer_state) =
     in
   
     let () = x_binfo_pp ">>>>> Step 2: Unfolding <<<<<" no_pos in
-    let is_all_constrs = unfolding prog is_all_constrs in
+    let is_all_constrs = x_add unfolding prog is_all_constrs in
     let () = x_binfo_hp (add_str "Unfolding result" 
         Cprinter.string_of_hprel_list_short) is_all_constrs no_pos
     in
   
     let () = x_binfo_pp ">>>>> Step 3: Dangling Parameterizing <<<<<" no_pos in
-    let is_all_constrs = dangling_parameterizing is_all_constrs in
+    let is_all_constrs = x_add_1 dangling_parameterizing is_all_constrs in
     let () = x_binfo_hp (add_str "Parameterizing result" 
         Cprinter.string_of_hprel_list_short) is_all_constrs no_pos
     in
