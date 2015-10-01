a8d7934404a3072cd74ae35edd5d22f4573e69aa
tranmuoi <tranmuoi@loris-10>
2015-10-01 19:40:27 +0800

    add some svcomp examples

---
+++ svcomp16/svcomp-length-while-loop-true-termination.c
@@ -0,0 +1,26 @@
+#include "../examples/working/cparser/stdhip.h"
+struct node {
+  int val;
+  struct node* next;
+}; 
+
+struct node* new_ll(int n)
+{
+  if (n==0)
+    return NULL;
+  struct node *x = malloc(sizeof *x);
+  x->next = new_ll(n-1);
+  return x;
+}
+
+void main()
+{
+  struct node *xs = new_ll(10);
+  int length = 0;
+  while (xs != NULL)
+  {
+    length++;
+    xs = xs->next;
+  }
+  return (length);
+}
