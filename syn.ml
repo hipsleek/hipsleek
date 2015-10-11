ee2c6c8468bfbc7cf192c23da753b3adbc9c8003
Le Ton Chanh <letonchanh@gmail.com>
2015-10-11 17:01:25 +0800

    make shape_extends_view work with new-pred-extn, need to handle ll*

--- syn.ml
+++ syn.ml
@@ -944,7 +944,9 @@ let extn_norm_pred iprog cprog extn_pred norm_pred =
   let orig_info = (norm_ipred.I.view_name, norm_ipred.I.view_vars) in
   (* TODO: Auto derive REC *)
   let extn_info = (extn_pred.C.view_name, ["REC"], [extn_view_var]) in
-  let extn_iview = { extn_iview with I.view_derv_info = [(orig_info, extn_info)] } in
+  let extn_iview = { extn_iview with 
+    I.view_derv_info = [(orig_info, extn_info)];
+    I.view_derv_extns = [extn_info] } in
   let extn_cview = x_add_1 Derive.trans_view_dervs iprog 
     Rev_ast.rev_trans_formula Astsimp.trans_view [] 
     cprog.C.prog_view_decls extn_iview
