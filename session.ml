f0ef5a372438a43cfdba4bd2400cfc6d274af56f
Andreea Costea <andre.costea@gmail.com>
2016-06-03 14:55:51 +0800

    session/protocol/projection signature

--- session.ml
+++ session.ml
@@ -1,41 +1,50 @@
 #include "xdebug.cppo"
 open VarGen
 
-(* Created 21 Feb 2006 Simplify Iast to Cast *)
 open Globals
 open Gen.Basic
 open Printf
-open Gen.Basic
 open Gen.BList
 
 module F =Iformula
-(* Protocol *)
-(* Projection *)
 
 type transmission = Send | Receive
+
+(* ======= base formula for session type ====== *)
+(* ============================================ *)
+module type Session_base = sig
+  type t
+  val print_session_base : t -> string
+end;;
 
 type protocol_base_formula = {
   protocol_base_formula_sender   : ident;
   protocol_base_formula_receiver : ident;
   protocol_base_formula_message  : F.formula;
-}
+} ;;
 
-and projection_base_bormula = {
+type projection_base_bormula = {
   projection_base_bormula_op      : transmission;
   projection_base_bormula_channel : ident;
   projection_base_bormula_message : F.formula;
-}
-;;
+} ;;
 
-module type Session = sig
-  type t
-    
+module Protocol_base : Session_base = struct
+  type t = protocol_base_formula
+  let print_session_base f = "Protocol_base: to be implemented"
+end;;
+
+(* ============== session type ================ *)
+(* ============================================ *)
+module Make_Session (Base: Session_base) = struct
+  type t = Base.t
+
   type session =
     | SSeq  of session_seq_formula
     | SOr   of session_or_formula
     | SStar of session_star_formula
     | SBase of t
-        
+
   and session_seq_formula = {
     session_seq_formula_head: session;
     session_seq_formula_tail: session;
@@ -53,9 +62,17 @@ module type Session = sig
     session_seq_formula_star2: session;
     session_seq_formula_pos:  loc;
   }
-  
+
+  let print_session_base = Base.print_session_base
+
+  let print_session_star f = "to be implemented"
 end;;
 
+(* =========== Protocol / Projection ========== *)
+(* ============================================ *)
+module Protocol = Make_Session(Protocol_base);;
+
+type session_test = Protocol.session (* | Projection.session *)
 
 let foo =
   let () = print_endline "!!!!! SESSION!!!!!!!" in
