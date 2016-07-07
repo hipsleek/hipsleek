5e64c9663a18a775212feec5f9f7c06a8483f93b
valentina <valentina@loris-5>
2016-07-07 16:40:28 +0800

    Add sleek tests for session.

--- examples/working/run-fast-tests.pl
+++ examples/working/run-fast-tests.pl
@@ -1843,6 +1843,9 @@ my $inv = '--inv-test';
 my $dis = '--dis-inv-baga';
 %sleek_files=(
     "sleek"=>[["sleek.slk", "",(), "Valid.Valid.Valid.Fail."],
+                      ["session/ent-1.slk", "", (), "Valid.Valid.Fail."],
+                      ["session/ex4-1B1S1S-a1.slk", "", (), "Fail.Valid.Valid.Valid.Valid.Fail.Valid.Valid.Valid.Valid.Valid.Valid."],
+                      ["session/ex4-1B1S1S.slk", "", (), "Valid.Fail.Valid.Fail.Fail.Valid.Valid.Valid.Fail.Fail.Valid.Fail.Valid.Fail.Valid."],
                       ["cll-d.slk", "", (), "Valid."],
                       ["label-basic.slk", "--dis-eps", (), "Fail.Valid.Valid.Fail."],
                       ["label-dll.slk", "--dis-eps", (), "Valid.Valid.Valid.Valid."],
