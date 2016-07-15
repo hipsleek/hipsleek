a23f953925e69dea500dee75425a6d029f81f553
valentina <valentina@loris-5>
2016-07-15 17:05:20 +0800

    Add new test file for session.

--- examples/working/run-fast-tests.pl
+++ examples/working/run-fast-tests.pl
@@ -1846,6 +1846,7 @@ my $dis = '--dis-inv-baga';
                       ["session/ent-1.slk", "", (), "Valid.Valid.Fail."],
                       ["session/ex4-1B1S1S-a1.slk", "", (), "Fail.Valid.Valid.Valid.Valid.Fail.Valid.Valid.Valid.Valid.Valid.Valid."],
                       ["session/ex4-1B1S1S.slk", "", (), "Valid.Fail.Valid.Fail.Fail.Valid.Valid.Valid.Fail.Fail.Valid.Fail.Valid.Fail.Valid."],
+                      ["session/tst.slk", "", (), ""],
                       ["cll-d.slk", "", (), "Valid."],
                       ["label-basic.slk", "--dis-eps", (), "Fail.Valid.Valid.Fail."],
                       ["label-dll.slk", "--dis-eps", (), "Valid.Valid.Valid.Valid."],
