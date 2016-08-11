fe7109c482d1932133dad7d90d06f9c67f229953
valentina <valentina@loris-5>
2016-08-11 18:02:58 +0800

    Backout setting node kind for Sess predicate.

--- parser.ml
+++ parser.ml
@@ -2228,8 +2228,7 @@ simple_heap_constr:
         let (c, hid, deref) = get_heap_id_info c hid in
         let ho_args = un_option opt1 [] in
         match hl with
-        | ([],[]) -> let node = F.mkHeapNode c hid ho_args deref dr split imm_opt false false false frac [] [] ofl (get_pos_camlp4 _loc 2) in
-                     Session.IForm.update_heap_kind node
+        | ([],[]) -> F.mkHeapNode c hid ho_args deref dr split imm_opt false false false frac [] [] ofl (get_pos_camlp4 _loc 2)
         | ([], t) -> 
             let t11, t12 = List.split t in  
             let t21, t22 = List.split t12 in 
