f01e840d8bc6ead1afd985b636aab2c4e5f2aa32
elena <devnull@localhost>
2017-03-24 11:31:46 +0800

    Moved predicates in hodef.slk files; Created a bash script used to run chr as a single command

---
+++ chr/run_pl.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Usage example: ./run_pl.sh session_chr_prelude /tmp/test
+
+if [ "$#" -ne 2 ]; then
+  echo "Usage: $0 <prolog script name> <goals file name>"
+  exit 1
+fi
+
+PL_FILE=$1
+PL_FILE="${PL_FILE%.*}"
+
+echo -en "[$PL_FILE]." $(cat $2) | prolog
