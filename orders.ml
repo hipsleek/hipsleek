37246047b3550005fd9ce947c12a3fe486d0d886
elena <devnull@localhost>
2017-05-01 17:02:33 +0800

    Change orders' type

--- orders.ml
+++ orders.ml
@@ -31,20 +31,40 @@ let string_of_event e = (string_of_role e.role) ^ "^" ^(string_of_suid e.uid)
 let string_of_transmission e = (string_of_role e.sender) ^ "-" ^ (string_of_suid e.uid) ^ "->" ^ (string_of_role e.receiver)
                                                             
 (* order relations - to be collected after analyzing the protocol *)
-type orders  = HBe of (* hbe_type *) event * event
-             | HBt of transmission * transmission
-             | CBe of event * event
+type orders  = HBe of hbe_type
+             | HBt of hbt_type
+             | CBe of cbe_type
 and assrt   = Event of event
              | NEvent of event
              | Transm of transmission
              | Order of orders
-             | And of assrt * assrt
-             | Or of assrt * assrt
-             | Impl of event * assrt
+             | And of and_type
+             | Or of or_type
+             | Impl of impl_type
 
 and hbe_type = {
   hbe_event1 : event;
   hbe_event2 : event;
+}
+and hbt_type = {
+  hbt_transmission1 : transmission;
+  hbt_transmission2 : transmission;
+}
+and cbe_type = {
+  cbe_event1 : event;
+  cbe_event2 : event;
+}
+and and_type = {
+  and_assrt1 :  assrt;
+  and_assrt2 :  assrt;
+}
+and or_type = {
+  or_assrt1 :  assrt;
+  or_assrt2 :  assrt;
+}
+and impl_type = {
+  impl_event :  event;
+  impl_assrt :  assrt;
 }
 
 let string_of_assrt e1 =
@@ -53,16 +73,16 @@ let string_of_assrt e1 =
     | Event e  -> string_of_event e
     | NEvent e -> "not("^ (string_of_event e) ^ ")"
     | Transm t -> string_of_transmission t
-    | Order (HBe (e1,e2)) -> (string_of_event e1) ^ " <_HB " ^ (string_of_event e2)
-    | Order (HBt (e1,e2)) -> (string_of_transmission e1) ^ " <_HB " ^ (string_of_transmission e2)
-    | Order (CBe (e1,e2)) -> (string_of_event e1) ^ " <_CB " ^ (string_of_event e2)
-    | And (a1,a2) -> (helper a1) ^ "&&" ^ (helper a2)
-    | Or  (a1,a2) -> (helper a1) ^ "||" ^ (helper a2)
-    | Impl (e,a) -> (string_of_event e) ^ "=>" ^ (helper a)
+    | Order (HBe e) -> (string_of_event e.hbe_event1) ^ " <_HB " ^ (string_of_event e.hbe_event2)
+    | Order (HBt t) -> (string_of_transmission t.hbt_transmission1) ^ " <_HB " ^ (string_of_transmission t.hbt_transmission2)
+    | Order (CBe e) -> (string_of_event e.cbe_event1) ^ " <_CB " ^ (string_of_event e.cbe_event2)
+    | And a -> (helper a.and_assrt1) ^ "&&" ^ (helper a.and_assrt2)
+    | Or  a -> (helper a.or_assrt1) ^ "||" ^ (helper a.or_assrt2)
+    | Impl a -> (string_of_event a.impl_event) ^ "=>" ^ (helper a.impl_assrt)
   in helper e1
     
-let mk_hbe e1 e2 = Order (HBe (e1,e2))
-let mk_hbt e1 e2 = Order (HBt (e1,e2))
+let mk_hbe e1 e2 = Order (HBe {hbe_event1 = e1; hbe_event2 = e2})
+let mk_hbt e1 e2 = Order (HBt {hbt_transmission1 = e1; hbt_transmission2 = e2})
 
 
 
