035bcd8ab4667c2f38c7a40df663798c112be1d5
Andreea Costea <andre.costea@gmail.com>
2017-05-22 10:47:13 +0800

    variant of chr

--- chr/chr/session_orders_vars.pl
+++ chr/chr/session_orders_vars.pl
@@ -1,9 +1,9 @@
 #!/usr/bin/swipl -q
-:- module(orders, [ev/1,hb/2,cb/2,snot/1]).
+:- module(orders, [ev/1,hb/2,cb/2,snot/1,snot_eq/2]).
 :- use_module(library(chr)).
 
 :- initialization start.
-:- chr_constraint ev/1,hb/2,cb/2,snot/1.
+:- chr_constraint ev/1,hb/2,cb/2,snot/1,snot_eq/2.
 
 
 % disable singleton warning
@@ -35,16 +35,18 @@
 neg2   @ snot(A;B) <=> snot(A),snot(B).
 neg3   @ snot((A,B)) <=> snot(A);snot(B).
 neg4   @ ev(A),snot(ev(A))  <=> false.
+neg5   @ hb(A,B),snot(hb(A,B)) <=> false.
+neg6   @ snot_eq(A,A) <=> false.
 % neg5   @ hb(A,A),snot(hb(A,A)) <=> false.
-neg6   @ hb(A,B),snot(hb(A,B)) <=> false.
+% neg6   @ hb(A,B),snot(hb(A,B)) <=> false.
 sym1   @ hb(A,B)\hb(A,B) <=> true .
-asy2   @ hb(A,B),hb(B,A) ==> A=B.
-% asy4   @ hb(A,A) <=>  true.
+asy2   @ hb(A,B),hb(B,A) <=> A=B.
+asy4   @ cb(A,B) ==>  snot_eq(A,B).
 % HB transitivity 
-hbhb   @ hb(A,B), hb(B,C) ==> A\=B,B\=C | hb(A,C).
-% % CB transitivity 
-cbhb   @ cb(A,B), hb(B,C) ==> B\=C | hb(A,C).
-% cbhb   @ cb(A,L1,B,L1), hb(B,L1,C,L4) ==>  hb(A,L1,C,L4).
+% hbhb   @ hb(A,B), hb(B,C) ==> A\=B,B\=C | hb(A,C).
+hbhb   @ hb(A,B), hb(B,C) ==>  hb(A,C).
+% CB transitivity 
+cbhb   @ cb(A,B), hb(B,C) ==> snot_eq(B,C) | hb(A,C).
 
 % hb(A,L1,B,L2) :- hb(A,L1,C,L3),hb(C,L3,B,L2).
 % hb(A,L1,B,L2) :- cb(A,L1,C,L1),hb(C,L1,B,L2).
