00e49aaf9e52788564af326d9a12284c3c99e9db
elena <devnull@localhost>
2017-07-20 17:18:48 +0800

    Transform sleek rels to order rels

--- orders_relation.ml
+++ orders_relation.ml
@@ -12,7 +12,8 @@ module SC = Sesscommons
 
 let vars_separator = "__"
 
-(* Transforms Order relations to Sleek relations *)
+(* Transforms Order relations to Sleek relations
+ * Example: ocb(A,id_1,B,id_2) -> cb(A__id_1,B__id_2) *)
 let trans_ord_rels_to_sleek_rels (p_formula:CP.p_formula) =
   match p_formula with
   | CP.RelForm (spec_var, vars, loc) ->
@@ -50,4 +51,40 @@ let trans_ord_rels_to_sleek_rels (p_formula:CP.p_formula) =
   let pr = !CP.print_p_formula in
   let pr_out = pr_opt !CP.print_p_formula in
   Debug.no_1 "trans_ord_rels_to_sleek_rels" pr pr_out trans_ord_rels_to_sleek_rels p_formula
+
+(* Transforms Sleek relations to Order relations
+ * Example: cb(A__id_1,B__id_2) -> ocb(A,id_1,B,id_2) *)
+let trans_sleek_rels_to_order_rels (p_formula:CP.p_formula) =
+  match p_formula with
+  | CP.RelForm (spec_var, vars, loc) ->
+      begin match spec_var with
+      | CP.SpecVar (typ, id, prmd) ->
+          let ev_rel_id = un_option !SC.sevent_rel_id "" in
+          let hbp_rel_id = un_option !SC.shbp_rel_id "" in
+          let cb_rel_id = un_option !SC.scb_rel_id "" in
+          (* obtains order relation id *)
+          let oid = 
+            if id = ev_rel_id then un_option !SC.event_rel_id ""
+            else if id = hbp_rel_id then un_option !SC.hbp_rel_id ""
+            else if id = cb_rel_id then un_option !SC.cb_rel_id ""
+            else ""
+          in
+          if oid != "" then (* check for sleek relation *)
+            let res = List.fold_left (fun acc var -> match var with
+              | CP.Var  (var, pos) ->
+                let (role, id) = S.CForm.divide_vars var vars_separator in
+                acc@[CP.Var (role, pos); CP.Var (id, pos)]
+              | _ -> acc
+            ) [] vars in
+            let new_spec_var = CP.SpecVar (typ, oid, prmd) in
+            Some (CP.RelForm (new_spec_var, res, loc))
+          else None
+      end
+  | _ -> None
+
+let trans_sleek_rels_to_order_rels (p_formula:CP.p_formula) =
+  let pr = !CP.print_p_formula in
+  let pr_out = pr_opt !CP.print_p_formula in
+  Debug.no_1 "trans_sleek_rels_to_order_rels" pr pr_out trans_sleek_rels_to_order_rels p_formula
+
 
