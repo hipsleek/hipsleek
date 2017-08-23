12e8e2b19e6566c7b6f7ac08cae02fc0f305f81c
alireza <alireza@loris-5>
2017-08-23 17:30:52 +0800

    probable bug

--- random_util.ml
+++ random_util.ml
@@ -1,10 +1,12 @@
 open Random
 
+let _ = Random.self_init();;
+
 let get_random f t =
   if f>t
   then failwith ("get_random: Invalid input "^(string_of_int f)^" "^(string_of_int t))  else f+(int (t-f+1))
 ;;
 
 let yes_or_no () =
-  get_random 0 2 = 0
+  get_random 0 1 = 0
 ;;
