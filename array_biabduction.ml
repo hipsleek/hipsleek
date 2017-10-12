a80fe6700d14d1d850cd548b1cad459c03e59523
caizh-pc <devnull@localhost>
2017-10-12 10:23:08 +0800

    add notes

--- array_biabduction.ml
+++ array_biabduction.ml
@@ -586,18 +586,18 @@ let array_biabduction_generic get_sorted content_printer mkContentEq mkFreshCont
       match lhs_h, rhs_h with
       | StarForm [], StarForm [] -> base_case0 indent lhs_p rhs_p pre_cond
       | StarForm [], StarForm (rh::rtail) ->
-         if !Globals.array_entailment_frame || !Globals.array_entailment
-         then
-           let norm = mkNormOr_base [] (mkNormBaseNeg [] [] [mkFalse ()]) in
-           (print_and_return (mkBExists (pre_cond.vset, (mkBBaseNeg ([present_pure lhs_p])))) indent,norm)
-         else
+         (* if !Globals.array_entailment_frame || !Globals.array_entailment *)
+         (* then *)
+         (*   let norm = mkNormOr_base [] (mkNormBaseNeg [] [] [mkFalse ()]) in *)
+         (*   (print_and_return (mkBExists (pre_cond.vset, (mkBBaseNeg ([present_pure lhs_p])))) indent,norm) *)
+         (* else *)
            base_case1 indent lhs_p (rhs_p, rh, mkStarForm rtail) pre_cond
       | StarForm [], MatchForm (rh, rtail) ->
-         if !Globals.array_entailment_frame || !Globals.array_entailment
-         then
-           let norm = mkNormOr_base [] (mkNormBaseNeg [] [] [mkFalse ()]) in
-           (print_and_return (mkBExists (pre_cond.vset, (mkBBaseNeg ([present_pure lhs_p])))) indent,norm)
-         else
+         (* if !Globals.array_entailment_frame || !Globals.array_entailment *)
+         (* then *)
+         (*   let norm = mkNormOr_base [] (mkNormBaseNeg [] [] [mkFalse ()]) in *)
+         (*   (print_and_return (mkBExists (pre_cond.vset, (mkBBaseNeg ([present_pure lhs_p])))) indent,norm) *)
+         (* else *)
            base_case1 indent lhs_p (rhs_p, rh, rtail) pre_cond
       | StarForm (lh::ltail), StarForm [] -> base_case2 indent (lhs_p, lh, mkStarForm ltail) rhs_p  pre_cond
       | MatchForm (lh, ltail), StarForm [] -> base_case2 indent (lhs_p, lh, ltail) rhs_p  pre_cond
