beb1fcb7fb13c8da7c7657971b795d396df6f3ea
Ta Quang Trung <taquangtrungvn@gmail.com>
2018-06-22 17:04:30 +0800

    prepare to integrate songbird

---
+++ songbird.ml
@@ -0,0 +1,8 @@
+(* translation of HIP's data structures to Songbird's data structures
+   will be done here *)
+
+
+
+
+(* calls to Songbird's functions in songbird/src/prover.ml
+   will be done here *)
