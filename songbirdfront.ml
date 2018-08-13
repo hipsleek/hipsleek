013bee1c39fe19da5f006c2b04bb023e8518c848
Toan Nguyen Thanh <thanhtoantnt@gmail.com>
2018-08-13 11:25:00 +0800

    refactor

--- songbirdfront.ml
+++ songbirdfront.ml
@@ -53,29 +53,29 @@ let translate_back_var (var : SBGlobals.var) =
 
 let rec translate_exp (exp: CP.exp) =
   match exp with
-  | CP.Null loc -> (SBCast.Null (translate_loc loc), [])
-  | CP.Var (var, loc) -> (SBCast.Var (translate_var var, translate_loc loc),[])
-  | CP.IConst (num, loc) -> (SBCast.IConst (num, translate_loc loc),[])
+  | CP.Null loc -> SBCast.Null (translate_loc loc)
+  | CP.Var (var, loc) -> SBCast.Var (translate_var var, translate_loc loc)
+  | CP.IConst (num, loc) -> SBCast.IConst (num, translate_loc loc)
   | CP.Add (exp1, exp2, loc) ->
-        let (t_exp1, l1) = translate_exp exp1 in
-        let (t_exp2, l2) = translate_exp exp2 in
-        (SBCast.BinOp (Add, t_exp1, t_exp2, translate_loc loc), l1@l2)
+        let t_exp1 = translate_exp exp1 in
+        let t_exp2 = translate_exp exp2 in
+        SBCast.BinOp (Add, t_exp1, t_exp2, translate_loc loc)
   | CP.Subtract (exp1, exp2, loc) ->
-        let (t_exp1, l1) = translate_exp exp1 in
-        let (t_exp2, l2) = translate_exp exp2 in
-        (SBCast.BinOp (Sub, t_exp1, t_exp2, translate_loc loc), l1@l2)
+        let t_exp1 = translate_exp exp1 in
+        let t_exp2 = translate_exp exp2 in
+        SBCast.BinOp (Sub, t_exp1, t_exp2, translate_loc loc)
   | CP.Mult (exp1, exp2, loc) ->
-        let (t_exp1, l1) = translate_exp exp1 in
-        let (t_exp2, l2) = translate_exp exp2 in
-        (SBCast.BinOp (Mul, t_exp1, t_exp2, translate_loc loc), l1@l2)
+        let t_exp1 = translate_exp exp1 in
+        let t_exp2 = translate_exp exp2 in
+        SBCast.BinOp (Mul, t_exp1, t_exp2, translate_loc loc)
   | CP.Div (exp1, exp2, loc) ->
-        let (t_exp1, l1) = translate_exp exp1 in
-        let (t_exp2, l2) = translate_exp exp2 in
-        (SBCast.BinOp (Div, t_exp1, t_exp2, translate_loc loc), l1@l2)
+        let t_exp1 = translate_exp exp1 in
+        let t_exp2 = translate_exp exp2 in
+        SBCast.BinOp (Div, t_exp1, t_exp2, translate_loc loc)
   | CP.Template templ ->
     let fun_name = CP.name_of_sv templ.templ_id in
-    let args = templ.templ_args |> List.map translate_exp |> List.map fst in
-    ((SBCast.mk_func (SBCast.FuncName fun_name) args), [templ])
+    let args = templ.templ_args |> List.map translate_exp in
+    SBCast.mk_func (SBCast.FuncName fun_name) args
   | _ -> Gen.Basic.report_error VarGen.no_pos "this exp is not handled"
 
 let rec translate_back_exp (exp: SBCast.exp) = match exp with
@@ -108,52 +108,52 @@ let rec translate_pf (pure_f: CP.formula)  =
     begin
       match p_formula with
       | BVar (var, loc) ->
-        (SBCast.BVar (translate_var var, translate_loc loc), [])
+        SBCast.BVar (translate_var var, translate_loc loc)
       | BConst (b, loc) ->
-        (SBCast.BConst (b, translate_loc loc), [])
+        SBCast.BConst (b, translate_loc loc)
       | Eq (exp1, exp2, loc) ->
-        let (sb_exp1, l1) = translate_exp exp1 in
-        let (sb_exp2, l2) = translate_exp exp2 in
+        let sb_exp1 = translate_exp exp1 in
+        let sb_exp2 = translate_exp exp2 in
         let sb_loc = translate_loc loc in
-        (SBCast.BinRel (Eq, sb_exp1, sb_exp2, sb_loc), l1@l2)
+        SBCast.BinRel (Eq, sb_exp1, sb_exp2, sb_loc)
       | Neq (exp1, exp2, loc) ->
-        let (sb_exp1, l1) = translate_exp exp1 in
-        let (sb_exp2, l2) = translate_exp exp2 in
+        let sb_exp1 = translate_exp exp1 in
+        let sb_exp2 = translate_exp exp2 in
         let sb_loc = translate_loc loc in
-        (SBCast.BinRel (Ne, sb_exp1, sb_exp2, sb_loc), l1@l2)
+        SBCast.BinRel (Ne, sb_exp1, sb_exp2, sb_loc)
       | Gt (exp1, exp2, loc) ->
-        let (sb_exp1, l1) = translate_exp exp1 in
-        let (sb_exp2, l2) = translate_exp exp2 in
+        let sb_exp1 = translate_exp exp1 in
+        let sb_exp2 = translate_exp exp2 in
         let sb_loc = translate_loc loc in
-        (SBCast.BinRel (Gt, sb_exp1, sb_exp2, sb_loc), l1@l2)
+        SBCast.BinRel (Gt, sb_exp1, sb_exp2, sb_loc)
       | Gte (exp1, exp2, loc) ->
-        let (sb_exp1, l1) = translate_exp exp1 in
-        let (sb_exp2, l2) = translate_exp exp2 in
+        let sb_exp1 = translate_exp exp1 in
+        let sb_exp2 = translate_exp exp2 in
         let sb_loc = translate_loc loc in
-        (SBCast.BinRel (Ge, sb_exp1, sb_exp2, sb_loc), l1@l2)
+        SBCast.BinRel (Ge, sb_exp1, sb_exp2, sb_loc)
       | Lt (exp1, exp2, loc) ->
-        let (sb_exp1, l1) = translate_exp exp1 in
-        let (sb_exp2, l2) = translate_exp exp2 in
+        let sb_exp1 = translate_exp exp1 in
+        let sb_exp2 = translate_exp exp2 in
         let sb_loc = translate_loc loc in
-        (SBCast.BinRel (Lt, sb_exp1, sb_exp2, sb_loc), l1@l2)
+        SBCast.BinRel (Lt, sb_exp1, sb_exp2, sb_loc)
       | Lte (exp1, exp2, loc) ->
-        let (sb_exp1, l1) = translate_exp exp1 in
-        let (sb_exp2, l2) = translate_exp exp2 in
+        let sb_exp1 = translate_exp exp1 in
+        let sb_exp2 = translate_exp exp2 in
         let sb_loc = translate_loc loc in
-        (SBCast.BinRel (Le, sb_exp1, sb_exp2, sb_loc), l1@l2)
+        SBCast.BinRel (Le, sb_exp1, sb_exp2, sb_loc)
       | _ -> Gen.Basic.report_error VarGen.no_pos "this p_formula is not handled"
     end
   | And (f1, f2, loc) ->
-    let (n_f1, l1) = translate_pf f1 in
-    let (n_f2, l2) = translate_pf f2 in
-    (SBCast.PConj ([n_f1; n_f2], translate_loc loc), l1@l2)
+    let n_f1 = translate_pf f1 in
+    let n_f2 = translate_pf f2 in
+    SBCast.PConj ([n_f1; n_f2], translate_loc loc)
   | Or (f1, f2, _, loc) ->
-    let (n_f1, l1) = translate_pf f1 in
-    let (n_f2, l2) = translate_pf f2 in
-    (SBCast.PDisj ([n_f1; n_f2], translate_loc loc), l1@l2)
+    let n_f1 = translate_pf f1 in
+    let n_f2 = translate_pf f2 in
+    SBCast.PDisj ([n_f1; n_f2], translate_loc loc)
   | Not (f, _, loc) ->
-    let (n_f, l) = translate_pf f in
-    (SBCast.PNeg (n_f, translate_loc loc), l)
+    let n_f = translate_pf f in
+    SBCast.PNeg (n_f, translate_loc loc)
   | _ -> Gen.Basic.report_error VarGen.no_pos "this pure formula not handled"
 
 let translate_back_pf (pf : SBCast.pure_form) = match pf with
@@ -179,13 +179,12 @@ let translate_back_pf (pf : SBCast.pure_form) = match pf with
    Adding template f(args) = ?
    Output: Input for songbird infer_unknown_functions
 *)
-let create_templ_prog prog ents templ
+let create_templ_prog prog ents
   =
   let program = SBCast.mk_program "hip_input" in
-  let fun_name = CP.name_of_sv templ.CP.templ_id in
   let exp_decl = List.hd (prog.Cast.prog_exp_decls) in
+  let fun_name = exp_decl.Cast.exp_name in
   let args = exp_decl.Cast.exp_params |> List.map translate_var in
-  (* let args = templ.templ_args |> List.map CP.afv |> List.concat |> List.map translate_var in *)
   let f_defn = SBCast.mk_func_defn_unknown fun_name args in
   let ifr_typ = SBGlobals.IfrStrong in
   let infer_func = {
@@ -214,22 +213,18 @@ let translate_ent ent =
   let () = x_tinfo_hp (Gen.Basic.add_str "rhs: " (Cprinter.string_of_pure_formula))
       rhs VarGen.no_pos in
 
-  let (sb_lhs, tmpl_list) = translate_pf lhs in
-  let (sb_rhs, _) = translate_pf rhs in
+  let sb_lhs = translate_pf lhs in
+  let sb_rhs = translate_pf  rhs in
   let () = x_tinfo_hp (Gen.Basic.add_str "lhs: " (SBCast.pr_pure_form))
       sb_lhs VarGen.no_pos in
   let () = x_tinfo_hp (Gen.Basic.add_str "rhs: " (SBCast.pr_pure_form))
       sb_rhs VarGen.no_pos in
 
-  let templ = List.hd tmpl_list in
-  let n_ent = SBCast.mk_pure_entail sb_lhs sb_rhs in
-  (n_ent, templ)
+  SBCast.mk_pure_entail sb_lhs sb_rhs
 
 let get_repair_candidate prog ents =
-  let sb_ent_and_tmpl_list = List.map translate_ent ents in
-  let (sb_ents, tmpls) = List.split sb_ent_and_tmpl_list in
-  let templ = List.hd tmpls in
-  let fun_defs = create_templ_prog prog sb_ents templ in
+  let sb_ents = List.map translate_ent ents in
+  let fun_defs = create_templ_prog prog sb_ents in
   let get_func_exp fun_defs exp_decls =
     let exp_decl = List.hd exp_decls in
     let ident = exp_decl.Cast.exp_name in
