4bc597a13bbb2f1f11a8a7080a3521dfb268940f
YinFang Chen <darknesschen1997@gmail.com>
2019-01-29 14:27:02 +0800

    nested case spec use in update

--- scontracts/test-list/append_multi.c
+++ scontracts/test-list/append_multi.c
@@ -4,10 +4,6 @@ struct node{
 };
 
 /*@
-  list<n,B> == self=null & n=0 & B={}
-           or self::node<v,q> * q::list<n-1,B1> & B = union(B1,{v})
-  inv n>=0;
-
   lseg<p, n> == self=p & n=0
            or self::node<_, q> * q::lseg<p, n-1>
   inv n>=0;
@@ -22,12 +18,13 @@ struct node{
   // method to update part of one list
 */
 
-void update(struct node* x, int n, int num)
+void update1(struct node* x, int num)
 /*@
      case {
        x!=null ->
-           requires x::lseg<null,n> & num>=0 & num<=n
-           ensures  x::lseg_x<z,num,0> * z::lseg<null,n-num>;
+           requires x::lseg<null,n> & num>=0
+           ensures  x::lseg_x<z,num,0> * z::lseg<null,n-num> & num<n
+                 or x::lseg_x<z,n,0> & num>=n;
        x=null  ->
           requires true
           ensures  true;
@@ -37,6 +34,31 @@ void update(struct node* x, int n, int num)
     if(x==NULL || num==0) return;
     else {
         x->val = 0;
-        update(x->next,n-1,num-1);
+        update1(x->next,num-1);
+    }
+}
+
+void update2(struct node* x, int num)
+/*@
+     case {
+       x!=null ->
+           case {
+             num<n  ->
+                requires x::lseg<null,n> & num>=0
+                ensures  x::lseg_x<z,num,0> * z::lseg<null,n-num>;
+             num>=n ->
+                requires x::lseg<null,n> & num>=0
+                ensures  x::lseg_x<z,n,0>;
+           }
+       x=null  ->
+          requires true
+          ensures  true;
+  }
+*/
+{
+    if(x==NULL || num==0) return;
+    else {
+        x->val = 0;
+        update2(x->next,num-1);
     }
 }
