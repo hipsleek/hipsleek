79963ace0b87b3cb32e96d5c75d870efb3391f3e
Toan Nguyen Thanh <thanhtoantnt@gmail.com>
2019-06-26 12:41:51 +0800

    add files

---
+++ examples/working/hip/repair/infestor/sll-append.c
@@ -0,0 +1,23 @@
+struct node {
+  int val;
+	struct node* next;
+};
+
+/*@
+ll<n> == self = null & n = 0
+	or self::node<_, q> * q::ll<n-1>;
+*/
+
+void append(struct node* x, struct node* y)
+/*@
+  requires x::ll<n1> * y::ll<n2> & x!=null
+  ensures x::ll<n1+n2>;
+*/
+{
+	if (x->next == NULL){
+    x->next = y;
+  } else {
+    append(x->next, y);
+  }
+
+}
