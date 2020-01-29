ca087d3a91f9f8743c7f1ac1f1412d159869c0f3
Toan Nguyen Thanh <thanhtoantnt@gmail.com>
2020-01-29 16:01:53 +0800

    add one more mutation tactic

--- repair.ml
+++ repair.ml
@@ -49,7 +49,7 @@ let rec helper args = match args with
 
 (* Generate repair candidates *)
 (* mutation strategy *)
-let mutate_iast_exp (input_exp: I.exp) =
+let mutate_iast_exp (input_exp: I.exp) : I.exp list =
   let eq_strategy = ref false in
   let rec aux_exp i_exp = match i_exp with
     | I.Binary b_exp ->
@@ -86,29 +86,32 @@ let mutate_iast_exp (input_exp: I.exp) =
       I.Cond {cond_exp with I.exp_cond_condition = n_condition}
     | _ -> i_exp in
   let not_eq_exp = aux_exp input_exp in
-  (* (eq_exp, !eq_strategy) *)
-  (not_eq_exp, !not_eq_strategy)
+  let mutated_list = [(eq_exp, !eq_strategy); (not_eq_exp, !not_eq_strategy)] in
+  mutated_list |> List.filter (fun (_,y) -> y) |> List.map fst
 
 let mutating_proc iprog (iproc: I.proc_decl): bool =
-  let (modified, n_proc) = match iproc.I.proc_body with
-    | None -> (false, iproc)
+  let n_proc_list = match iproc.I.proc_body with
+    | None -> []
     | Some proc_body ->
-      let (n_body, b_modified) = mutate_iast_exp proc_body in
-      let n_proc = {iproc with I.proc_body = Some n_body} in
-      (b_modified, n_proc) in
-  if modified then
-    let aux_map i_proc =
-      if Syn.contains iproc.I.proc_name i_proc.I.proc_name
-      then n_proc else i_proc in
-    let n_procedures = iprog.I.prog_proc_decls |> List.map aux_map in
-    let n_iprog = {iprog with I.prog_proc_decls = n_procedures} in
-    try
-      let cprog, _ = Astsimp.trans_prog n_iprog in
-      let () = Typechecker.check_prog_wrapper n_iprog cprog in
-      let () = x_binfo_pp "REPAIRING BY MUTATION SUCCESSFUL" no_pos in
-      true
-    with _ -> false
-  else false
+      let n_exp_list = mutate_iast_exp proc_body in
+      let n_proc_list =
+        n_exp_list |> List.map (fun x -> {iproc with I.proc_body = Some x}) in
+      n_proc_list in
+  let aux_repair curr_res mutated_proc =
+    if curr_res then true
+    else
+      let aux_map i_proc =
+           if Syn.contains mutated_proc.I.proc_name i_proc.I.proc_name
+           then mutated_proc else i_proc in
+      let n_procedures = iprog.I.prog_proc_decls |> List.map aux_map in
+      let n_iprog = {iprog with I.prog_proc_decls = n_procedures} in
+      try
+        let cprog, _ = Astsimp.trans_prog n_iprog in
+        let () = Typechecker.check_prog_wrapper n_iprog cprog in
+        let () = x_binfo_pp "REPAIRING BY MUTATION SUCCESSFUL" no_pos in
+        true
+      with _ -> false in
+  List.fold_left aux_repair false n_proc_list
 
 let repair_iprog_by_mutation (iprog: I.prog_decl) =
   match (!Typechecker.repair_proc) with
@@ -731,12 +734,11 @@ let start_repair_wrapper (iprog: I.prog_decl) level =
   let () = if mutate_res then
       let () = x_binfo_pp "REPAIRING BY MUTATION SUCCESSFUL" no_pos in
       let duration = get_time() -. start_time in
-      let () = x_binfo_hp (add_str "TOTAL REPAIR TIME" pr_float) duration no_pos in
-      () in
+      x_binfo_hp (add_str "TOTAL REPAIR TIME" pr_float) duration no_pos
+    else
+      x_binfo_pp "REPAIRING BY MUTATION FAIL\n" no_pos
+  in
   mutate_res
-  (* if not(!infestor) then
-   *   mutate_res
-   * else mutate_res *)
 
 let infest_and_repair src (iprog : I.prog_decl) =
   let buggy_progs = create_buggy_prog src iprog in
