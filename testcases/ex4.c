6b7a4e16ae8c748b0e05755180eb2a3bb6b06033
Benedict Lee <benedictleejh@gmail.com>
2020-05-09 23:37:07 +0800

    Add improved spec for ex4

--- testcases/ex4.c
+++ testcases/ex4.c
@@ -9,6 +9,10 @@ struct node
 pred ll<n> == self = null & n = 0
            or self::node<_, q> * q::ll<n-1>
    inv n >= 0;
+
+pred lseg<n,q> == self = q & n=0
+           or self::node<_, q> * q::lseg<n-1,q>
+   inv n >= 0;
 */
 
 struct node *malloc(int size)
@@ -21,48 +25,45 @@ struct node *malloc(int size)
   }
 */;
 
-void addFirst(struct node **head_ref, int data)
+void addFirst(struct node **head_ref, int d)
 /*@
-  requires head_ref::node_star<v> * v::ll<n>
-  ensures head_ref::node_star<v1> * v1::ll<n + 1>;
+  requires head_ref::node_star<v>
+  ensures head_ref::node_star<q> * q::node<d1,v>;
+  requires head_ref::node_star<v> * v::ll<m>
+  ensures head_ref::node_star<v1> * v1::ll<m + 1>;
 */
 {
   struct node *newNode = malloc(sizeof(struct node));
-  newNode->data = data;
+  newNode->data = d;
   newNode->next = *head_ref;
   *head_ref = newNode;
 }
 
-void addLast(struct node **head_ref, int data)
+void addLast(struct node **head_ref, int d1)
 /*@
-  requires head_ref::node_star<v> & v = null
-  ensures head_ref::node_star<v1> * v1::ll<1>;
-  requires head_ref::node_star<v> * v::ll<n> & n > 0
-  ensures head_ref::node_star<v> * v::ll<n + 1>;
-*/;
-// {
-//   struct node *newNode = malloc(sizeof(struct node));
-//   newNode->data = data;
-//   newNode->next = NULL;
+   requires head_ref::node_star<null>
+   ensures  head_ref::node_star<q> * q::node<_,null>;
+   requires head_ref::node_star<q> * q::lseg<n,r> * r::node<_,null>
+   ensures  head_ref::node_star<q> * q::lseg<n,r> * r::node<_,s> *s::node<d1,null>;
+*/
+{
+   struct node* newNode = (struct node*) malloc(sizeof(struct node));
+   newNode -> data = d1;
+   newNode -> next = NULL;
 
-//   if (*head_ref == NULL)
-//   {
-//     *head_ref = newNode;
-//   }
-//   else
-//   {
-//     struct node *ptr = *head_ref;
-//     while (ptr->next != NULL)
-//     /*@
-//       requires ptr::ll<n> & n > 0
-//       ensures ptr::node<_, null>;
-//     */
-//     {
-//       ptr = ptr->next;
-//     }
-//     ptr->next = newNode;
-//   }
-// }
+   if (*head_ref == NULL) {
+       *head_ref = newNode;
+   } else {
+       struct node* ptr = *head_ref;
+       while (ptr -> next != NULL)
+         /*@
+           requires ptr::lseg<n,q>*q::node<_,null>
+           ensures  ptr::lseg<n,q>*q::node<_,null> & ptr'=q;
+          */
+          ptr = ptr -> next;
+       ptr -> next = newNode;
+   }
+}
 
 void freeList(struct node *hd)
 /*@
