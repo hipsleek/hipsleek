c6e9825425d19ac9c82fadd20c1bd2e60d3e166e
Benedict Lee <benedictleejh@gmail.com>
2020-05-09 18:04:54 +0800

    Add current results of ex4

--- testcases/ex4.c
+++ testcases/ex4.c
@@ -1,7 +1,8 @@
 //Ex.4: Memory Leak
-struct node {
-   int data;
-   struct node* next;
+struct node
+{
+  int data;
+  struct node *next;
 };
 
 /*@
@@ -10,68 +11,113 @@ pred ll<n> == self = null & n = 0
    inv n >= 0;
 */
 
-// void addFirst(struct node** head_ref, int data)
-void addFirst(struct node* head_ref, int data)
+struct node *malloc(int size)
 /*@
-   requires head_ref::ll<n>
-   ensures head_ref::ll<n + 1>;
+  case {
+    size <= 0 -> requires true ensures res = null;
+    size >  0 ->
+      requires true
+      ensures res::node<_, _>;
+  }
+*/;
+
+void addFirst(struct node **head_ref, int data)
+/*@
+  requires head_ref::node_star<v> * v::ll<n>
+  ensures head_ref::node_star<v1> * v1::ll<n + 1>;
 */
 {
-   struct node* newNode = (struct node*) malloc(sizeof(struct node));
-   newNode -> data = data;
-   // newNode -> next = *head_ref;
-   newNode -> next = head_ref;
-   // *head_ref = newNode;
-   head_ref = newNode;
+  struct node *newNode = malloc(sizeof(struct node));
+  newNode->data = data;
+  newNode->next = *head_ref;
+  *head_ref = newNode;
 }
 
-void addLast(struct node** head_ref, int data)
-// void addLast(struct node* head_ref, int data)
+void addLast(struct node **head_ref, int data)
+/*@
+  requires head_ref::node_star<v> & v = null
+  ensures head_ref::node_star<v1> * v1::ll<1>;
+  requires head_ref::node_star<v> * v::ll<n> & n > 0
+  ensures head_ref::node_star<v> * v::ll<n + 1>;
+*/;
+// {
+//   struct node *newNode = malloc(sizeof(struct node));
+//   newNode->data = data;
+//   newNode->next = NULL;
+
+//   if (*head_ref == NULL)
+//   {
+//     *head_ref = newNode;
+//   }
+//   else
+//   {
+//     struct node *ptr = *head_ref;
+//     while (ptr->next != NULL)
+//     /*@
+//       requires ptr::ll<n> & n > 0
+//       ensures ptr::node<_, null>;
+//     */
+//     {
+//       ptr = ptr->next;
+//     }
+//     ptr->next = newNode;
+//   }
+// }
+
+void freeList(struct node *hd)
+/*@
+  requires hd::ll<n>
+  ensures emp;
+*/
 {
-   struct node* newNode = (struct node*) malloc(sizeof(struct node));
-   newNode -> data = data;
-   newNode -> next = NULL;
-
-   if (*head_ref == NULL) {
-       *head_ref = newNode;
-   } else {
-       struct node* ptr = *head_ref;
-       while (ptr -> next != NULL) ptr = ptr -> next;
-       ptr -> next = newNode;
-   }
+  struct node *p1 = hd;
+  struct node *p2;
+  while (p1 != NULL)
+  /*@
+    requires p1::ll<n>
+    ensures emp;
+  */
+  {
+    p2 = p1;
+    p1 = p1->next;
+    free(p2);
+  }
 }
 
-void freeList(struct node* head) {
-   struct node* p1 = head;
-   struct node* p2;
-   while (p1 != NULL) {
-       p2 = p1;
-       p1 = p1 -> next;
-       free(p2);
-   }
-}
-
-void printList(struct node* ptr) {
-   // printf("List:\n");
-   while (ptr != NULL) {
-      //  printf("%d\n", ptr -> data);
-       ptr = ptr -> next;
-   }
+void printList(struct node *ptr)
+/*@
+  requires ptr::ll<n>
+  ensures ptr::ll<n>;
+*/
+{
+  // printf("List:\n");
+  while (ptr != NULL)
+  /*@
+    requires ptr::ll<n>
+    ensures ptr::ll<n>;
+  */
+  {
+    //  printf("%d\n", ptr -> data);
+    ptr = ptr->next;
+  }
 }
 
 // Memory Leak Example
-int main() {
-   struct node* head = NULL;
-   // addLast(&head, 1);
-   addLast(head, 1);
-   // addFirst(&head, 0);
-   addFirst(head, 0);
-   // 2. if also comment out the printList() line,
-   // which accesses the structure in a loop,
-   // error can be found even with --unroll 1 in SMACK
-   printList(head);
-   // 1. if only comment out the freeList() line,
-   // no error reported by SMACK unless with --unroll 3 or more
-   // freeList(head);
-   return 0;
+int main()
+/*@
+  requires true
+  ensures emp;
+*/
+{
+  struct node *head = NULL;
+  addLast(&head, 1);
+  addFirst(&head, 0);
+  // 2. if also comment out the printList() line,
+  // which accesses the structure in a loop,
+  // error can be found even with --unroll 1 in SMACK
+  printList(head);
+  // 1. if only comment out the freeList() line,
+  // no error reported by SMACK unless with --unroll 3 or more
+  // freeList(head);
+  return 0;
 }
