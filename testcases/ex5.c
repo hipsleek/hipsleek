dbf30f93d56c5eb5d6f5e5e455115c6233d145d6
Andreea Costea <andre.costea@gmail.com>
2020-05-10 18:44:06 +0800

    ex5 still doesn't work

--- testcases/ex5.c
+++ testcases/ex5.c
@@ -1,20 +1,12 @@
 //Ex.5: tricky memory leak
 
-/*@
-pred arr_seg<p,n> == self::int_star<_> & self=p & n=1
-  or self::int_star<_>*q::arr_seg<p,n-1> & q = self + 1 & n > 1
-  inv n>=1.
-
-*/
-
 int *malloc(int size)
 /*@
   case {
     size <= 0 -> requires true ensures res = null;
     size >  0 ->
       requires true
-      ensures  res::arr_seg<q,size>;
-      //      ensures res::int_star<_>;
+      ensures res::int_star<_>;
   }
 */;
 
@@ -23,29 +15,22 @@ void *memcpy(int *dest, int *src, int length) __attribute__ ((noreturn))
 /*@
   requires dest=null & src = null
   ensures  false;
-  requires src::arr_seg<_, _>@L & dest=null
+  requires src::int_star<_>@L & dest=null
   ensures  false;
-  requires dest::arr_seg<_, _>@L & src=null
+  requires dest::int_star<_>@L & src=null
   ensures  false;
-  requires dest::arr_seg<_, n1> * src::arr_seg<_, n2>@L  & length>=0 & n1>=length & n2>=length
-  ensures  dest::arr_seg<_, n2>; //TODO how to capture that the values in dest are clones of src?
+  requires dest::int_star<_> * src::int_star<x>@L  & length>=0 
+  ensures  dest::int_star<x>; 
+
 */;
 
 // char a[sizeof(int*)];
 int *a;
 
-int** cast_temp(void **p)
-/*@
-  requires p::void_star_star<_>
-  ensures  p::int_star_star<_> & res=p;
-*/;
-
-
-
 void foo(void)
-/*@ //infer[@classic]
-  requires a::arr_seg<_,_>
-  ensures  a'::arr_seg<_,_>;
+/*@ infer [@leak]
+  requires a::int_star<_>
+  ensures  a'::int_star<_>;
 */
 {
   int *p = (int *)malloc(10); // This p will leak
@@ -54,28 +39,15 @@ void foo(void)
 
 
 int main(void)
-/*@ //infer[@classic]
-  requires a::arr_seg<_,_>
-  ensures  a'::arr_seg<_,_>;
+/*@
+  requires a::int_star<_>
+  ensures  a'::int_star<_>;
 */
 {
   foo();
-  void *pp; // this p will free
+  void *p; // this p will free
+  memcpy(&p, a, sizeof p);
   /*@ dprint; */
-  //int *x = &pp;
-  /*@ dprint; */
-  memcpy(&pp, a, sizeof pp);
-  /*@ dprint; */
-  free((int *)pp);
+  free((int *)p);
 }
 
-
-/*
-  Q1. are we planning to converge to actual sizes of the primitive types?
-  memcpy(a, &p, sizeof p);
-  ===>
-  in HIP: memcpy(a_78, p, 1)
-  in C:   memcpy(a_78, p, 8)
-
-  Q2. What should be the type of & pointer? 
-*/
