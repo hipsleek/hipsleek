1c7ab9b5935681978e4c9301137faf2b33208779
Dan Baterisna <alden.baterisna@yahoo.com>
2024-11-22 15:16:53 +0800

    Add copying prelude files to Dune build
    Adds two new targets, one for each prelude file

--- hip.ml
+++ hip.ml
@@ -130,8 +130,8 @@ let process_primitives (file_list: string list) : Iast.prog_decl list =
     Debug.info_zprint (lazy ((" processing primitives \"" ^(pr_list pr_id file_list) ^ "\n"))) no_pos;
   flush stdout;
   let new_names = List.map (fun c-> (Gen.get_path Sys.executable_name) ^ (String.sub c 1 ((String.length c) - 2))) file_list in
-  if (Sys.file_exists "./prelude.ss") then
-    [(parse_file_full "./prelude.ss" true)]
+  if (Sys.file_exists "prelude.ss") then
+    [(parse_file_full "prelude.ss" true)]
   else List.map (fun x -> parse_file_full x true) new_names
 
 let process_primitives (file_list: string list) : Iast.prog_decl list =
